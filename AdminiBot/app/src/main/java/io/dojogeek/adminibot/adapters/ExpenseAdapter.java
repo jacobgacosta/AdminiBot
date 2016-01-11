539d98d384ff01803293e231ccbf952894443549
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-11 17:53:38 -0600

    Adapter for Expense is created

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/adapters/ExpenseAdapter.java
@@ -0,0 +1,37 @@
+package io.dojogeek.adminibot.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import io.dojogeek.adminibot.R;
+import io.dojogeek.adminibot.models.ExpenseModel;
+
+public class ExpenseAdapter extends ArrayAdapter<ExpenseModel> {
+
+    public ExpenseAdapter(Context context, int resource, List<ExpenseModel> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ExpenseModel expense = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_expense_list , parent, false);
+        }
+
+        convertView.findViewById(R.id.expense_description);
+        convertView.findViewById(R.id.expense_amount);
+        convertView.findViewById(R.id.payment_method);
+        convertView.findViewById(R.id.expense_date);
+
+        return convertView;
+
+    }
+}
