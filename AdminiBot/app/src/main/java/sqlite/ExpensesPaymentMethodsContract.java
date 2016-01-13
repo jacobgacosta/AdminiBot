735b09a5a43f417cc81bba1e38570040796ce4fd
Jacob G.Acosta <jacob.guzman@adesis.com>
2016-01-12 23:37:39 -0600

    'ExpensesPaymentMethodsContract' class is created to bridge between 'Expenses' and 'PaymentMethods' tables

---
+++ AdminiBot/app/src/main/java/sqlite/ExpensesPaymentMethodsContract.java
@@ -0,0 +1,15 @@
+package sqlite;
+
+import android.provider.BaseColumns;
+
+public class ExpensesPaymentMethodsContract {
+
+    public static abstract class ExpensesPaymentMethods implements BaseColumns {
+        public static final String TABLE_NAME = "expenses_payment_methods ";
+        public static final String COLUMN_EXPENSE_ID = "expense_id";
+        public static final String COLUMN_PAYMENTH_METHOD_ID = "payment_method_id";
+        public static final String COLUMN_AMOUNT = "amount";
+        public static final String COLUMN_NAME_NULLABLE = "null";
+    }
+
+}
