465f57d7b4ef3b226c9d755adf467ced3550ed64
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-15 14:17:45 -0600

    Regex for only letters is added

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/validators/ValidatorsValues.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/validators/ValidatorsValues.java
@@ -3,6 +3,7 @@ package io.dojogeek.adminibot.validators;
 public interface ValidatorsValues {
 
     String EMAIL_REGEXP = "^([a-zA-Z0-9_\\.\\-])+@(([a-zA-Z0-9\\-])+\\.)+([a-zA-Z0-9]{2,4})+";
+    String ONLY_LETTERS = "[a-zA-Z]+";
     int EMAIL_MAX_LENGTH = 40;
     int NAME_MAX_LENGHT = 15;
 
