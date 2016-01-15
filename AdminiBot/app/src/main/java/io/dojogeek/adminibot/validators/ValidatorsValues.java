30eccd53010b8a704d68f1b279e34c2517cf2f88
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-15 17:57:50 -0600

    Regex for 'only letters' is change to 'only letters and spaces'

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/validators/ValidatorsValues.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/validators/ValidatorsValues.java
@@ -3,8 +3,9 @@ package io.dojogeek.adminibot.validators;
 public interface ValidatorsValues {
 
     String EMAIL_REGEXP = "^([a-zA-Z0-9_\\.\\-])+@(([a-zA-Z0-9\\-])+\\.)+([a-zA-Z0-9]{2,4})+";
-    String ONLY_LETTERS = "[a-zA-Z]+";
+    String ONLY_LETTERS_AND_SPACES = "[a-zA-Z\\s']+";
     int EMAIL_MAX_LENGTH = 40;
     int NAME_MAX_LENGHT = 15;
+    int LAST_NAME_MAX_LENGHT = 30;
 
 }
