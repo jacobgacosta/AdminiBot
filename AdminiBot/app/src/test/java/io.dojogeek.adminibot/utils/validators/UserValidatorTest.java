ee8703e5fa4ceaed49442c0707f403be935fe98b
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-15 14:17:24 -0600

    Valid values for User are declared to class level

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/UserValidatorTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/UserValidatorTest.java
@@ -12,6 +12,9 @@ import static org.junit.Assert.assertTrue;
 
 public class UserValidatorTest {
 
+    private static final String CORRECT_NAME = "Jacob";
+    private static final String CORRECT_LAST_NAME = "Guzman A";
+    private static final String CORRECT__EMAIL = "jgacosta@dojogeek.io";
     private static final String EMTPY_VALUE = "";
     private static final String SPACE_VALUE = "  ";
     private UserValidator mUserValidator;
@@ -24,7 +27,7 @@ public class UserValidatorTest {
     @Test
     public void userValidator_userModel_isTrue() {
 
-        UserValidator userValidator = createUserValidator("jgacosta@dojogeek.io", "Jacob", "G. Acosta");
+        UserValidator userValidator = createUserValidator(CORRECT__EMAIL, CORRECT_NAME, CORRECT_LAST_NAME);
 
         boolean isValid = userValidator.validate();
         
@@ -41,7 +44,7 @@ public class UserValidatorTest {
         UserValidator userValidator;
 
         for (String email : invalidEmails) {
-            userValidator = createUserValidator(email, "Jacob", "G. Acosta");
+            userValidator = createUserValidator(email, CORRECT_NAME, CORRECT_LAST_NAME);
             boolean isValid = userValidator.validate();
             assertFalse(isValid);
         }
@@ -51,7 +54,7 @@ public class UserValidatorTest {
     @Test
     public void userValidator_nameLenght_isFalse() {
 
-        UserValidator userValidator = createUserValidator("jgacosta@dojogeek.io", "Jacooooooooooooooooooooooob", "G. Acosta");
+        UserValidator userValidator = createUserValidator(CORRECT__EMAIL, "Jacooooooooooooooooooooooob", CORRECT_LAST_NAME);
         boolean isValid = userValidator.validate();
         assertFalse(isValid);
         assertEquals(R.string.error_wrong_lenght_name, userValidator.getErrorMessageName());
@@ -63,7 +66,7 @@ public class UserValidatorTest {
         final String [] emptyValues = {EMTPY_VALUE, SPACE_VALUE};
 
         for (String value : emptyValues) {
-            UserValidator userValidator = createUserValidator("jgacosta@dojogeek.io", value, "G. Acosta");
+            UserValidator userValidator = createUserValidator(CORRECT__EMAIL, value, CORRECT_LAST_NAME);
             boolean isValid = userValidator.validate();
             assertFalse(isValid);
             assertEquals(R.string.error_empty_value, userValidator.getErrorMessageName());
@@ -72,13 +75,17 @@ public class UserValidatorTest {
     }
 
     @Test
-    public void userValidator_nameWithNumbers_isFalse() {
+    public void userValidator_nameWithoutOnlyLetters_isFalse() {
 
-        UserValidator userValidator = createUserValidator("jgacosta@dojogeek.io", "Jac0b", "G. Acosta");
+        final String [] invalidNames = {"12345", "Jacob2", "j@cob", "Ja_cob",
+                "J$b", "jacob!"};
 
-        boolean isValid = userValidator.validate();
-
-        assertFalse(isValid);
+        for (String value : invalidNames) {
+            UserValidator userValidator = createUserValidator(CORRECT__EMAIL, value, CORRECT_LAST_NAME);
+            boolean isValid = userValidator.validate();
+            assertFalse(isValid);
+            assertEquals(R.string.error_wrong_format_name, userValidator.getErrorMessageName());
+        }
 
     }
 
