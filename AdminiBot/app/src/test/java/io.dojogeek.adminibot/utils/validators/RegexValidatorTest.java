a281ed7c934f03b471e2e376bb4016991a8d70c7
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-16 09:49:51 -0600

    test is added to validate correct matched string

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/RegexValidatorTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/RegexValidatorTest.java
@@ -1,7 +1,28 @@
 package io.dojogeek.adminibot.utils.validators;
 
+import org.junit.Test;
+
+import io.dojogeek.adminibot.R;
+import io.dojogeek.adminibot.utils.ValidatorsValues;
+import io.dojogeek.adminibot.validators.RegexValidator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class RegexValidatorTest {
 
+    @Test
+    public void regexValidator_matchedString_isTrue() {
+
+        String correctEmail = "jgacosta@dojogeek.io";
+
+        RegexValidator regexValidator = new RegexValidator(ValidatorsValues.EMAIL_REGEXP);
+        boolean isValid = regexValidator.isValid(correctEmail);
+
+        assertTrue(isValid);
+
+    }
 
 
 }
