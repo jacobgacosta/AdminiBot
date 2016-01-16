d8baece8576e5dbda89384bacae30310c1e777d4
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-15 20:40:58 -0600

    test is added to validate correct length value

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/LenghtValidatorTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/LenghtValidatorTest.java
@@ -1,4 +1,20 @@
 package io.dojogeek.adminibot.utils.validators;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import io.dojogeek.adminibot.validators.LenghtValidator;
+
 public class LenghtValidatorTest {
+
+    @Test
+    public void lengthValidator_correctValue_isTrue() {
+
+        int maxLength = 8;
+        String value = "Dojogeek";
+
+        LenghtValidator lenghtValidator = new LenghtValidator(maxLength);
+        lenghtValidator.isValid(value);
+    }
+
 }
