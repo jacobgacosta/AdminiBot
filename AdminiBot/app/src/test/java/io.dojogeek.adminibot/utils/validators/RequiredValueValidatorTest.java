f660fca049def3df6637e0f7eedd0d6ec4f95fba
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-15 20:14:30 -0600

    RequiredValueValidator class is created

---
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/RequiredValueValidatorTest.java
@@ -0,0 +1,15 @@
+package io.dojogeek.adminibot.utils.validators;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+
+import io.dojogeek.adminibot.validators.RequiredValueValidator;
+
+public class RequiredValueValidatorTest {
+
+
+
+}
