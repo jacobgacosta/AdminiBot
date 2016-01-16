7b8a8e849dd140a874e8a9318d3eecb63620eeb8
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-16 15:04:43 -0600

    Test is added to validate correct email

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/CompoundValidatorsFactoryTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/CompoundValidatorsFactoryTest.java
@@ -2,7 +2,26 @@ package io.dojogeek.adminibot.utils.validators;
 
 import org.junit.Test;
 
+import io.dojogeek.adminibot.validators.CompoundValidator;
+import io.dojogeek.adminibot.validators.CompoundValidatorsFactory;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 public class CompoundValidatorsFactoryTest {
 
+    private static final int NO_ERRORS = 0;
+
+    @Test
+    public void compoundValidators_correctEmail_isTrue() {
+
+        final String email = "jgacosta@dojogeek.io";
+
+        CompoundValidator compoundValidator = CompoundValidatorsFactory.emailValidator();
+        compoundValidator.isValid(email);
+
+        assertEquals(compoundValidator.getErrorMsg(), NO_ERRORS);
+    }
 
 }
