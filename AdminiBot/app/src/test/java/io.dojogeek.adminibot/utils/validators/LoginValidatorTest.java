40fe553c535b6186472b20faa1d8d45fd705fe1f
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-16 10:43:41 -0600

    test is added to validate correct data

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/LoginValidatorTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/LoginValidatorTest.java
@@ -9,6 +9,24 @@ import static org.junit.Assert.assertTrue;
 
 public class LoginValidatorTest {
 
-    
+    private LoginValidator mLoginValidator;
+
+    @Before
+    public void setup() {
+
+        mLoginValidator = new LoginValidator();
+
+    }
+
+    @Test
+    public void loginValidator_correctData_isTrue() {
+
+        mLoginValidator.setEmail("jgacosta@dojogeek.io");
+        mLoginValidator.setPassword("dojogeek_1234_#990DG");
+
+        boolean isValid = mLoginValidator.validate();
+
+        assertTrue(isValid);
+    }
 
 }
