a0518693cef952642226eeaa1a233e27998f788e
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-15 20:26:36 -0600

    test is added to validate empty value

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/RequiredValueValidatorTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/RequiredValueValidatorTest.java
@@ -5,11 +5,14 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import io.dojogeek.adminibot.validators.RequiredValueValidator;
 
 public class RequiredValueValidatorTest {
 
+    private static final String EMTPY_VALUE = "";
+    private static final String SPACE_VALUE = "  ";
     private RequiredValueValidator mRequiredValueValidator;
 
     @Before
@@ -27,5 +30,15 @@ public class RequiredValueValidatorTest {
         assertFalse(isValid);
     }
 
+    @Test
+    public void requiredValueValidator_emtptyValue_isFalse() {
+
+        String [] emptyValues = {EMTPY_VALUE, SPACE_VALUE};
+
+        for (String value : emptyValues) {
+            boolean isValid = mRequiredValueValidator.isValid(value);
+            assertFalse(isValid);
+        }
+    }
 
 }
