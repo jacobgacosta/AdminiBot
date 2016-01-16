1fcc7c06f5bf885b737d55758f47d7b5a7675417
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-16 11:09:57 -0600

    test is added to validate invalid format email

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/LoginValidatorTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/LoginValidatorTest.java
@@ -3,8 +3,11 @@ package io.dojogeek.adminibot.utils.validators;
 import org.junit.Before;
 import org.junit.Test;
 
+import io.dojogeek.adminibot.R;
 import io.dojogeek.adminibot.validators.LoginValidator;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class LoginValidatorTest {
@@ -29,4 +32,21 @@ public class LoginValidatorTest {
         assertTrue(isValid);
     }
 
+    @Test
+    public void loginValidator_formatEmail_isFalse() {
+
+        String [] invalidEmails = {"jgacosta@dojogeek", "jgacosta@dojo@.com", "jgacosta@.com",
+                "jgacosta@..", "jgacosta@@@.com", "jgacosta@dojogeek@dojogeek.io", "dojogeek.io"};
+
+        for (String emails : invalidEmails) {
+
+            mLoginValidator.setEmail(emails);
+            mLoginValidator.setPassword("dojogeek1234@_DG#1");
+            boolean isValid = mLoginValidator.validate();
+            assertFalse(isValid);
+            assertEquals(R.string.error_wrong_format_email, mLoginValidator.getErrorMessageEmail());
+
+        }
+
+    }
 }
