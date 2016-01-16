c871721f023a3e0c85b1f8d9b9e8151b211887bb
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-16 10:39:51 -0600

    LoginValidatorTest is added to validate LoginValidator functionality

---
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/LoginValidatorTest.java
@@ -0,0 +1,14 @@
+package io.dojogeek.adminibot.utils.validators;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import io.dojogeek.adminibot.validators.LoginValidator;
+
+import static org.junit.Assert.assertTrue;
+
+public class LoginValidatorTest {
+
+    
+
+}
