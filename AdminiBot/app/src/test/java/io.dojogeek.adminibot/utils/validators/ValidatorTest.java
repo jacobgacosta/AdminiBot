0abe4504fcf43ec51fdff00cde06665bad7f836c
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-17 11:13:41 -0600

    ValidatorTest class is created to test Validator functionality

---
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/ValidatorTest.java
@@ -0,0 +1,25 @@
+package io.dojogeek.adminibot.utils.validators;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.dojogeek.adminibot.utils.ValidatorsValues;
+import io.dojogeek.adminibot.validators.DataValidator;
+import io.dojogeek.adminibot.validators.LenghtValidator;
+import io.dojogeek.adminibot.validators.RegexValidator;
+import io.dojogeek.adminibot.validators.RequiredValueValidator;
+import io.dojogeek.adminibot.validators.Validator;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ValidatorTest {
+
+
+}
