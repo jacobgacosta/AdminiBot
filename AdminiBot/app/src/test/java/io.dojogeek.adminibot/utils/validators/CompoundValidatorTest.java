761ec3751f44a338f33b24afb38ddcbe4b0fd8f3
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-17 13:37:43 -0600

    Test case is added to validate correct value

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/CompoundValidatorTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/CompoundValidatorTest.java
@@ -2,7 +2,27 @@ package io.dojogeek.adminibot.utils.validators;
 
 import org.junit.Test;
 
+import io.dojogeek.adminibot.utils.ValidatorsValues;
+import io.dojogeek.adminibot.validators.CompoundValidator;
+import io.dojogeek.adminibot.validators.LenghtValidator;
+import io.dojogeek.adminibot.validators.RegexValidator;
+import io.dojogeek.adminibot.validators.RequiredValueValidator;
+
+import static org.junit.Assert.*;
+
 public class CompoundValidatorTest {
 
+    @Test
+    public void compoundValidator_correctValue_isTrue() {
+
+        final String email = "jgacosta@dojogeek.io";
+
+        CompoundValidator compoundValidator = new CompoundValidator();
+        compoundValidator.addValidator(RegexValidator.withRegexp(ValidatorsValues.EMAIL_REGEXP));
+        compoundValidator.addValidator(new RequiredValueValidator());
+        compoundValidator.addValidator(LenghtValidator.withMaxLenght(ValidatorsValues.EMAIL_MAX_LENGTH));
+
+        assertTrue(compoundValidator.isValid(email));
+    }
 
 }
