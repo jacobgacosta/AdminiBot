189a512e7ff9080cb501bebd13c3d462601046c4
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-17 11:09:09 -0600

    Regex is added for only numbers

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/utils/ValidatorsValues.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/utils/ValidatorsValues.java
@@ -4,6 +4,7 @@ public interface ValidatorsValues {
 
     String EMAIL_REGEXP = "^([a-zA-Z0-9_\\.\\-])+@(([a-zA-Z0-9\\-])+\\.)+([a-zA-Z0-9]{2,4})+";
     String ONLY_LETTERS_AND_SPACES = "[a-zA-Z\\s']+";
+    String ONLY_NUMBERS = "[0-9]+";
     int EMAIL_MAX_LENGTH = 40;
     int NAME_MAX_LENGHT = 15;
     int LAST_NAME_MAX_LENGHT = 30;
