d8170da813c24f2ee290626366a38bd7ddf3c9d0
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-17 14:33:45 -0600

    test case is added to validate wrong value

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/CompoundValidatorTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/utils/validators/CompoundValidatorTest.java
@@ -2,6 +2,7 @@ package io.dojogeek.adminibot.utils.validators;
 
 import org.junit.Test;
 
+import io.dojogeek.adminibot.R;
 import io.dojogeek.adminibot.utils.ValidatorsValues;
 import io.dojogeek.adminibot.validators.CompoundValidator;
 import io.dojogeek.adminibot.validators.LenghtValidator;
@@ -25,4 +26,17 @@ public class CompoundValidatorTest {
         assertTrue(compoundValidator.isValid(email));
     }
 
+    @Test
+    public void compoundValidator_wrongValue_isFalse() {
+
+        final String name = "Jac0b";
+
+        CompoundValidator compoundValidator = new CompoundValidator();
+        compoundValidator.addValidator(new RequiredValueValidator());
+        compoundValidator.addValidator(RegexValidator.withRegexp(ValidatorsValues.ONLY_LETTERS_AND_SPACES).
+                withErroMessage(R.string.error_wrong_format_name));
+
+        assertFalse(compoundValidator.isValid(name));
+        assertEquals(compoundValidator.getErrorMsg(), R.string.error_wrong_format_name);
+    }
 }
