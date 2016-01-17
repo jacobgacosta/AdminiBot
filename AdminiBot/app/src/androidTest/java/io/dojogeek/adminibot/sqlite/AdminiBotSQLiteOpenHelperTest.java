ceec645b22d9243cb56011f69a81692bf83fb387
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-17 17:20:09 -0600

    'AdminiBotSQLiteOpenHelperTest' class is created to validate 'AdminiBotSQLiteOpenHelper' functionality

---
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/AdminiBotSQLiteOpenHelperTest.java
@@ -0,0 +1,14 @@
+package io.dojogeek.adminibot.sqlite;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+import sqlite.AdminiBotSQLiteOpenHelper;
+
+@RunWith(AndroidJUnit4.class)
+public class AdminiBotSQLiteOpenHelperTest {
+
+
+
+}
