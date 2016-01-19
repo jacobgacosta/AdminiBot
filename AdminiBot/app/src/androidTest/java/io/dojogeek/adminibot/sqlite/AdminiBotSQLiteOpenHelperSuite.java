6ba666c21adb93f4cb63b5020a3e1d7bde803ac0
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-18 21:56:45 -0600

    AdminiBotSQLiteOpenHelperTest  is renamed to AdminiBotSQLiteOpenHelperSuite for build a suite class

---
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/AdminiBotSQLiteOpenHelperSuite.java
@@ -0,0 +1,12 @@
+package io.dojogeek.adminibot.sqlite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ExpensesTypesSQLiteOpenHelperTest.class})
+public class AdminiBotSQLiteOpenHelperSuite {
+
+
+
+}
