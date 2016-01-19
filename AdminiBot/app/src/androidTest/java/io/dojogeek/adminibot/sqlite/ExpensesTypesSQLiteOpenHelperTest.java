636df8b86c6f16de5d19faa843b7600a1cc4d52e
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-18 21:58:19 -0600

    ExpensesTypesSQLiteOpenHelperTest class is created to validate ExpenseType table operations

---
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/ExpensesTypesSQLiteOpenHelperTest.java
@@ -0,0 +1,128 @@
+package io.dojogeek.adminibot.sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.dojogeek.adminibot.R;
+import io.dojogeek.adminibot.models.ExpenseTypeModel;
+import sqlite.AdminiBotSQLiteOpenHelper;
+import sqlite.ExpenseTypeContract;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class ExpensesTypesSQLiteOpenHelperTest {
+
+    private static final int ADITIONAL_INDEX = 1;
+    private static final int INSERTION_RESULT_FAILED = -1;
+    private AdminiBotSQLiteOpenHelper mAdminiBotSQLiteOpenHelper;
+    private Context mContext;
+
+    @Before
+    public void setUp() {
+
+        mContext = getTargetContext();
+
+        mContext.deleteDatabase(AdminiBotSQLiteOpenHelper.DATABASE_NAME);
+
+        mAdminiBotSQLiteOpenHelper = AdminiBotSQLiteOpenHelper.getInstance(mContext);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mAdminiBotSQLiteOpenHelper.close();
+    }
+
+    @Test
+    public void sqliteHelper_correctCreatedTable() {
+
+        
+
+    }
+
+    @Test
+    public void sqliteHelper_numberOfInsertedExpensesTypes_isEquals() {
+
+        final int insertedValues = 5;
+
+        SQLiteDatabase database = mAdminiBotSQLiteOpenHelper.getReadableDatabase();
+        Cursor cursor = database.query(ExpenseTypeContract.ExpenseType.TABLE_NAME, null, null, null, null, null, null);
+
+        assertEquals(cursor.getCount(), insertedValues);
+
+    }
+
+    @Test
+    public void sqliteHelper_expensesTypesInserted_isEquals() {
+
+        SQLiteDatabase database = mAdminiBotSQLiteOpenHelper.getReadableDatabase();
+        Cursor cursor = database.query(ExpenseTypeContract.ExpenseType.TABLE_NAME, null, null, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+
+            while (cursor.isAfterLast() == false) {
+                compareResultQueryFields(cursor);
+                cursor.moveToNext();
+            }
+        }
+    }
+
+    private void compareResultQueryFields(Cursor currentPosition) {
+
+        int [] expectedExpensesTypes = ExpenseTypeContract.EXPENSES_TYPES;
+        int [] expextedExpensesTypesDescriptions = ExpenseTypeContract.EXPENSES_TYPES_DESCRIPTIONS;
+
+        long id = currentPosition.getInt(currentPosition.getColumnIndex(ExpenseTypeContract.ExpenseType._ID));
+        int name = currentPosition.getInt(currentPosition.getColumnIndex(ExpenseTypeContract.ExpenseType.COLUMN_NAME));
+        int description = currentPosition.getInt(currentPosition.getColumnIndex(ExpenseTypeContract.ExpenseType.COLUMN_DESCRIPTION));
+
+        String expectedName = getStringFromResourceId(expectedExpensesTypes[getId(id)]);
+        String expectedDescription = getStringFromResourceId(expextedExpensesTypesDescriptions[getId(id)]);
+
+        String actualName = getStringFromResourceId(name);
+        String actualDescription = getStringFromResourceId(description);
+
+        assertEquals(expectedName, actualName);
+        assertEquals(expectedDescription, actualDescription);
+
+    }
+
+    private String getStringFromResourceId(int resourceId) {
+        return mContext.getResources().getString(resourceId);
+    }
+
+    private int getId(long value) {
+        return (int) value - ADITIONAL_INDEX;
+    }
+
+    private ContentValues createContentValues() {
+
+        ExpenseTypeModel expenseTypeModel = createExpenseTypeModel();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ExpenseTypeContract.ExpenseType.COLUMN_NAME, expenseTypeModel.name);
+        contentValues.put(ExpenseTypeContract.ExpenseType.COLUMN_DESCRIPTION, expenseTypeModel.description);
+
+        return contentValues;
+    }
+
+    private ExpenseTypeModel createExpenseTypeModel() {
+
+        ExpenseTypeModel expenseTypeModel = new ExpenseTypeModel();
+        expenseTypeModel.name = R.string.expenses_types_foods;
+        expenseTypeModel.description = 0;
+
+        return expenseTypeModel;
+
+    }
+
+}
