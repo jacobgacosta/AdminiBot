4c82044906c3b20d3b277545f245ca776a18b75b
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-19 17:46:45 -0600

    UserSQLiteOpenHelperTest is added to this suite

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/AdminiBotSQLiteOpenHelperSuite.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/AdminiBotSQLiteOpenHelperSuite.java
@@ -5,6 +5,6 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ExpenseTypeSQLiteOpenHelperTest.class, PaymentMethodSQLiteOpenHelperTest.class,
-        ExpenseSQLiteOpenHelperTest.class})
+        ExpenseSQLiteOpenHelperTest.class, UserSQLiteOpenHelperTest.class})
 public class AdminiBotSQLiteOpenHelperSuite {
 }
