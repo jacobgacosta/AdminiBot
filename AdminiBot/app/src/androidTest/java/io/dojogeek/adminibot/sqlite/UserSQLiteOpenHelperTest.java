b46b01909ae0231ccd316990d7d865a32ad928f4
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-19 17:31:56 -0600

    UserSQLiteOpenHelperTest class is created to validate table user operations

---
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/UserSQLiteOpenHelperTest.java
@@ -0,0 +1,16 @@
+package io.dojogeek.adminibot.sqlite;
+
+import android.content.Context;
+
+import org.junit.After;
+import org.junit.Before;
+
+import sqlite.AdminiBotSQLiteOpenHelper;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+
+public class UserSQLiteOpenHelperTest {
+
+    
+
+}
