1423d0bd82083133b98847350fafbb2a7d21ed3e
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-19 12:18:13 -0600

    ExpenseSQLiteOpenHelperTest class is created to validate Expense table operations

---
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/ExpenseSQLiteOpenHelperTest.java
@@ -0,0 +1,12 @@
+package io.dojogeek.adminibot.sqlite;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ExpenseSQLiteOpenHelperTest {
+
+    
+
+}
