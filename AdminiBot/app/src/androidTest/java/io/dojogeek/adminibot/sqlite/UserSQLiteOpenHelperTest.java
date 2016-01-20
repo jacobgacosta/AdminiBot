d13521f7a7911dbe66355c2d955e2635ef52d5d2
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-19 18:00:22 -0600

    Test case is created to validate correct creation for user table

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/UserSQLiteOpenHelperTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/UserSQLiteOpenHelperTest.java
@@ -1,16 +1,56 @@
 package io.dojogeek.adminibot.sqlite;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import sqlite.AdminiBotSQLiteOpenHelper;
+import sqlite.PaymentMethodsContract;
+import sqlite.UserContract;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static org.junit.Assert.assertTrue;
 
 public class UserSQLiteOpenHelperTest {
 
-    
+    private static final int ADITIONAL_INDEX = 1;
+    private static final int NONE_TABLE_CREATED = 0;
+    private AdminiBotSQLiteOpenHelper mAdminiBotSQLiteOpenHelper;
+    private Context mContext;
+    private SQLiteDatabase mSQLiteDatabase;
 
+    @Before
+    public void setUp() {
+
+        mContext = getTargetContext();
+
+        mContext.deleteDatabase(AdminiBotSQLiteOpenHelper.DATABASE_NAME);
+
+        mAdminiBotSQLiteOpenHelper = AdminiBotSQLiteOpenHelper.getInstance(mContext);
+
+        mSQLiteDatabase = mAdminiBotSQLiteOpenHelper.getReadableDatabase();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mAdminiBotSQLiteOpenHelper.close();
+    }
+
+    @Test
+    public void sqliteHelper_correctCreatedTable_isTrue() {
+
+        Cursor cursor = mSQLiteDatabase.rawQuery("SELECT count(*) FROM sqlite_master WHERE type = 'table' AND name = '" +
+                UserContract.User.TABLE_NAME + "'", null);
+
+        if (cursor != null) {
+
+            assertTrue(cursor.getCount() > NONE_TABLE_CREATED);
+
+        }
+
+    }
 }
