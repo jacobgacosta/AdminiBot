67ed0bdc52582e70bca1a529ec373b3d8f3517a2
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-20 13:29:34 -0600

    DatabaseOperationsUtils class is created to perform CRUD to database

---
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/utils/DatabaseOperationsUtils.java
@@ -0,0 +1,56 @@
+package io.dojogeek.adminibot.sqlite.utils;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import io.dojogeek.adminibot.models.UserModel;
+import io.dojogeek.adminibot.sqlite.UserContract;
+
+public class DatabaseOperationsUtils {
+
+    private SQLiteDatabase mSQLiteDatabase;
+
+    public long createRecord(SQLiteDatabase sqliteDatabase, String tableName, ContentValues contentValues) {
+
+        setSQLiteDatabase(sqliteDatabase);
+
+        return createRecord(tableName, contentValues);
+    }
+
+    private long createRecord(String tableName, ContentValues contentValues) {
+
+        long insertedRecordId = mSQLiteDatabase.insert(tableName,
+                UserContract.User.COLUMN_NAME_NULLABLE, contentValues);
+
+        return insertedRecordId;
+    }
+
+    public Cursor queryAllRecords(String tableName) {
+
+        Cursor cursor = queryRecord(tableName, null, null, null, null, null, null);
+
+        return cursor;
+    }
+
+    public Cursor queryRecordBySelection(String tableName, String selection) {
+
+        Cursor cursor = queryRecord(tableName, null, selection, null, null, null, null);
+
+        return cursor;
+    }
+
+    private Cursor queryRecord(String table, String[] columns, String selection, String[] selectionArgs,
+                               String groupBy, String having, String orderBy) {
+
+        Cursor cursor = mSQLiteDatabase.query(table, columns, selection,
+                selectionArgs, groupBy, having, orderBy);
+
+        return cursor;
+    }
+
+    private void setSQLiteDatabase(SQLiteDatabase sqLiteDatabase) {
+        mSQLiteDatabase = sqLiteDatabase;
+    }
+
+}
