79b209a9e805288f7930bd4b70b043cce48277bc
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-20 22:27:49 -0600

    "getValuePaymentMethodFromId", "getValuePaymentMethodDescriptionFromId" and 'getId' methods are added to this class

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/utils/PaymentMethodDataUtilTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/utils/PaymentMethodDataUtilTest.java
@@ -1,6 +1,7 @@
 package io.dojogeek.adminibot.sqlite.utils;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -9,6 +10,7 @@ import io.dojogeek.adminibot.sqlite.UserContract;
 
 public class PaymentMethodDataUtilTest {
 
+    private static final int ADITIONAL_INDEX = 1;
     private SQLiteDatabase mSQLiteDatabase;
     private DatabaseOperationsUtilTest mDatabaseOperationsUtilTest;
     private static PaymentMethodDataUtilTest mPaymentMethodDataUtilTest;
@@ -23,6 +25,24 @@ public class PaymentMethodDataUtilTest {
         Cursor cursor = mDatabaseOperationsUtilTest.queryAllRecords(mSQLiteDatabase, PaymentMethodsContract.PaymentMethod.TABLE_NAME);
 
         return cursor;
+    }
+
+    public static String getValuePaymentMethodFromId(Context context, long id) {
+
+        int [] paymentMethods = PaymentMethodsContract.PAYMENT_METHODS;
+
+        return  context.getString(paymentMethods[getId(id)]);
+    }
+
+    public static String getValuePaymentMethodDescriptionFromId(Context context, long id) {
+
+        int [] paymentMethodsDescriptions = PaymentMethodsContract.PAYMENT_METHODS_DESCRIPTIONS;
+
+        return context.getString(paymentMethodsDescriptions[getId(id)]);
+    }
+
+    private static int getId(long value) {
+        return (int) value - ADITIONAL_INDEX;
     }
 
 }
