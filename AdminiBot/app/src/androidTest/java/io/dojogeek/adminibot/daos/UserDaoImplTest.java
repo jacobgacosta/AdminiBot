c005b1d7da195f0b5be6f8ce2a8faf8e23754745
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-21 17:02:37 -0600

    Test case for user creation is added

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/UserDaoImplTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/UserDaoImplTest.java
@@ -1,9 +1,43 @@
 package io.dojogeek.adminibot.daos;
 
+import android.content.Context;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import io.dojogeek.adminibot.models.UserModel;
+import io.dojogeek.adminibot.sqlite.utils.UserDataUtilTest;
+import io.dojogeek.adminibot.utiltest.CreatorModels;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class UserDaoImplTest {
+
+    private UserDao mUserDao;
+
+    @Before
+    public void setup() {
+        mUserDao = new UserDaoImpl(getTargetContext());
+        mUserDao.openConection();
+    }
+
+    @After
+    public void finishTest() {
+        mUserDao.closeConection();
+    }
+
+    @Test
+    public void userDao_createUser_isTrue() {
+
+        boolean isInserted = mUserDao.createUser(CreatorModels.createUserModel());
+
+        assertTrue(isInserted);
+    }
+
 }
