1d590676106d3dada7977ebbb4b1ba1e8807459f
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-21 16:23:19 -0600

    createUserModel method is added

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/utiltest/CreatorModels.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/utiltest/CreatorModels.java
@@ -4,5 +4,14 @@ import io.dojogeek.adminibot.models.UserModel;
 
 public class CreatorModels {
 
+    public static UserModel createUserModel() {
+
+        UserModel userModel = new UserModel();
+        userModel.name = "Jacob";
+        userModel.lastName = "Guzman";
+        userModel.email = "jgacosta@dojogeek.io";
+
+        return userModel;
+    }
 
 }
