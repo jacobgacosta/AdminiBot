087aecb8c13b85dc976b94af9abc357b4234e556
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-20 23:14:41 -0600

    "getExpenseTypeFromId", "getExpenseTypeDescriptionFromId" and "getId" methods are added to this class

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/utils/ExpenseTypeDataUtilTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/utils/ExpenseTypeDataUtilTest.java
@@ -1,5 +1,6 @@
 package io.dojogeek.adminibot.sqlite.utils;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -20,6 +21,24 @@ public class ExpenseTypeDataUtilTest {
         Cursor cursor = mDatabaseOperationsUtilTest.queryAllRecords(mSQLiteDatabase, ExpenseTypeContract.ExpenseType.TABLE_NAME);
 
         return cursor;
+    }
+
+    public static String getExpenseTypeFromId(Context context, long id) {
+
+        int [] expectedExpensesTypes = ExpenseTypeContract.EXPENSES_TYPES;
+
+        return context.getString(expectedExpensesTypes[getId(id)]);
+    }
+
+    public static String getExpenseTypeDescriptionFromId(Context context, long id) {
+
+        int [] expextedExpensesTypesDescriptions = ExpenseTypeContract.EXPENSES_TYPES_DESCRIPTIONS;
+
+        return context.getString(expextedExpensesTypesDescriptions[getId(id)]);
+    }
+
+    private static int getId(long value) {
+        return (int) value - ADITIONAL_INDEX;
     }
 
 }
