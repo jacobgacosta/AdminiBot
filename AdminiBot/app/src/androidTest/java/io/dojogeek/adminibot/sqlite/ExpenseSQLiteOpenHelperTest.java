683feba0a9d1415aa3d793da6fb371db5759381d
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-21 12:53:24 -0600

    ExpenseDataUtilTest is used to create a Expense model

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/ExpenseSQLiteOpenHelperTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/ExpenseSQLiteOpenHelperTest.java
@@ -73,7 +73,7 @@ public class ExpenseSQLiteOpenHelperTest {
     @Test
     public void sqliteHelper_correctInsertionData_isTrue() {
 
-        ExpenseModel expenseModel = createExpenseModel();
+        ExpenseModel expenseModel = mExpenseDataUtilTest.createExpenseModel();
 
         assertTrue(createRecord(expenseModel) > INSERT_ERROR);
     }
