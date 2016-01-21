08a78a943350a4aa2ab771536a242e44e56fb891
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-21 12:38:38 -0600

    createExpenseModel method is added to this class

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/utils/ExpenseDataUtilTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/utils/ExpenseDataUtilTest.java
@@ -2,6 +2,9 @@ package io.dojogeek.adminibot.sqlite.utils;
 
 import android.database.sqlite.SQLiteDatabase;
 
+import io.dojogeek.adminibot.models.ExpenseModel;
+import io.dojogeek.adminibot.utils.DateUtils;
+
 public class ExpenseDataUtilTest {
 
     private SQLiteDatabase mSQLiteDatabase;
@@ -10,6 +13,18 @@ public class ExpenseDataUtilTest {
     public ExpenseDataUtilTest(SQLiteDatabase sqliteDatabase) {
         mSQLiteDatabase = sqliteDatabase;
         mDatabaseOperationsUtilTest = new DatabaseOperationsUtilTest();
+    }
+
+    public ExpenseModel createExpenseModel() {
+
+        ExpenseModel expenseModel = new ExpenseModel();
+        expenseModel.totalAmount = "4566.90";
+        expenseModel.userId = 1;
+        expenseModel.expenseTypeId = 1;
+        expenseModel.dataExpediture = DateUtils.getCurrentData();
+        expenseModel.name = "expense test";
+
+        return expenseModel;
     }
 
 }
