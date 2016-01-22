5fa75def2ef404e8cd9ce765cf1e802274dd0270
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-22 13:36:40 -0600

    ExpenseTypeDaoImplTest class is created

---
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/ExpenseTypeDaoImplTest.java
@@ -0,0 +1,12 @@
+package io.dojogeek.adminibot.daos;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ExpenseTypeDaoImplTest {
+
+
+
+}
