7bb521406df8d29c0c531c591552fdbeb6be9a41
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-22 10:32:54 -0600

    createPaymentMethod is added

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/PaymentMethodsDao.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/PaymentMethodsDao.java
@@ -1,5 +1,7 @@
 package io.dojogeek.adminibot.daos;
 
+import android.content.ContentValues;
+
 import java.util.List;
 
 import io.dojogeek.adminibot.models.PaymentMethodModel;
@@ -7,5 +9,7 @@ import io.dojogeek.adminibot.models.PaymentMethodModel;
 public interface PaymentMethodsDao extends ConectionDao {
 
     List<PaymentMethodModel> getPaymentMethods();
+
+    public long createPaymentMethod(PaymentMethodModel paymentMethod);
 
 }
