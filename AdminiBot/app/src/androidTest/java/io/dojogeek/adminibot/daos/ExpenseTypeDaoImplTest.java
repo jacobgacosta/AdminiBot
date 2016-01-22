cb7e2c7f2f05552440a56b3ecabba1bd25083bf8
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-22 14:17:38 -0600

    Test case is added to validate initial inserted values of Expenses types

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/ExpenseTypeDaoImplTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/ExpenseTypeDaoImplTest.java
@@ -5,11 +5,21 @@ import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.List;
+
+import io.dojogeek.adminibot.models.ExpenseModel;
+import io.dojogeek.adminibot.models.ExpenseTypeModel;
 import io.dojogeek.adminibot.sqlite.AdminiBotSQLiteOpenHelper;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class ExpenseTypeDaoImplTest {
@@ -29,4 +39,16 @@ public class ExpenseTypeDaoImplTest {
         mExpenseTypeDao.closeConection();
     }
 
+    @Test
+    public void expenseTypeDao_numberInsertedExpensesTypes_isTrue() {
+
+        int numberOfinitialInsertedValues =  5;
+
+        List<ExpenseTypeModel> expenseTypeModelList =  mExpenseTypeDao.getExpensesTypes();
+
+        assertNotNull(expenseTypeModelList);
+        assertTrue(!expenseTypeModelList.isEmpty());
+        assertEquals(numberOfinitialInsertedValues, expenseTypeModelList.size());
+
+    }
 }
