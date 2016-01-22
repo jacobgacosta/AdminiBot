d1030232aee82f88e2ec1ba54777760c787983bc
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-22 15:31:27 -0600

    createExpenseType is added to DAO

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/ExpenseTypeDao.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/ExpenseTypeDao.java
@@ -6,6 +6,8 @@ import io.dojogeek.adminibot.models.ExpenseTypeModel;
 
 public interface ExpenseTypeDao extends ConectionDao {
 
+    long createExpenseType(ExpenseTypeModel expenseType);
+
     List<ExpenseTypeModel> getExpensesTypes();
 
     ExpenseTypeModel getExpenseTypeById(long id);
