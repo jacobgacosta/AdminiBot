8816f16b937554c63c081379cd3cd00d58c907f7
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-22 10:28:02 -0600

    "openConnection" is adde to setup method and test case is created to validate correct "payment method" creation

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/PaymentMethodsDaoImplTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/PaymentMethodsDaoImplTest.java
@@ -1,19 +1,35 @@
 package io.dojogeek.adminibot.daos;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.SmallTest;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import io.dojogeek.adminibot.models.PaymentMethodModel;
 import io.dojogeek.adminibot.sqlite.AdminiBotSQLiteOpenHelper;
+import io.dojogeek.adminibot.utiltest.CreatorModels;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(AndroidJUnit4.class)
 public class PaymentMethodsDaoImplTest {
 
+    private static final int NO_VALUE = 0;
+    private static final int OPERATIONAL_ERROR = -1;
     private PaymentMethodsDao mPaymentMethodsDao;
 
     @Before
@@ -21,6 +37,7 @@ public class PaymentMethodsDaoImplTest {
         Context context = getTargetContext();
         context.deleteDatabase(AdminiBotSQLiteOpenHelper.DATABASE_NAME);
         mPaymentMethodsDao = new PaymentMethodsDaoImpl(context);
+        mPaymentMethodsDao.openConection();
     }
 
     @After
@@ -28,4 +45,14 @@ public class PaymentMethodsDaoImplTest {
         mPaymentMethodsDao.closeConection();
     }
 
+    @Test
+    public void paymentMethodsDao_createPaymentMethod_isTrue() {
+
+        PaymentMethodModel paymentMethodModel = CreatorModels.createPaymentMethodModel();
+
+        long insertedRecordId = mPaymentMethodsDao.createPaymentMethod(paymentMethodModel);
+
+        assertTrue(insertedRecordId != OPERATIONAL_ERROR);
+        assertTrue(insertedRecordId > NO_VALUE);
+    }
 }
