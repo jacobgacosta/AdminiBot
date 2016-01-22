f1e22f01814318ff1dec6f39b59539bcd14bbad4
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-21 22:22:50 -0600

    "setup" and "finish" methods are added to load configurations before and after each test execution

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/PaymentMethodsDaoImplTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/PaymentMethodsDaoImplTest.java
@@ -1,12 +1,31 @@
 package io.dojogeek.adminibot.daos;
 
+import android.content.Context;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+
+import io.dojogeek.adminibot.sqlite.AdminiBotSQLiteOpenHelper;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
 
 @RunWith(AndroidJUnit4.class)
 public class PaymentMethodsDaoImplTest {
 
-    
+    private PaymentMethodsDao mPaymentMethodsDao;
+
+    @Before
+    public void setup() {
+        Context context = getTargetContext();
+        context.deleteDatabase(AdminiBotSQLiteOpenHelper.DATABASE_NAME);
+        mPaymentMethodsDao = new PaymentMethodsDaoImpl(context);
+    }
+
+    @After
+    public void finishTest() {
+        mPaymentMethodsDao.closeConection();
+    }
 
 }
