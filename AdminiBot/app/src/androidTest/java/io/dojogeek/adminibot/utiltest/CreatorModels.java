5b83a59145213ccb1f78f8874ddfc23ce7e356f4
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-22 10:32:01 -0600

    createPaymentMethodModel method is added to CreatorModels

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/utiltest/CreatorModels.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/utiltest/CreatorModels.java
@@ -1,5 +1,7 @@
 package io.dojogeek.adminibot.utiltest;
 
+import io.dojogeek.adminibot.R;
+import io.dojogeek.adminibot.models.PaymentMethodModel;
 import io.dojogeek.adminibot.models.UserModel;
 
 public class CreatorModels {
@@ -12,6 +14,15 @@ public class CreatorModels {
         userModel.email = "jgacosta@dojogeek.io";
 
         return userModel;
+    }
+
+    public static PaymentMethodModel createPaymentMethodModel() {
+
+        PaymentMethodModel paymentMethodModel = new PaymentMethodModel();
+        paymentMethodModel.name = R.string.payment_methods_credit_card;
+        paymentMethodModel.description = R.string.payment_methods_credit_card_description;
+
+        return paymentMethodModel;
     }
 
 }
