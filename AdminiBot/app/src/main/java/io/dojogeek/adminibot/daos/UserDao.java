9a38b4079a5eedc92500be800e71fed0370f0c83
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-21 22:01:45 -0600

    Signature method is changed to return List<UserModel>

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/UserDao.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/UserDao.java
@@ -1,11 +1,13 @@
 package io.dojogeek.adminibot.daos;
 
+import java.util.List;
+
 import io.dojogeek.adminibot.models.UserModel;
 
 public interface UserDao extends ConectionDao {
 
     boolean createUser(UserModel userModel);
 
-    UserModel getUser();
+    List<UserModel> getUsers();
 
 }
