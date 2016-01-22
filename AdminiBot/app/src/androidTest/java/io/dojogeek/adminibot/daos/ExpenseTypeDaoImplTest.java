ba8400b437b7190e4a4df9dc9f4dc28a1b2357d3
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-22 13:39:50 -0600

    @After and @Before configurations are added

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/ExpenseTypeDaoImplTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/ExpenseTypeDaoImplTest.java
@@ -1,12 +1,32 @@
 package io.dojogeek.adminibot.daos;
 
+import android.content.Context;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+
+import io.dojogeek.adminibot.sqlite.AdminiBotSQLiteOpenHelper;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
 
 @RunWith(AndroidJUnit4.class)
 public class ExpenseTypeDaoImplTest {
 
+    private ExpenseTypeDao mExpenseTypeDao;
 
+    @Before
+    public void setup() {
+        Context context = getTargetContext();
+        context.deleteDatabase(AdminiBotSQLiteOpenHelper.DATABASE_NAME);
+        mExpenseTypeDao = new ExpenseTypeDaoImpl(context);
+        mExpenseTypeDao.openConection();
+    }
+
+    @After
+    public void finishTest() {
+        mExpenseTypeDao.closeConection();
+    }
 
 }
