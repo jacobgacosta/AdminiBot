8c628085ffb48fd4400dc518d5b806b5d538e331
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-01-22 17:29:17 -0600

    ExpenseDaoImplTest class is created

---
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/ExpenseDaoImplTest.java
@@ -0,0 +1,17 @@
+package io.dojogeek.adminibot.daos;
+
+import android.content.Context;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+
+@RunWith(AndroidJUnit4.class)
+public class ExpenseDaoImplTest {
+
+
+
+}
