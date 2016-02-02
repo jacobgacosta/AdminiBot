35f5c58d296d4796882d6d7b9df07a0f7ef97a67
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-02 13:53:42 -0600

    Create Payment Method model

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/models/PaymentMethodModel.java
@@ -0,0 +1,12 @@
+package io.dojogeek.adminibot.models;
+
+public class PaymentMethodModel {
+
+    public long id;
+    public String name;
+    public String reference;
+    public long typePaymentMethod;
+    public double availabreCredit;
+    public long userId;
+
+}
