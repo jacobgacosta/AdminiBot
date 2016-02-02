1b2b1a98c33dd53b5741c802a312a8075bc69aa0
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-01 20:20:23 -0600

    Add configuration methods to use data base operations

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/PaymentMethodsSQLiteOpenHelperTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/PaymentMethodsSQLiteOpenHelperTest.java
@@ -1,12 +1,37 @@
 package io.dojogeek.adminibot.sqlite;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
+
+import io.dojogeek.adminibot.sqlite.utils.DataBaseConfigurationTest;
+import io.dojogeek.adminibot.sqlite.utils.PaymentMethodDataUtilTest;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
 
 @RunWith(AndroidJUnit4.class)
 public class PaymentMethodsSQLiteOpenHelperTest {
 
-    
+    private SQLiteDatabase mSQLiteDatabase;
+    private DataBaseConfigurationTest mDataBaseConfigurationTest;
+    private PaymentMethodDataUtilTest mPaymentMethodDataUtilTest;
+
+    @Before
+    public void setup() {
+
+        mDataBaseConfigurationTest = DataBaseConfigurationTest.getInstance(getTargetContext());
+        mDataBaseConfigurationTest.prepareDataBase();
+
+        mSQLiteDatabase = mDataBaseConfigurationTest.getSQLiteDatabase();
+
+        loadPaymentMethodUtil();
+
+    }
+
+    private void loadPaymentMethodUtil() {
+        mPaymentMethodDataUtilTest = new PaymentMethodDataUtilTest(mSQLiteDatabase);
+    }
 
 }
