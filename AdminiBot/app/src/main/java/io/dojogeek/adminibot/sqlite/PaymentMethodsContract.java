cd78adf07bbc20c177a01a3df64f59d249119621
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-02 13:56:21 -0600

    Fix SQL structure and table name

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/sqlite/PaymentMethodsContract.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/sqlite/PaymentMethodsContract.java
@@ -8,7 +8,7 @@ public class PaymentMethodsContract {
 
     public static abstract class PaymentMethods implements BaseColumns {
 
-        public static final String TABLE_NAME = "chingatureputamadremalnacido";
+        public static final String TABLE_NAME = "payment_methods";
         public static final String COLUMN_NAME = "name";
         public static final String COLUMN_REFERENCE = "reference";
         public static final String COLUMN_TYPE_PAYMENT_METHOD_ID = "type_payment_method_id";
@@ -21,10 +21,10 @@ public class PaymentMethodsContract {
     public static final String SQL_CREATE_TABLE = "CREATE TABLE " + PaymentMethods.TABLE_NAME + "(" +
             PaymentMethods._ID  + " INTEGER PRIMARY KEY, " +
             PaymentMethods.COLUMN_NAME + " TEXT NOT NULL, " +
-            PaymentMethods.COLUMN_REFERENCE + " TEST NOT NULL, " +
+            PaymentMethods.COLUMN_REFERENCE + " TEXT NOT NULL, " +
             PaymentMethods.COLUMN_TYPE_PAYMENT_METHOD_ID + " INTEGER NOT NULL, " +
-            PaymentMethods.COLUMN_AVAILABLE_CREDIT + "REAL NOT NULL, " +
-            PaymentMethods.COLUMN_USER_ID + "INTEGER NOT NULL)";
+            PaymentMethods.COLUMN_AVAILABLE_CREDIT + " REAL NOT NULL, " +
+            PaymentMethods.COLUMN_USER_ID + " INTEGER NOT NULL)";
 
     public static final String SQL_DELETE_ENTRIES = DROP_TABLE_IF_EXIST + PaymentMethods.TABLE_NAME;
 
