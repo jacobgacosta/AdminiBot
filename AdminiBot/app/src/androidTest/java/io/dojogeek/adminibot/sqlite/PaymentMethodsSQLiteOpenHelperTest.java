fe13bb38d59f734c953f93d502eb2d827e34632e
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-01 21:03:04 -0600

    Add test case to verify successful creation of the table "Payment Methods"

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/PaymentMethodsSQLiteOpenHelperTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/PaymentMethodsSQLiteOpenHelperTest.java
@@ -1,19 +1,23 @@
 package io.dojogeek.adminibot.sqlite;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import io.dojogeek.adminibot.sqlite.utils.DataBaseConfigurationTest;
 import io.dojogeek.adminibot.sqlite.utils.PaymentMethodDataUtilTest;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class PaymentMethodsSQLiteOpenHelperTest {
 
+    private static final int NONE_TABLE_CREATED = 0;
     private SQLiteDatabase mSQLiteDatabase;
     private DataBaseConfigurationTest mDataBaseConfigurationTest;
     private PaymentMethodDataUtilTest mPaymentMethodDataUtilTest;
@@ -27,6 +31,20 @@ public class PaymentMethodsSQLiteOpenHelperTest {
         mSQLiteDatabase = mDataBaseConfigurationTest.getSQLiteDatabase();
 
         loadPaymentMethodUtil();
+
+    }
+
+    @Test
+    public void sqliteHelper_creationTablePaymentMethods_isTrue() {
+
+        Cursor cursor = mSQLiteDatabase.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = '" +
+                PaymentMethodsContract.PaymentMethods.TABLE_NAME + "'", null);
+
+        if (cursor != null) {
+
+            assertTrue(cursor.getCount() > NONE_TABLE_CREATED);
+
+        }
 
     }
 
