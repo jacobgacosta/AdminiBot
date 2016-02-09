ab91511c3eb275e5be8fbc21f4a4503eb80097ca
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-09 15:41:49 -0600

    Rename class

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/ExpensesPaymentMethodsOpenHelperTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/ExpensesPaymentMethodsOpenHelperTest.java
@@ -1,20 +1,27 @@
 package io.dojogeek.adminibot.sqlite;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import io.dojogeek.adminibot.utiltest.sqlite.DataBaseConfigurationTest;
 import io.dojogeek.adminibot.utiltest.sqlite.ExpenseDataUtilTest;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
-public class ExpensesPaymentMethodsContractTest {
+public class ExpensesPaymentMethodsOpenHelperTest {
+
+    private static final int NONE_TABLE_CREATED = 0;
 
     private Context mContext;
     private SQLiteDatabase mSQLiteDatabase;
@@ -36,6 +43,16 @@ public class ExpensesPaymentMethodsContractTest {
     @After
     public void tearDown() throws Exception {
         mDataBaseConfigurationTest.closeDataBaseConnection();
+    }
+
+    @Test
+    public void sqliteHelper_correctCreatedTable_isTrue() {
+
+        Cursor cursor = mSQLiteDatabase.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = '" +
+                ExpensesPaymentMethodsContract.ExpensesPaymentMethods.TABLE_NAME + "'", null);
+
+        assertNotNull(cursor);
+        assertTrue(cursor.getCount() > NONE_TABLE_CREATED);
     }
 
 }
