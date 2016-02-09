87fb7ff6e50d94c20640049cdde05333dab30695
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-09 12:39:14 -0600

    Will add new fields to "Expense" and SQL structure is changed

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/sqlite/ExpenseContract.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/sqlite/ExpenseContract.java
@@ -17,22 +17,25 @@ public class ExpenseContract {
     public static abstract class Expense implements BaseColumns {
 
         public static final String TABLE_NAME = "expenses";
-        public static final String COLUMN_NAME = "name";
-        public static final String COLUMN_USER_ID = "user_id";
-        public static final String COLUMN_EXPENSES_TYPE_ID = "expenses_type_id";
+        public static final String COLUMN_DESCRIPTION = "description";
         public static final String COLUMN_AMOUNT = "amount";
         public static final String COLUMN_DATE_EXPEDITURE = "date_expediture";
+        public static final String COLUMN_NEXT_EXIT = "next_exit";
+        public static final String COLUMN_EXPENSES_TYPE_ID = "expenses_type_id";
+        public static final String COLUMN_USER_ID = "user_id";
         public static final String COLUMN_NAME_NULLABLE = "null";
 
     }
 
-    public static final String SQL_CREATE_ENTRIES = CREATE_TABLE + Expense.TABLE_NAME + OPEN_PARENTHESIS +
-            Expense._ID + INTEGER + PRIMARY_KEY + COMMA_SEP +
-            Expense.COLUMN_NAME + TEXT_TYPE + COMMA_SEP +
-            Expense.COLUMN_EXPENSES_TYPE_ID + INTEGER + COMMA_SEP +
-            Expense.COLUMN_USER_ID + INTEGER + COMMA_SEP +
-            Expense.COLUMN_AMOUNT + DOUBLE  + COMMA_SEP +
-            Expense.COLUMN_DATE_EXPEDITURE + TEXT_TYPE + CLOSE_PARENTHESIS;
+    public static final String SQL_CREATE_ENTRIES = "CREATE TABLE " +  Expense.TABLE_NAME +
+            "(" + Expense._ID + " INTEGER PRIMARY KEY, " +
+            Expense.COLUMN_DESCRIPTION + " TEXT NOT NULL, " +
+            Expense.COLUMN_AMOUNT + " REAL NOT NULL, " +
+            Expense.COLUMN_DATE_EXPEDITURE + " TEXT NOT NULL, " +
+            Expense.COLUMN_NEXT_EXIT + " TEXT NOT NULL, " +
+            Expense.COLUMN_EXPENSES_TYPE_ID + " INTEGER NOT NULL, " +
+            Expense.COLUMN_USER_ID + " INTEGER NOT NULL)";
+
 
     public static final String SQL_DELETE_ENTRIES = DROP_TABLE_IF_EXIST + Expense.TABLE_NAME;
 
