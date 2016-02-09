635a7ad4e6ca394cc5d6537d143e68a206685f8c
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-09 15:46:47 -0600

    Rename reference to old "ExpensesPaymentMethodsOpenHelperTest.class"

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/AdminiBotSQLiteOpenHelperSuite.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/AdminiBotSQLiteOpenHelperSuite.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ExpenseTypeSQLiteOpenHelperTest.class, TypesPaymentMethodsSQLiteOpenHelperTest.class,
-        ExpenseSQLiteOpenHelperTest.class, UserSQLiteOpenHelperTest.class, PaymentMethodsSQLiteOpenHelperTest.class})
+        ExpenseSQLiteOpenHelperTest.class, UserSQLiteOpenHelperTest.class,
+        PaymentMethodsSQLiteOpenHelperTest.class, ExpensesPaymentMethodsOpenHelperTest.class})
 public class AdminiBotSQLiteOpenHelperSuite {
 }
