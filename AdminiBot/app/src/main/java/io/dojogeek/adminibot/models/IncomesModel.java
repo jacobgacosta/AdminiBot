c1e4ebe8409b3bed3271159a2194df7f6f03d862
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-09 18:08:09 -0600

    Create IncomesModel class

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/models/IncomesModel.java
@@ -0,0 +1,12 @@
+package io.dojogeek.adminibot.models;
+
+public class IncomesModel {
+
+    public long id;
+    public String description;
+    public double amount;
+    public String date;
+    public String nextDate;
+    public long userId;
+
+}
