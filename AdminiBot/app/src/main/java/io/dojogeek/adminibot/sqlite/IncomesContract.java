a7f477c791014b0aececc7b9bd1cdbbffbea7bcc
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-09 18:06:50 -0600

    Add SQL sentence for creation of Incomes table

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/sqlite/IncomesContract.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/sqlite/IncomesContract.java
@@ -2,12 +2,15 @@ package io.dojogeek.adminibot.sqlite;
 
 import android.provider.BaseColumns;
 
+import static io.dojogeek.adminibot.sqlite.SQLiteConstants.DROP_TABLE_IF_EXIST;
+
 public class IncomesContract {
 
 
     public static abstract class Incomes implements BaseColumns {
 
-        public static final String TABLE_DESCRIPTION = "description";
+        public static final String TABLE_NAME = "incomes";
+        public static final String COLUMN_DESCRIPTION = "description";
         public static final String COLUMN_AMOUNT = "amount";
         public static final String COLUMN_DATE = "receipt_date";
         public static final String COLUMN_NEXT_ENTRY = "next_entry";
@@ -15,5 +18,15 @@ public class IncomesContract {
         public static final String COLUMN_NAME_NULLABLE = "null";
 
     }
+
+    public static final String SQL_CREATE_TABLE = "CREATE TABLE " +  Incomes.TABLE_NAME +
+            "(" + Incomes._ID + " INTEGER PRIMARY KEY, " +
+            Incomes.COLUMN_DESCRIPTION + " TEXT NOT NULL, " +
+            Incomes.COLUMN_AMOUNT + " REAL NOT NULL, " +
+            Incomes.COLUMN_DATE + " TEXT NOT NULL, " +
+            Incomes.COLUMN_NEXT_ENTRY + " TEXT NOT NULL, " +
+            Incomes.COLUMN_USER_ID + " INTEGER NOT NULL)";
+
+    public static final String SQL_DELETE_ENTRIES = DROP_TABLE_IF_EXIST + Incomes.TABLE_NAME;
 
 }
