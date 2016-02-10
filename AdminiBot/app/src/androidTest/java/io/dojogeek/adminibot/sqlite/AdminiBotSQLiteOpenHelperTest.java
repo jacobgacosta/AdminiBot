6c0144eeff65b94ce41317d944cbce9385893c61
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-10 17:43:52 -0600

    Create AdminiBotSQLiteOpenHelperTest class

---
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/AdminiBotSQLiteOpenHelperTest.java
@@ -0,0 +1,19 @@
+package io.dojogeek.adminibot.sqlite;
+
+import android.content.Context;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class AdminiBotSQLiteOpenHelperTest {
+
+
+
+}
