2fdb4d008e41ff28b0b4f76ee79bc2cc2a9e2129
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-11 11:58:25 -0600

    Add CardTypeContract table to creation test

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/AdminiBotSQLiteOpenHelperTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/AdminiBotSQLiteOpenHelperTest.java
@@ -75,7 +75,8 @@ public class AdminiBotSQLiteOpenHelperTest {
                 PaymentMethodsContract.PaymentMethods.TABLE_NAME,
                 TypesPaymentMethodsContract.TypePaymentMethod.TABLE_NAME, UsersContract.User.TABLE_NAME,
                 IncomesPaymentMethodsContract.IncomesPaymentMethods.TABLE_NAME, BankCardsContract.BankCard.TABLE_NAME,
-                IncomesBankCardsContract.IncomesBankCards.TABLE_NAME, ExpensesBankCardsContract.BankCard.TABLE_NAME};
+                IncomesBankCardsContract.IncomesBankCards.TABLE_NAME, ExpensesBankCardsContract.BankCard.TABLE_NAME,
+                CardTypeContract.CardType.TABLE_NAME};
 
         for (String table : tables) {
             Cursor cursor = selectTable(table);
