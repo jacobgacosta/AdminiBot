1b48469a8539547d39c5a2e1dbf96828baea6fd3
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-10 21:38:54 -0600

    Add test case to verify initial inserted values for Expenses types

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/ExpenseTypesSQLiteOpenHelperInsertionTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/ExpenseTypesSQLiteOpenHelperInsertionTest.java
@@ -17,5 +17,48 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExpenseTypesSQLiteOpenHelperInsertionTest {
 
-    
+    private static final int INSERT_ERROR = -1;
+    private static final int NONE_TABLE_CREATED = 0;
+    private Context mContext;
+    private AdminiBotSQLiteOpenHelper mAdminiBotSQLiteOpenHelper;
+    private SQLiteDatabase mSQLiteDatabase;
+
+    @Before
+    public void setup() {
+
+        mContext = getTargetContext();
+
+        loadSQLiteHelper();
+
+        mSQLiteDatabase = openDataBaseConnection();
+    }
+
+    @After
+    public void tearDown() {
+        closeDataBaseConnection();
+    }
+
+    @Test
+    public void sqliteHelper_numberOfInsertedExpensesTypes_isEquals() {
+
+        final int insertedInitialValues = 5;
+
+        Cursor cursor = mSQLiteDatabase.rawQuery("SELECT * FROM " + ExpenseTypesContract.ExpenseType.TABLE_NAME,
+                null);
+
+        assertEquals(insertedInitialValues, cursor.getCount());
+
+    }
+
+    private void loadSQLiteHelper() {
+        mAdminiBotSQLiteOpenHelper = AdminiBotSQLiteOpenHelper.getInstance(mContext);
+    }
+
+    public void closeDataBaseConnection() {
+        mAdminiBotSQLiteOpenHelper.close();
+    }
+
+    public SQLiteDatabase openDataBaseConnection() {
+        return mAdminiBotSQLiteOpenHelper.getReadableDatabase();
+    }
 }
