605566c18efcba40c4fea8c3bf9b29ade8500032
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-11 08:23:02 -0600

    Remove unused static imports

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/sqlite/ExpensesContract.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/sqlite/ExpensesContract.java
@@ -2,14 +2,6 @@ package io.dojogeek.adminibot.sqlite;
 
 import android.provider.BaseColumns;
 
-import static io.dojogeek.adminibot.sqlite.SQLiteConstants.COMMA_SEP;
-import static io.dojogeek.adminibot.sqlite.SQLiteConstants.DOUBLE;
-import static io.dojogeek.adminibot.sqlite.SQLiteConstants.TEXT_TYPE;
-import static io.dojogeek.adminibot.sqlite.SQLiteConstants.PRIMARY_KEY;
-import static io.dojogeek.adminibot.sqlite.SQLiteConstants.INTEGER;
-import static io.dojogeek.adminibot.sqlite.SQLiteConstants.CREATE_TABLE;
-import static io.dojogeek.adminibot.sqlite.SQLiteConstants.OPEN_PARENTHESIS;
-import static io.dojogeek.adminibot.sqlite.SQLiteConstants.CLOSE_PARENTHESIS;
 import static io.dojogeek.adminibot.sqlite.SQLiteConstants.DROP_TABLE_IF_EXIST;
 
 public class ExpensesContract {
