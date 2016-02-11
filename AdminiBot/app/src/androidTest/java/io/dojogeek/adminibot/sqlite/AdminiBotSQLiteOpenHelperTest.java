f99374f6071e470ae2d736207ba63162d0aab910
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-10 20:45:18 -0600

    Add database configurations

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/AdminiBotSQLiteOpenHelperTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/AdminiBotSQLiteOpenHelperTest.java
@@ -1,13 +1,18 @@
 package io.dojogeek.adminibot.sqlite;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.File;
+
+import io.dojogeek.adminibot.utiltest.sqlite.DataBaseConfigurationTest;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
 
@@ -16,15 +21,25 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class AdminiBotSQLiteOpenHelperTest {
 
-    private AdminiBotSQLiteOpenHelper mAdminiBotSQLiteOpenHelper;
+    private static final int INSERT_ERROR = -1;
+    private static final int NONE_TABLE_CREATED = 0;
     private Context mContext;
+    private AdminiBotSQLiteOpenHelper mAdminiBotSQLiteOpenHelper;
+    private SQLiteDatabase mSQLiteDatabase;
 
     @Before
     public void setup() {
 
         mContext = getTargetContext();
-        mAdminiBotSQLiteOpenHelper = AdminiBotSQLiteOpenHelper.getInstance(mContext);
 
+        loadSQLiteHelper();
+
+        mSQLiteDatabase = openDataBaseConnection();
+    }
+
+    @After
+    public void tearDown() {
+        closeDataBaseConnection();
     }
 
     @Test
