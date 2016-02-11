55a8030e323cde7b7d1bb9848abb526f1140c21a
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-10 21:36:48 -0600

    Rename AdminiBotSQLiteOpenHelperInsertionTest class to ExpenseTypesSQLiteOpenHelperInsertionTest

---
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/ExpenseTypesSQLiteOpenHelperInsertionTest.java
@@ -0,0 +1,21 @@
+package io.dojogeek.adminibot.sqlite;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class ExpenseTypesSQLiteOpenHelperInsertionTest {
+
+    
+}
