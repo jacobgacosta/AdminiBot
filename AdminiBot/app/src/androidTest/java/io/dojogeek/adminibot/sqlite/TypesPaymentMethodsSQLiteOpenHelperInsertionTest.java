b808b4deedd41d74e1d00b54c0c96429391a64c0
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-10 21:59:02 -0600

    Add test case to validate initial insertion of "types payment methods"

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/TypesPaymentMethodsSQLiteOpenHelperInsertionTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/TypesPaymentMethodsSQLiteOpenHelperInsertionTest.java
@@ -17,6 +17,47 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class TypesPaymentMethodsSQLiteOpenHelperInsertionTest {
 
-    
+    private Context mContext;
+    private AdminiBotSQLiteOpenHelper mAdminiBotSQLiteOpenHelper;
+    private SQLiteDatabase mSQLiteDatabase;
+
+    @Before
+    public void setup() {
+
+        mContext = getTargetContext();
+
+        loadSQLiteHelper();
+
+        mSQLiteDatabase = openDataBaseConnection();
+    }
+
+    @After
+    public void tearDown() {
+        closeDataBaseConnection();
+    }
+
+    @Test
+    public void sqliteHelper_numberOfInsertedTypesPaymentMethods_isEquals() {
+
+        final int initialValuesInserted = 3;
+
+        Cursor cursor = mSQLiteDatabase.rawQuery("SELECT * FROM " + TypesPaymentMethodsContract.TypePaymentMethod.TABLE_NAME,
+                null);
+
+        assertEquals(cursor.getCount(), initialValuesInserted);
+
+    }
+
+    private void loadSQLiteHelper() {
+        mAdminiBotSQLiteOpenHelper = AdminiBotSQLiteOpenHelper.getInstance(mContext);
+    }
+
+    public void closeDataBaseConnection() {
+        mAdminiBotSQLiteOpenHelper.close();
+    }
+
+    public SQLiteDatabase openDataBaseConnection() {
+        return mAdminiBotSQLiteOpenHelper.getReadableDatabase();
+    }
 
 }
