43bb254068808f5e39a1164bad40ba5ed7d33557
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-13 09:44:05 -0600

    Add test case to validate expense creation

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/ExpenseDaoImplTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/ExpenseDaoImplTest.java
@@ -5,13 +5,20 @@ import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import io.dojogeek.adminibot.models.ExpenseModel;
+import io.dojogeek.adminibot.utiltest.CreatorModels;
+
 import static android.support.test.InstrumentationRegistry.getTargetContext;
+
+import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class ExpenseDaoImplTest {
 
+    private static final int OPERATIONAL_ERROR = -1;
     private ExpenseDao mExpenseDao;
 
     @Before
@@ -24,6 +31,17 @@ public class ExpenseDaoImplTest {
     @After
     public void finishTest() {
         mExpenseDao.closeConection();
+    }
+
+    @Test
+    public void expenseDao_creationExpense_isTrue() {
+
+        ExpenseModel expenseModel = CreatorModels.createExpenseModel();
+
+        long insertedRecordId = mExpenseDao.createExpense(expenseModel);
+
+        assertNotEquals(OPERATIONAL_ERROR, insertedRecordId);
+
     }
 
 }
