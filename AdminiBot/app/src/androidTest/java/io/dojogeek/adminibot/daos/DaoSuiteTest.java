3b9f16ee61ee5fa0d3c61fb4aa6d9c6cd4f49a63
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-13 09:30:05 -0600

    Add deleteDataBase method to suite

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/DaoSuiteTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/DaoSuiteTest.java
@@ -1,10 +1,25 @@
 package io.dojogeek.adminibot.daos;
 
+import android.content.Context;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import io.dojogeek.adminibot.sqlite.AdminiBotSQLiteOpenHelper;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+
 @RunWith(Suite.class)
-@Suite.SuiteClasses({UserDaoImplTest.class, TypesTypesPaymentMethodsDaoImplTest.class,
-        ExpenseTypeDaoImplTest.class, ExpenseDaoImplTest.class})
+@Suite.SuiteClasses({UserDaoImplTest.class, ExpenseTypeDaoImplTest.class})
 public class DaoSuiteTest {
+
+    @AfterClass
+    public void deleteDataBase() {
+
+        Context context = getTargetContext();
+        context.deleteDatabase(AdminiBotSQLiteOpenHelper.DATABASE_NAME);
+
+    }
+
 }
