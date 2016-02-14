272afd17ef608094f88326d5798dbc68cfda0e52
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-13 20:25:27 -0600

    Change implementations of test cases

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/ExpenseTypeDaoImplTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/ExpenseTypeDaoImplTest.java
@@ -9,9 +9,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import io.dojogeek.adminibot.R;
+import io.dojogeek.adminibot.models.ExpenseModel;
 import io.dojogeek.adminibot.models.ExpenseTypeModel;
 import io.dojogeek.adminibot.sqlite.AdminiBotSQLiteOpenHelper;
 import io.dojogeek.adminibot.sqlite.ExpensesTypesContract;
@@ -30,20 +32,21 @@ public class ExpenseTypeDaoImplTest {
 
     private static final int NO_VALUE = 0;
     private static final int OPERATIONAL_ERROR = -1;
-    private static final int SUCCESS_CODE = 1;
     private ExpenseTypeDao mExpenseTypeDao;
+    private Context mContext;
 
     @Before
     public void setup() {
-        Context context = getTargetContext();
+        mContext = getTargetContext();
 
-        mExpenseTypeDao = new ExpenseTypeDaoImpl(context);
+        mExpenseTypeDao = new ExpenseTypeDaoImpl(mContext);
         mExpenseTypeDao.openConection();
     }
 
     @After
     public void finishTest() {
         mExpenseTypeDao.closeConection();
+        mContext.deleteDatabase(AdminiBotSQLiteOpenHelper.DATABASE_NAME);
     }
 
     @Test
@@ -57,23 +60,7 @@ public class ExpenseTypeDaoImplTest {
     }
 
     @Test
-    public void expenseTypeDao_creationAndUpdatingExpenseType_isTrue() {
-
-        ExpenseTypeModel expenseTypeModel = CreatorModels.createExpenseTypeModel();
-
-        long insertedExpenseTypeId = mExpenseTypeDao.createExpenseType(expenseTypeModel);
-
-        ExpenseTypeModel newExpenseTypeModel = changeExpenseTypeValues(expenseTypeModel);
-
-        String where = ExpensesTypesContract.ExpenseType._ID + "= " + insertedExpenseTypeId;
-
-        long rowsUpdated = mExpenseTypeDao.updateExpensetype(newExpenseTypeModel, where);
-
-        assertNotEquals(NO_VALUE, rowsUpdated);
-    }
-
-    @Test
-    public void expenseTypeDao_creationAndObtainingExpenseType_isTrue() {
+    public void expenseTypeDao_creationAndObtainingExpenseTypeById_isTrue() {
 
         ExpenseTypeModel expenseTypeModel = CreatorModels.createExpenseTypeModel();
 
@@ -87,6 +74,52 @@ public class ExpenseTypeDaoImplTest {
 
     }
 
+    @Test
+    public void expenseTypeDao_obtainingAllExpensesTypesInitiallyInserted_isTrue() {
+
+        int initiallyInsertedExpensesTypes = 5;
+
+        List<ExpenseTypeModel> actualExpenseTypeModelList = mExpenseTypeDao.getExpensesTypes();
+
+        assertNotNull(actualExpenseTypeModelList);
+        assertTrue(!actualExpenseTypeModelList.isEmpty());
+        assertEquals(initiallyInsertedExpensesTypes, actualExpenseTypeModelList.size());
+
+        compareExpensesModelsInitiallyInserted(actualExpenseTypeModelList);
+    }
+
+    @Test
+    public void expenseTypeDao_creationUpdatingAndObtainingExpenseTypeById_isTrue() {
+
+        ExpenseTypeModel expenseTypeModel = CreatorModels.createExpenseTypeModel();
+
+        long insertedExpenseTypeId = mExpenseTypeDao.createExpenseType(expenseTypeModel);
+
+        ExpenseTypeModel expectedNewExpenseTypeModel = changeExpenseTypeValues(expenseTypeModel);
+
+        String where = ExpensesTypesContract.ExpenseType._ID + "= " + insertedExpenseTypeId;
+
+        long rowsUpdated = mExpenseTypeDao.updateExpensetype(expectedNewExpenseTypeModel, where);
+
+        assertNotEquals(NO_VALUE, rowsUpdated);
+
+        ExpenseTypeModel actualExpenseTypeModel = mExpenseTypeDao.getExpenseTypeById(insertedExpenseTypeId);
+
+        compareExpensesModels(expectedNewExpenseTypeModel, actualExpenseTypeModel);
+    }
+
+    @Test
+    public void expenseTypeDao_deletionOfExpenseTypeInitiallyInserted_isTrue() {
+
+        long initiallyInsertedExpenseTypeId = 3;
+
+        long removedRows = mExpenseTypeDao.removeExpenseTypeById(initiallyInsertedExpenseTypeId);
+
+        assertEquals(1, removedRows);
+
+    }
+
+
     private ExpenseTypeModel changeExpenseTypeValues(ExpenseTypeModel expenseTypeModel) {
 
         expenseTypeModel.name = R.string.expenses_types_luxuries;
@@ -94,4 +127,28 @@ public class ExpenseTypeDaoImplTest {
 
         return expenseTypeModel;
     }
+
+    private void compareExpensesModels(ExpenseTypeModel expectedExpenseTypeModel,
+                                       ExpenseTypeModel actualExpenseTypeModel) {
+
+        assertEquals(expectedExpenseTypeModel.name, actualExpenseTypeModel.name);
+        assertEquals(expectedExpenseTypeModel.description, actualExpenseTypeModel.description);
+
+    }
+
+    private void compareExpensesModelsInitiallyInserted(List<ExpenseTypeModel> actualExpenseTypeModelList) {
+
+        for (int index = 0; index < actualExpenseTypeModelList.size(); index++) {
+            ExpenseTypeModel actualExpenseTypeModel = actualExpenseTypeModelList.get(index);
+
+            String expectedName = mContext.getString(ExpensesTypesContract.EXPENSES_TYPES[index]);
+            String expectedDescription = mContext.getString(ExpensesTypesContract.EXPENSES_TYPES_DESCRIPTIONS[index]);
+
+            assertEquals(expectedName, mContext.getString(actualExpenseTypeModel.name));
+            assertEquals(expectedDescription, mContext.getString(actualExpenseTypeModel.description));
+
+        }
+
+    }
+
 }
