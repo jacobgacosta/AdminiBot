135d1158c96a3416c8556955c12a854473f46376
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-14 22:16:58 -0600

    Create BankCardModel class

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/models/BankCardModel.java
@@ -0,0 +1,14 @@
+package io.dojogeek.adminibot.models;
+
+public class BankCardModel {
+
+    public long id;
+    public String name;
+    public String number;
+    public long bankId;
+    public long trademarkId;
+    public double availableCredit;
+    public long cardTypeId;
+    public long userId;
+
+}
