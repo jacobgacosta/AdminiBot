ad0e3ab83ae725a68276210732f360b0bead311e
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-16 11:39:43 -0600

    Create CardDetailModel class

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/models/CardDetailModel.java
@@ -0,0 +1,12 @@
+package io.dojogeek.adminibot.models;
+
+public class CardDetailModel {
+
+    public long id;
+    public double creditLimit;
+    public double currentBalance;
+    public String cuttoffDate;
+    public String payDayLimit;
+    public long bankCardId;
+
+}
