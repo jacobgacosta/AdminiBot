614ba289f57b0fbc75c9478066a0427bafdebb73
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-16 16:07:59 -0600

    Change "card type" to enum

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/models/BankCardModel.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/models/BankCardModel.java
@@ -1,5 +1,7 @@
 package io.dojogeek.adminibot.models;
 
+import io.dojogeek.adminibot.enums.CardTypeEnum;
+
 public class BankCardModel {
 
     public long id;
@@ -8,7 +10,7 @@ public class BankCardModel {
     public long bankId;
     public long trademarkId;
     public double availableCredit;
-    public long cardTypeId;
+    public CardTypeEnum cardType;
     public long userId;
 
 }
