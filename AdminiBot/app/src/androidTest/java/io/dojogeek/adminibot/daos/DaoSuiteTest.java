64464427b41d516f7f26cc4e0cf89b59f459d244
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-16 11:43:01 -0600

    Add CardDetailDaoImplTest class to suite

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/DaoSuiteTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/DaoSuiteTest.java
@@ -12,7 +12,7 @@ import static android.support.test.InstrumentationRegistry.getTargetContext;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({UserDaoImplTest.class, ExpenseTypeDaoImplTest.class, ExpenseDaoImplTest.class,
-        IncomeDaoImplTest.class, BankCardDaoImplTest.class})
+        IncomeDaoImplTest.class, BankCardDaoImplTest.class, CardDetailDaoImplTest.class})
 public class DaoSuiteTest {
 
 }
