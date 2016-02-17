f9193b6432dd5e28a6f4cedbae4093ac914c2f8a
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-17 12:08:04 -0600

    Create OtherPaymentMethodsDaoImplTest class

---
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/OtherPaymentMethodsDaoImplTest.java
@@ -0,0 +1,37 @@
+package io.dojogeek.adminibot.daos;
+
+import android.content.Context;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import io.dojogeek.adminibot.sqlite.AdminiBotSQLiteOpenHelper;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+
+@RunWith(AndroidJUnit4.class)
+public class OtherPaymentMethodsDaoImplTest {
+
+    private static final int SUCCESS_OPERATION = 1;
+    private static final int OPERATIONAL_ERROR = -1;
+    private static final int NO_OPERATION = 0;
+
+    private Context mContext;
+    private OtherPaymentMethodDao mOtherPaymentMethodDao;
+
+    @Before
+    public void setup() {
+        mContext = getTargetContext();
+        mOtherPaymentMethodDao = new OtherPaymentMethodDaoImpl(mContext);
+        mOtherPaymentMethodDao.openConection();
+    }
+
+    @After
+    public void finishTest() {
+        mOtherPaymentMethodDao.closeConection();
+        mContext.deleteDatabase(AdminiBotSQLiteOpenHelper.DATABASE_NAME);
+    }
+
+}
