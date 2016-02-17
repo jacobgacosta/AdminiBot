62679b0b0322f266ca9ef16056d00fd8cff8fffe
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-17 12:10:52 -0600

    Rename OtherPaymentMethodsDaoImplTest class

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/DaoSuiteTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/DaoSuiteTest.java
@@ -1,19 +1,12 @@
 package io.dojogeek.adminibot.daos;
 
-import android.content.Context;
-
-import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-
-import io.dojogeek.adminibot.sqlite.AdminiBotSQLiteOpenHelper;
-
-import static android.support.test.InstrumentationRegistry.getTargetContext;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({UserDaoImplTest.class, ExpenseTypeDaoImplTest.class, ExpenseDaoImplTest.class,
         IncomeDaoImplTest.class, BankCardDaoImplTest.class, CardDetailDaoImplTest.class,
-        OtherPaymentMethodsDaoImplTest.class})
+        OtherPaymentMethodDaoImplTest.class})
 public class DaoSuiteTest {
 
 }
--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/OtherPaymentMethodDaoImplTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/OtherPaymentMethodDaoImplTest.java
@@ -5,6 +5,7 @@ import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import io.dojogeek.adminibot.sqlite.AdminiBotSQLiteOpenHelper;
@@ -12,7 +13,7 @@ import io.dojogeek.adminibot.sqlite.AdminiBotSQLiteOpenHelper;
 import static android.support.test.InstrumentationRegistry.getTargetContext;
 
 @RunWith(AndroidJUnit4.class)
-public class OtherPaymentMethodsDaoImplTest {
+public class OtherPaymentMethodDaoImplTest {
 
     private static final int SUCCESS_OPERATION = 1;
     private static final int OPERATIONAL_ERROR = -1;
@@ -34,4 +35,5 @@ public class OtherPaymentMethodsDaoImplTest {
         mContext.deleteDatabase(AdminiBotSQLiteOpenHelper.DATABASE_NAME);
     }
 
+    
 }
