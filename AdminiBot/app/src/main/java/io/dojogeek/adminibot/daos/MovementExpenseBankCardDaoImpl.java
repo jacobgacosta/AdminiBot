e348c579e7ffe2c1f5e8a0910283fa9e68dc1bb9
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-18 21:11:31 -0600

    Create MovementExpenseBankCardDaoImpl class

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/MovementExpenseBankCardDaoImpl.java
@@ -0,0 +1,10 @@
+package io.dojogeek.adminibot.daos;
+
+import android.content.Context;
+
+public class MovementExpenseBankCardDaoImpl extends SQLiteGlobalDao implements MovementExpenseBankCardDao {
+
+    public MovementExpenseBankCardDaoImpl(Context context) {
+        super(context);
+    }
+}
