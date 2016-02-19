588179380071f4a5895842a5ac15c74736388223
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-19 12:18:04 -0600

    Create MovementExpenseBankCardModel class

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/models/MovementExpenseBankCardModel.java
@@ -0,0 +1,12 @@
+package io.dojogeek.adminibot.models;
+
+public class MovementExpenseBankCardModel {
+
+    public long id;
+    public String description;
+    public String date;
+    public double amount;
+    public long expenseId;
+    public long bankCardId;
+
+}
