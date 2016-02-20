3df292f818b82332f9a85ccb03a5ef27df33b6e3
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-20 10:05:55 -0600

    Add "DataException" to "getMovementExpenseBankCardById" method

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/MovementExpenseBankCardDao.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/MovementExpenseBankCardDao.java
@@ -1,10 +1,11 @@
 package io.dojogeek.adminibot.daos;
 
+import io.dojogeek.adminibot.exceptions.DataException;
 import io.dojogeek.adminibot.models.MovementExpenseBankCardModel;
 
 public interface MovementExpenseBankCardDao extends ConectionDao {
 
     long createMovementExpenseBankCard(MovementExpenseBankCardModel movementExpenseBankCardModel);
 
-    MovementExpenseBankCardModel getMovementExpenseBankCardById(long movementExpenseBankCardModelId);
+    MovementExpenseBankCardModel getMovementExpenseBankCardById(long movementExpenseBankCardModelId) throws DataException;
 }
