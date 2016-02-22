3bc7c3d828306880395b1c9143581aefa9b89b72
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-22 13:20:14 -0600

    Add MovementIncomeBankCardModel class to suite

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/DaoSuiteTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/DaoSuiteTest.java
@@ -6,7 +6,8 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({UserDaoImplTest.class, ExpenseTypeDaoImplTest.class, ExpenseDaoImplTest.class,
         IncomeDaoImplTest.class, BankCardDaoImplTest.class, CardDetailDaoImplTest.class,
-        OtherPaymentMethodDaoImplTest.class, MovementExpenseBankCardDaoImplTest.class})
+        OtherPaymentMethodDaoImplTest.class, MovementExpenseBankCardDaoImplTest.class,
+        MovementIncomeBankCardDaoImplTest.class})
 public class DaoSuiteTest {
 
 }
