baeb20024171ce9cd9ce34122a31f8df4e12de18
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-22 13:20:05 -0600

    Create MovementIncomeBankCardModel class

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/models/MovementIncomeBankCardModel.java
@@ -0,0 +1,12 @@
+package io.dojogeek.adminibot.models;
+
+public class MovementIncomeBankCardModel {
+
+    public long id;
+    public String description;
+    public String date;
+    public double amount;
+    public long incomeId;
+    public long bankCardId;
+
+}
