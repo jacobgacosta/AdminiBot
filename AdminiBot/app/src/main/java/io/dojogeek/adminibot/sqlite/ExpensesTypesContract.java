eb3df1de18034a3254ae6018fb0cf7098be264f8
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-26 12:31:18 -0600

    Replace EXPENSES_TYPES for "ExpenseTypeEnum" type

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/sqlite/ExpensesTypesContract.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/sqlite/ExpensesTypesContract.java
@@ -3,17 +3,13 @@ package io.dojogeek.adminibot.sqlite;
 import android.provider.BaseColumns;
 
 import io.dojogeek.adminibot.R;
+import io.dojogeek.adminibot.enums.ExpenseTypeEnum;
 
 import static io.dojogeek.adminibot.sqlite.SQLiteConstants.DROP_TABLE_IF_EXIST;
 
 public class ExpensesTypesContract {
 
-    public static final int [] EXPENSES_TYPES = {R.string.expenses_types_foods,
-            R.string.expenses_types_clothes, R.string.expenses_types_drinks, R.string.expenses_types_medicals,
-            R.string.expenses_types_luxuries};
-    public static final int [] EXPENSES_TYPES_DESCRIPTIONS = {R.string.expenses_types_foods_description,
-            R.string.expenses_types_clothes_description, R.string.expenses_types_drinks_description,
-            R.string.expenses_types_medicals_description, R.string.expenses_types_luxuries_description};
+    public static final ExpenseTypeEnum [] EXPENSES_TYPES = ExpenseTypeEnum.values();
 
     public static abstract class ExpenseType implements BaseColumns {
 
@@ -26,33 +22,33 @@ public class ExpensesTypesContract {
 
     public static final String SQL_CREATE_TABLE = "CREATE TABLE " + ExpenseType.TABLE_NAME +
             "(" + ExpenseType._ID + " INTEGER PRIMARY KEY, " +
-            ExpenseType.COLUMN_NAME + " INTEGER NOT NULL, " +
+            ExpenseType.COLUMN_NAME + " TEXT NOT NULL, " +
             ExpenseType.COLUMN_DESCRIPTION + " INTEGER NOT NULL)";
 
     public static final String SQL_INSERT_INITIAL_VALUES_1 = "INSERT INTO " + ExpenseType.TABLE_NAME +
             " VALUES(" + ExpenseType.COLUMN_NAME_NULLABLE + "," +
-            "'" + EXPENSES_TYPES[0] + "'," +
-            "'" + EXPENSES_TYPES_DESCRIPTIONS[0] + "')";
+            "'" + EXPENSES_TYPES[0].name() + "'," +
+            "'" + EXPENSES_TYPES[0].getDescription() + "')";
 
     public static final String SQL_INSERT_INITIAL_VALUES_2 = "INSERT INTO " + ExpenseType.TABLE_NAME +
             " VALUES(" + ExpenseType.COLUMN_NAME_NULLABLE + "," +
-            "'" + EXPENSES_TYPES[1]  + "'," +
-            "'" + EXPENSES_TYPES_DESCRIPTIONS[1] + "')";
+            "'" + EXPENSES_TYPES[1].name() + "'," +
+            "'" + EXPENSES_TYPES[1].getDescription() + "')";
 
     public static final String SQL_INSERT_INITIAL_VALUES_3 = "INSERT INTO " + ExpenseType.TABLE_NAME +
             " VALUES(" + ExpenseType.COLUMN_NAME_NULLABLE + "," +
-            "'" + EXPENSES_TYPES[2] + "'," +
-            "'" + EXPENSES_TYPES_DESCRIPTIONS[2] + "')";
+            "'" + EXPENSES_TYPES[2].name() + "'," +
+            "'" + EXPENSES_TYPES[2].getDescription() + "')";
 
     public static final String SQL_INSERT_INITIAL_VALUES_4 = "INSERT INTO " + ExpenseType.TABLE_NAME +
             " VALUES(" + ExpenseType.COLUMN_NAME_NULLABLE + "," +
-            "'" + EXPENSES_TYPES[3] + "'," +
-            "'" + EXPENSES_TYPES_DESCRIPTIONS[3] + "')";
+            "'" + EXPENSES_TYPES[3].name() + "'," +
+            "'" + EXPENSES_TYPES[3].getDescription() + "')";
 
     public static final String SQL_INSERT_INITIAL_VALUES_5 = "INSERT INTO " + ExpenseType.TABLE_NAME +
             " VALUES(" + ExpenseType.COLUMN_NAME_NULLABLE + "," +
-            "'" + EXPENSES_TYPES[4] + "'," +
-            "'" + EXPENSES_TYPES_DESCRIPTIONS[4] + "')";
+            "'" + EXPENSES_TYPES[4].name() + "'," +
+            "'" + EXPENSES_TYPES[4].getDescription() + "')";
 
     public static final String SQL_DELETE_ENTRIES = DROP_TABLE_IF_EXIST + ExpenseType.TABLE_NAME;
 
