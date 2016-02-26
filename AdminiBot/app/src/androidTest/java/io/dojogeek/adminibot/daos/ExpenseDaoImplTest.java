de8a103a4cf108b1ff04c9994156fcc3ea542a37
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-26 14:55:58 -0600

    Add missing test cases for ExpenseDaoImpl

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/ExpenseDaoImplTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/ExpenseDaoImplTest.java
@@ -1,6 +1,7 @@
 package io.dojogeek.adminibot.daos;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteConstraintException;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.After;
@@ -11,6 +12,7 @@ import org.junit.runner.RunWith;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.dojogeek.adminibot.exceptions.DataException;
 import io.dojogeek.adminibot.models.ExpenseModel;
 import io.dojogeek.adminibot.sqlite.AdminiBotSQLiteOpenHelper;
 import io.dojogeek.adminibot.sqlite.ExpensesContract;
@@ -20,12 +22,16 @@ import io.dojogeek.adminibot.utiltest.CreatorModels;
 import static android.support.test.InstrumentationRegistry.getTargetContext;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class ExpenseDaoImplTest {
 
+    private static final long NO_OPERATION = 0;
     private static final int SUCCESS_OPERATION = 1;
-    private static final int OPERATIONAL_ERROR = -1;
+    private static final long OPERATIONAL_ERROR = -1;
     private ExpenseDao mExpenseDao;
     private Context mContext;
 
@@ -53,6 +59,27 @@ public class ExpenseDaoImplTest {
 
     }
 
+    @Test(expected = NullPointerException.class)
+    public void testCreateExpense_withNullModel_isException() {
+
+        ExpenseModel expenseModel = null;
+
+        mExpenseDao.createExpense(expenseModel);
+
+    }
+
+    @Test
+    public void testCreateExpense_withNullRequieredField_noInsertion() {
+
+        ExpenseModel expenseModel = CreatorModels.createExpenseModel();
+        expenseModel.description = null;
+
+        long insertedRecordId = mExpenseDao.createExpense(expenseModel);
+
+        assertThat(insertedRecordId, is(OPERATIONAL_ERROR));
+
+    }
+
     @Test
     public void testGetExpenses_successObtainingList() {
 
@@ -65,9 +92,17 @@ public class ExpenseDaoImplTest {
         compareExpensesList(expectedExpenses, actualExpenses);
     }
 
+    @Test
+    public void testGetExpenses_withNoRecords_isEmptyList() {
+
+        List<ExpenseModel> actualExpenses = mExpenseDao.getExpenses();
+
+        assertThat(actualExpenses.isEmpty(), is(true));
+    }
+
 
     @Test
-    public void testGetExpenseById_successObtaining() {
+    public void testGetExpenseById_successObtaining() throws DataException {
 
         ExpenseModel expectedExpenseModel = CreatorModels.createExpenseModel();
 
@@ -79,8 +114,21 @@ public class ExpenseDaoImplTest {
 
     }
 
+    @Test(expected = DataException.class)
+    public void testGetExpenseById_withNonExistentId_isException() throws DataException {
+
+        ExpenseModel expectedExpenseModel = CreatorModels.createExpenseModel();
+
+        mExpenseDao.createExpense(expectedExpenseModel);
+
+        long nonExistentId = 4;
+
+        mExpenseDao.getExpenseById(nonExistentId);
+
+    }
+
     @Test
-    public void testUpdateExpense_successUpdating() {
+    public void testUpdateExpense_successUpdating() throws DataException {
 
         ExpenseModel expenseModel = CreatorModels.createExpenseModel();
 
@@ -100,6 +148,53 @@ public class ExpenseDaoImplTest {
 
     }
 
+    @Test(expected = NullPointerException.class)
+    public void testUpdateExpense_withNullModel_isException() {
+
+        ExpenseModel expenseModel = CreatorModels.createExpenseModel();
+
+        long insertedRecordId = mExpenseDao.createExpense(expenseModel);
+
+        String where = ExpensesContract.Expense._ID + "= " + insertedRecordId;
+
+        mExpenseDao.updateExpense(null, where);
+
+    }
+
+    @Test
+    public void testUpdateExpense_withNonExistentId_noUpdating() {
+
+        ExpenseModel expenseModel = CreatorModels.createExpenseModel();
+
+        mExpenseDao.createExpense(expenseModel);
+
+        ExpenseModel newExpenseModel = changeExpenseModelValues(expenseModel);
+
+        long nonExistentId = 5;
+
+        String where = ExpensesContract.Expense._ID + "= " + nonExistentId;
+
+        long updatedRows = mExpenseDao.updateExpense(newExpenseModel, where);
+
+        assertThat(updatedRows, is(NO_OPERATION));
+    }
+
+    @Test(expected = SQLiteConstraintException.class)
+    public void testUpdateExpense_withNullRequiredField_isException() {
+
+        ExpenseModel expenseModel = CreatorModels.createExpenseModel();
+
+        long insertedRecordId = mExpenseDao.createExpense(expenseModel);
+
+        ExpenseModel newExpenseModel = changeExpenseModelValues(expenseModel);
+        newExpenseModel.description = null;
+
+        String where = ExpensesContract.Expense._ID + "= " + insertedRecordId;
+
+        mExpenseDao.updateExpense(newExpenseModel, where);
+
+    }
+
     @Test
     public void testDeleteExpense_successDeletion() {
 
@@ -113,6 +208,16 @@ public class ExpenseDaoImplTest {
     }
 
     @Test
+    public void testDeleteExpense_withNonExistentId_noDeletion() {
+
+        long nonExistentId = 5;
+
+        long deletedRows = mExpenseDao.deleteExpense(nonExistentId);
+
+        assertThat(deletedRows, is(NO_OPERATION));
+    }
+
+    @Test
     public void testGetExpenseByExpenseTypeId_successObtaining() {
 
         int numberOfInsertions = 5;
@@ -123,6 +228,17 @@ public class ExpenseDaoImplTest {
         List<ExpenseModel> actualExpensesModels = mExpenseDao.getExpenseByExpenseTypeId(expenseTypeId);
 
         compareExpensesList(expectedExpensesModels, actualExpensesModels);
+    }
+
+    @Test
+    public void testGetExpenseByExpenseTypeId_withNonExistentExpenseTypeId_isEmptyList() {
+
+        long nonExistentId = 4;
+
+        List<ExpenseModel> actualExpensesModels = mExpenseDao.getExpenseByExpenseTypeId(nonExistentId);
+
+        assertThat(actualExpensesModels.isEmpty(), is(true));
+
     }
 
     private ExpenseModel changeExpenseModelValues(ExpenseModel expenseModel) {
