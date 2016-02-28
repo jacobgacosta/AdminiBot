4cccab5f7cf45ff6d760cb850a5c29c403ecf6ab
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-28 16:34:05 -0600

    Remove "getCardDetailById"

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/CardDetailDao.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/CardDetailDao.java
@@ -1,14 +1,13 @@
 package io.dojogeek.adminibot.daos;
 
+import io.dojogeek.adminibot.exceptions.DataException;
 import io.dojogeek.adminibot.models.CardDetailModel;
 
 public interface CardDetailDao extends ConnectionDao {
 
     long createCardDetail(CardDetailModel cardDetailModel);
 
-    CardDetailModel getCardDetailById(long cardDetailId);
-
-    CardDetailModel getCardDetailByBankCardId(long bankCardId);
+    CardDetailModel getCardDetailByBankCardId(long bankCardId) throws DataException;
 
     long updateCardDetail(CardDetailModel cardDetailModel, String where);
 
