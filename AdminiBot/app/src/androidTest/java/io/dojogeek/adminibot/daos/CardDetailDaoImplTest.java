c984d657e755a8796e98d7ac4f12c6f9895aa82c
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-28 15:51:50 -0600

    Add missing tests for CardDetailDao

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/CardDetailDaoImplTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/daos/CardDetailDaoImplTest.java
@@ -1,6 +1,7 @@
 package io.dojogeek.adminibot.daos;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteConstraintException;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.After;
@@ -8,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import io.dojogeek.adminibot.exceptions.DataException;
 import io.dojogeek.adminibot.models.CardDetailModel;
 import io.dojogeek.adminibot.sqlite.AdminiBotSQLiteOpenHelper;
 import io.dojogeek.adminibot.sqlite.CardDetailContract;
@@ -15,14 +17,16 @@ import io.dojogeek.adminibot.utils.DateUtils;
 import io.dojogeek.adminibot.utiltest.CreatorModels;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
+
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 @RunWith(AndroidJUnit4.class)
 public class CardDetailDaoImplTest {
 
     private static final int SUCCESS_OPERATION = 1;
-    private static final int OPERATIONAL_ERROR = -1;
-    private static final int NO_OPERATION = 0;
+    private static final long OPERATIONAL_ERROR = -1;
+    private static final long NO_OPERATION = 0;
     private CardDetailDao mCardDetailDao;
     private Context mContext;
 
@@ -51,20 +55,28 @@ public class CardDetailDaoImplTest {
 
     }
 
-    @Test
-    public void testGetCardDetailById_successObtaining() {
+    @Test(expected = NullPointerException.class)
+    public void testCreateCardDetail_withNullModel_isException() {
 
-        CardDetailModel expectedCardDetailModel = CreatorModels.createCardDetailModel();
+        CardDetailModel cardDetailModel = null;
 
-        long insertedRecordId = mCardDetailDao.createCardDetail(expectedCardDetailModel);
+        mCardDetailDao.createCardDetail(cardDetailModel);
 
-        CardDetailModel actualCardDetailModel = mCardDetailDao.getCardDetailById(insertedRecordId);
-
-        compareCardsDetails(expectedCardDetailModel, actualCardDetailModel);
     }
 
     @Test
-    public void testGetCardDetailByBankCardId_successObtaining() {
+    public void testCreateCardDetail_withNullRequiredField_noInsertion() {
+
+        CardDetailModel cardDetailModel = CreatorModels.createCardDetailModel();
+        cardDetailModel.cuttoffDate = null;
+
+        long insetedRecordId = mCardDetailDao.createCardDetail(cardDetailModel);
+
+        assertThat(insetedRecordId, is(OPERATIONAL_ERROR));
+    }
+
+    @Test
+    public void testGetCardDetailByBankCardId_successObtaining() throws DataException {
 
         CardDetailModel expectedCardDetailModel = CreatorModels.createCardDetailModel();
 
@@ -76,8 +88,17 @@ public class CardDetailDaoImplTest {
 
     }
 
+    @Test(expected = DataException.class)
+    public void testGetCardDetailByBankCardId_withNonExistentBanCardId_isException() throws DataException {
+
+        long nonExistentId = 5;
+
+        mCardDetailDao.getCardDetailByBankCardId(nonExistentId);
+
+    }
+
     @Test
-    public void testUpdateCardDetail_successUpdating() {
+    public void testUpdateCardDetail_successUpdating() throws DataException {
 
         CardDetailModel cardDetailModel = CreatorModels.createCardDetailModel();
 
@@ -91,9 +112,51 @@ public class CardDetailDaoImplTest {
 
         assertEquals(SUCCESS_OPERATION, updatedRows);
 
-        CardDetailModel actualCardDetailModel = mCardDetailDao.getCardDetailById(insertedRecordId);
+        CardDetailModel actualCardDetailModel = mCardDetailDao.getCardDetailByBankCardId(cardDetailModel.bankCardId);
 
         compareCardsDetails(expectedUpdatedCardDetail, actualCardDetailModel);
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testUpdateCardDetail_withNullModel_isException() {
+
+        CardDetailModel cardDetailModel = CreatorModels.createCardDetailModel();
+
+        long insertedRecordId = mCardDetailDao.createCardDetail(cardDetailModel);
+
+        String where = CardDetailContract.CardDetail._ID + "= " + insertedRecordId;
+
+        mCardDetailDao.updateCardDetail(null, where);
+
+    }
+
+    @Test(expected = SQLiteConstraintException.class)
+    public void testUpdateCardDetail_withNullRequiredField_noUpdate() {
+
+        CardDetailModel cardDetailModel = CreatorModels.createCardDetailModel();
+
+        long insertedRecordId = mCardDetailDao.createCardDetail(cardDetailModel);
+
+        CardDetailModel expectedUpdatedCardDetail = changeCardDetailValues(cardDetailModel);
+        expectedUpdatedCardDetail.cuttoffDate = null;
+
+        String where = CardDetailContract.CardDetail._ID + "= " + insertedRecordId;
+
+        mCardDetailDao.updateCardDetail(expectedUpdatedCardDetail, where);
+
+    }
+
+    @Test
+    public void testUpdateCardDetail_withNonExistentId_noUpdating() {
+
+        CardDetailModel cardDetailModel = CreatorModels.createCardDetailModel();
+
+        long nonExistentId = 5;
+
+        String where = CardDetailContract.CardDetail._ID + "= " + nonExistentId;
+
+        mCardDetailDao.updateCardDetail(cardDetailModel, where);
 
     }
 
@@ -107,6 +170,15 @@ public class CardDetailDaoImplTest {
         long deletedRows = mCardDetailDao.deleteCardDetail(insertedRecordId);
 
         assertEquals(SUCCESS_OPERATION, deletedRows);
+    }
+
+    @Test
+    public void testDeleteCardDetail_withNonExistentId_noDeletion() {
+
+        long nonExistentId = 4;
+
+        mCardDetailDao.deleteCardDetail(nonExistentId);
+
     }
 
     private void compareCardsDetails(CardDetailModel expectedCardDetailModel, CardDetailModel actualCardDetailModel) {
