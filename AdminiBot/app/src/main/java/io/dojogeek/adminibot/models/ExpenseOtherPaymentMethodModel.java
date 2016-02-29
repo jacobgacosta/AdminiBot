f92b0fee66cd90c95631133116908283576a1582
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-02-29 15:10:58 -0600

    Create ExpenseOtherPaymentMethodModel class

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/models/ExpenseOtherPaymentMethodModel.java
@@ -0,0 +1,10 @@
+package io.dojogeek.adminibot.models;
+
+public class ExpenseOtherPaymentMethodModel {
+
+    public long id;
+    public long expenseId;
+    public long otherPaymentMethodId;
+    public double amount;
+
+}
