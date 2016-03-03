64724ffe468fe9a094fe33d5c1e18da8894e088e
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-03-02 20:46:43 -0600

    Declare methods  for IncomeOtherPaymentMethodsDao

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/IncomeOtherPaymentMethodsDao.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/IncomeOtherPaymentMethodsDao.java
@@ -1,4 +1,21 @@
 package io.dojogeek.adminibot.daos;
 
-public interface IncomeOtherPaymentMethodsDao {
+import java.util.List;
+
+import io.dojogeek.adminibot.exceptions.DataException;
+import io.dojogeek.adminibot.models.IncomeOtherPaymentMethodModel;
+
+public interface IncomeOtherPaymentMethodsDao extends ConnectionDao {
+
+    long createIncomeOtherPaymentMethod(IncomeOtherPaymentMethodModel incomeOtherPaymentMethodModel);
+
+    IncomeOtherPaymentMethodModel getIncomeOtherPaymentMethodById(long incomeOtherPaymentMethodModelId) throws DataException;
+
+    List<IncomeOtherPaymentMethodModel> getIncomesOthersPaymentMethods();
+
+    List<IncomeOtherPaymentMethodModel> getIncomeOtherPaymentMethodByIncomeId(long incomeId);
+
+    List<IncomeOtherPaymentMethodModel> getIncomeOtherPaymentMethodByPaymentMethodId(long paymentMethodId);
+
+    long deleteIncomeOtherPaymentMethod(long incomeOtherPaymentMethodModelId);
 }
