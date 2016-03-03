a4bfcbb225cd59dac5969a7dd6e91580fce56b4e
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-03-02 20:41:14 -0600

    Create IncomeOtherPaymentMethodModel class

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/models/IncomeOtherPaymentMethodModel.java
@@ -0,0 +1,10 @@
+package io.dojogeek.adminibot.models;
+
+public class IncomeOtherPaymentMethodModel {
+
+    public long id;
+    public long incomeId;
+    public long otherPaymentMethodId;
+    public double amount;
+
+}
