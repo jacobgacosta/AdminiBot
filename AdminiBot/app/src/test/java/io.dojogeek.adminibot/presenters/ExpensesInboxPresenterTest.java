572d15e296f5839df5f48ecb573558a8b76961b3
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-03-07 20:48:15 -0600

    Create tests for  "ExpensesInbox" presenter

---
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/presenters/ExpensesInboxPresenterTest.java
@@ -0,0 +1,127 @@
+package io.dojogeek.adminibot.presenters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.dojogeek.adminibot.R;
+import io.dojogeek.adminibot.daos.ExpenseDaoImpl;
+import io.dojogeek.adminibot.models.ExpenseModel;
+import io.dojogeek.adminibot.utils.DateUtils;
+import io.dojogeek.adminibot.views.ExpensesInbox;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ExpensesInboxPresenterTest {
+
+    @Mock
+    private ExpensesInbox mExpensesInboxActivity;
+
+    @Mock
+    private ExpenseDaoImpl mExpenseDao;
+
+    @InjectMocks
+    private ExpensesInboxPresenter mExpensesInboxPresenter = new ExpensesInboxPresenterImpl(mExpensesInboxActivity, mExpenseDao);
+
+    @Test
+    public void testListExpenses_correctFlowWithListOfRecords() {
+
+        List<ExpenseModel> expenseModelList = getExpensesList();
+
+        when(mExpenseDao.getExpenses()).thenReturn(expenseModelList);
+
+        mExpensesInboxPresenter.getExpenses();
+
+        verify(mExpenseDao).getExpenses();
+        verify(mExpensesInboxActivity).listExpenses(expenseModelList);
+
+    }
+
+    @Test
+    public void testListExpenses_correctFlowWithEmptyList() {
+
+        List<ExpenseModel> emptyExpenseModelList =  new ArrayList<>();
+
+        when(mExpenseDao.getExpenses()).thenReturn(emptyExpenseModelList);
+
+        mExpensesInboxPresenter.getExpenses();
+
+        verify(mExpenseDao).getExpenses();
+        verify(mExpensesInboxActivity).listExpenses(emptyExpenseModelList);
+    }
+
+    @Test
+    public void testDeleteExpense_successDeletion() {
+
+        long deletedRows = 1;
+        long expenseId = 4;
+
+        when(mExpenseDao.deleteExpense(anyLong())).thenReturn(deletedRows);
+
+        mExpensesInboxPresenter.deleteExpense(expenseId);
+
+        verify(mExpenseDao).deleteExpense(anyLong());
+        verify(mExpensesInboxActivity).showNotification(R.string.success_deletion);
+    }
+
+    @Test
+    public void testDeleteExpense_errorDeletion() {
+
+        long deletedRows = 0;
+        long expenseId = 1;
+
+        when(mExpenseDao.deleteExpense(anyLong())).thenReturn(deletedRows);
+
+        mExpensesInboxPresenter.deleteExpense(expenseId);
+
+        verify(mExpenseDao).deleteExpense(anyLong());
+        verify(mExpensesInboxActivity).showNotification(R.string.error_deletion);
+    }
+
+    @Test
+    public void testCloseConnection_correctInvocationAfterUnnusedView() {
+
+        mExpensesInboxPresenter.unnusedView();
+
+        verify(mExpenseDao).closeConnection();
+
+    }
+
+    private List<ExpenseModel> getExpensesList() {
+
+        ExpenseModel expenseModel = createExpenseModel(1, "Payment camera", 7800,
+                DateUtils.getCurrentData(), "", 1, 1);
+
+        List<ExpenseModel> expenseModelList = new ArrayList<>();
+        expenseModelList.add(expenseModel);
+        expenseModelList.add(expenseModel);
+        expenseModelList.add(expenseModel);
+        expenseModelList.add(expenseModel);
+
+        return expenseModelList;
+    }
+
+    private ExpenseModel createExpenseModel(int id, String description, double amount,
+                                            String dateExpediture, String nextExit, long expenseTypeId,
+                                            long userId) {
+
+        ExpenseModel expenseModel = new ExpenseModel();
+        expenseModel.id = id;
+        expenseModel.description = description;
+        expenseModel.amount = amount;
+        expenseModel.dateExpediture = dateExpediture;
+        expenseModel.nextExit = nextExit;
+        expenseModel.expenseTypeId = expenseTypeId;
+        expenseModel.userId = userId;
+
+        return expenseModel;
+    }
+
+}
