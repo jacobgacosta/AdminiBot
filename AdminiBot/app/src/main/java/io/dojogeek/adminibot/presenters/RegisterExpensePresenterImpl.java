b129ccc25c9ac502e5326c7846b34952d2404c77
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-03-07 20:18:59 -0600

    Remove open and close connection from presenter

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/presenters/RegisterExpensePresenterImpl.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/presenters/RegisterExpensePresenterImpl.java
@@ -2,7 +2,6 @@ package io.dojogeek.adminibot.presenters;
 
 import java.util.List;
 
-import io.dojogeek.adminibot.daos.ConnectionDao;
 import io.dojogeek.adminibot.daos.ExpenseDao;
 import io.dojogeek.adminibot.daos.ExpenseTypeDao;
 import io.dojogeek.adminibot.daos.TypesPaymentMethodsDao;
@@ -36,8 +35,6 @@ public class RegisterExpensePresenterImpl implements RegisterExpensePresenter {
     @Override
     public void getExpensesTypes() {
 
-        mExpenseTypeDao.openConection();
-
         List<ExpenseTypeModel> expenseTypeModelList = mExpenseTypeDao.getExpensesTypes();
 
         mRegisterExpense.deployExpensesTypes(expenseTypeModelList);
@@ -45,10 +42,6 @@ public class RegisterExpensePresenterImpl implements RegisterExpensePresenter {
 
     @Override
     public void addExpense(ExpenseModel expenseModel) {
-
-        mExpenseDao.openConection();
-
-        mUserDao.openConection();
 
         UserModel userModel = mUserDao.getUsers().get(INDEX_UNIQUE_USER);
         expenseModel.userId = userModel.id;
@@ -59,23 +52,15 @@ public class RegisterExpensePresenterImpl implements RegisterExpensePresenter {
 
     @Override
     public void unnusedView() {
-        closeConnection(mExpenseTypeDao);
-        closeConnection(mUserDao);
-        closeConnection(mExpenseDao);
-        closeConnection(mTypesPaymentMethodsDao);
     }
 
     @Override
     public void getPaymentMethods() {
 
-        mTypesPaymentMethodsDao.openConection();
 
         List<TypePaymentMethodModel> typePaymentMethodsModels = mTypesPaymentMethodsDao.getPaymentMethods();
 
         mRegisterExpense.deployPaymentMethods(typePaymentMethodsModels);
     }
 
-    private void closeConnection(ConnectionDao connectionDao) {
-        connectionDao.closeConection();
-    }
 }
