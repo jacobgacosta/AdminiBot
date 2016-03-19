d298482aa0bc07a44269addae698329f579c5cf5
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-03-19 12:12:58 -0600

    Create ModelsFactory class

---
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/factory/ModelsFactory.java
@@ -0,0 +1,43 @@
+package io.dojogeek.adminibot.factory;
+
+import io.dojogeek.adminibot.models.ExpenseBankCardModel;
+import io.dojogeek.adminibot.models.ExpenseModel;
+import io.dojogeek.adminibot.models.ExpenseOtherPaymentMethodModel;
+import io.dojogeek.adminibot.models.OtherPaymentMethodModel;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+public class ModelsFactory {
+
+    private static PodamFactory factory = new PodamFactoryImpl();
+
+    public static ExpenseOtherPaymentMethodModel createExpenseOtherPaymentMethodModel() {
+
+        ExpenseOtherPaymentMethodModel expenseOtherPaymentMethodModel =
+                factory.manufacturePojo(ExpenseOtherPaymentMethodModel.class);
+
+        return expenseOtherPaymentMethodModel;
+    }
+
+    public static ExpenseModel createExpenseModel() {
+
+        ExpenseModel expenseModel = factory.manufacturePojo(ExpenseModel.class);
+
+        return expenseModel;
+    }
+
+    public static ExpenseBankCardModel createExpenseBankCardModel() {
+
+        ExpenseBankCardModel expenseBankCardModel = factory.manufacturePojo(ExpenseBankCardModel.class);
+
+        return expenseBankCardModel;
+    }
+
+    public static OtherPaymentMethodModel createOtherPaymentMethodModel() {
+
+        OtherPaymentMethodModel otherPaymentMethodModel =
+                factory.manufacturePojo(OtherPaymentMethodModel.class);
+
+        return otherPaymentMethodModel;
+    }
+}
