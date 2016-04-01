1461d02768a0d0f012acead9b44c16b2d4669526
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-04-01 14:01:42 -0600

    Add test cases for "loadPaymentMethods"

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/presenters/PaymentMethodsPresenterTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/presenters/PaymentMethodsPresenterTest.java
@@ -23,6 +23,62 @@ import static org.mockito.Mockito.*;
 @RunWith(MockitoJUnitRunner.class)
 public class PaymentMethodsPresenterTest {
 
-    
+    @Mock
+    private PaymentMethods mPaymentMethodsActivity;
+
+    @Mock
+    private OtherPaymentMethodDao mOtherPaymentMethodDao;
+
+    @Mock
+    private BankCardDao mBankCardDao;
+
+    @InjectMocks
+    private PaymentMethodsPresenter mPaymentMethodsPresenter =
+            new PaymentMethodsPresenterImpl(mPaymentMethodsActivity, mOtherPaymentMethodDao, mBankCardDao);
+
+    @Test
+    public void testLoadPaymentMethods_successfulListPaymentMethods() {
+
+        OtherPaymentMethodModel otherPaymentMethodModelOne = ModelsFactory.createOtherPaymentMethodModel();
+        otherPaymentMethodModelOne.setTypePaymentMethod(TypePaymentMethodEnum.CASH);
+        otherPaymentMethodModelOne.setTypePaymentMethod(TypePaymentMethodEnum.CASH);
+
+        OtherPaymentMethodModel otherPaymentMethodModelTwo = ModelsFactory.createOtherPaymentMethodModel();
+        otherPaymentMethodModelTwo.setTypePaymentMethod(TypePaymentMethodEnum.CHEQUE);
+        otherPaymentMethodModelTwo.setTypePaymentMethod(TypePaymentMethodEnum.CHEQUE);
+
+        List<OtherPaymentMethodModel> otherPaymentMethodModelList = new ArrayList<>();
+        otherPaymentMethodModelList.add(otherPaymentMethodModelOne);
+        otherPaymentMethodModelList.add(otherPaymentMethodModelTwo);
+
+        List<BankCardModel> bankCardModelList = new ArrayList<>();
+        bankCardModelList.add(ModelsFactory.createBankCardModel());
+        bankCardModelList.add(ModelsFactory.createBankCardModel());
+
+        when(mOtherPaymentMethodDao.getOtherPaymentMethods()).thenReturn(otherPaymentMethodModelList);
+        when(mBankCardDao.getBankCards()).thenReturn(bankCardModelList);
+
+        mPaymentMethodsPresenter.loadPaymentMethods();
+
+        verify(mOtherPaymentMethodDao).getOtherPaymentMethods();
+        verify(mBankCardDao).getBankCards();
+        verify(mPaymentMethodsActivity).
+                showTypesPaymentMethods(argThat(new IsAList<TypePaymentMethodEnum>().size(3)));
+
+    }
+
+    @Test
+    public void testLoadPaymentMethods_withEmptyPaymentMethods() {
+
+        when(mOtherPaymentMethodDao.getOtherPaymentMethods()).thenReturn(new ArrayList<OtherPaymentMethodModel>());
+        when(mBankCardDao.getBankCards()).thenReturn(new ArrayList<BankCardModel>());
+
+        mPaymentMethodsPresenter.loadPaymentMethods();
+
+        verify(mOtherPaymentMethodDao).getOtherPaymentMethods();
+        verify(mBankCardDao).getBankCards();
+        verify(mPaymentMethodsActivity).
+                showTypesPaymentMethods(argThat(new IsAList<TypePaymentMethodEnum>().size(0)));
+    }
 
 }
