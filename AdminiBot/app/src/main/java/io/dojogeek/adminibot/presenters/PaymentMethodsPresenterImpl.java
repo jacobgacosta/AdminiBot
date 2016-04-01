f60e88eb872ca4f090af12e1e8b874986ec6ab1c
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-04-01 14:02:56 -0600

    Implement "loadPaymentMethods"

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/presenters/PaymentMethodsPresenterImpl.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/presenters/PaymentMethodsPresenterImpl.java
@@ -11,6 +11,58 @@ import io.dojogeek.adminibot.models.OtherPaymentMethodModel;
 import io.dojogeek.adminibot.sqlite.PaymentMethodsContract;
 import io.dojogeek.adminibot.views.PaymentMethods;
 
-public class PaymentMethodsPresenterImpl {
-    
+public class PaymentMethodsPresenterImpl implements PaymentMethodsPresenter {
+
+    private PaymentMethods mPaymentMethods;
+    private OtherPaymentMethodDao mOtherPaymentMethodDao;
+    private BankCardDao mBankCardDao;
+    private List<OtherPaymentMethodModel> mOtherPaymentMethodModels;
+    private List<BankCardModel> mBankCardModels;
+
+    public PaymentMethodsPresenterImpl(PaymentMethods paymentMethods, OtherPaymentMethodDao otherPaymentMethodDao,
+                                       BankCardDao bankCardDao) {
+        mPaymentMethods = paymentMethods;
+        mOtherPaymentMethodDao = otherPaymentMethodDao;
+        mBankCardDao = bankCardDao;
+    }
+
+    @Override
+    public void loadPaymentMethods() {
+
+        List<TypePaymentMethodEnum> typePaymentMethodEnumList = getTypesPaymentMethods();
+
+        mPaymentMethods.showTypesPaymentMethods(typePaymentMethodEnumList);
+
+    }
+
+    private List<TypePaymentMethodEnum> getTypesPaymentMethods() {
+
+        List<TypePaymentMethodEnum> paymentMethods = new ArrayList<>();
+
+        mOtherPaymentMethodModels = mOtherPaymentMethodDao.getOtherPaymentMethods();
+
+        mBankCardModels = mBankCardDao.getBankCards();
+
+        if (!mBankCardModels.isEmpty()) {
+            paymentMethods.add(TypePaymentMethodEnum.CARD);
+        }
+
+        for (OtherPaymentMethodModel otherPaymentMethodModel : mOtherPaymentMethodModels) {
+            addTypePaymentMethodIfNonExistToList(paymentMethods, otherPaymentMethodModel);
+        }
+
+        return paymentMethods;
+    }
+
+    private void addTypePaymentMethodIfNonExistToList(List<TypePaymentMethodEnum> paymentMethods,
+                                                      OtherPaymentMethodModel otherPaymentMethodModel) {
+
+        TypePaymentMethodEnum typePaymentMethod = otherPaymentMethodModel.getTypePaymentMethod();
+
+        if (!paymentMethods.contains(typePaymentMethod)) {
+            paymentMethods.add(typePaymentMethod);
+        }
+
+    }
+
 }
