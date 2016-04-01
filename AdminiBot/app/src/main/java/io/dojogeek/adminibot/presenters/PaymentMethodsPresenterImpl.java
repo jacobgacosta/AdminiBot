5926147d3e436059b57a7d9496eadd627f2e052a
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-04-01 14:02:33 -0600

    Create "PaymentMethodsPresenterImpl" class

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/presenters/PaymentMethodsPresenterImpl.java
@@ -0,0 +1,16 @@
+package io.dojogeek.adminibot.presenters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.dojogeek.adminibot.daos.BankCardDao;
+import io.dojogeek.adminibot.daos.OtherPaymentMethodDao;
+import io.dojogeek.adminibot.enums.TypePaymentMethodEnum;
+import io.dojogeek.adminibot.models.BankCardModel;
+import io.dojogeek.adminibot.models.OtherPaymentMethodModel;
+import io.dojogeek.adminibot.sqlite.PaymentMethodsContract;
+import io.dojogeek.adminibot.views.PaymentMethods;
+
+public class PaymentMethodsPresenterImpl {
+    
+}
