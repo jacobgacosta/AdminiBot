617476701e6e07872090ade94a37152c6e3bb7e6
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-04-01 14:01:08 -0600

    Create PaymentMethodsPresenterTest class

---
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/presenters/PaymentMethodsPresenterTest.java
@@ -0,0 +1,28 @@
+package io.dojogeek.adminibot.presenters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.dojogeek.adminibot.daos.BankCardDao;
+import io.dojogeek.adminibot.daos.OtherPaymentMethodDao;
+import io.dojogeek.adminibot.enums.TypePaymentMethodEnum;
+import io.dojogeek.adminibot.factory.ModelsFactory;
+import io.dojogeek.adminibot.matchers.IsAList;
+import io.dojogeek.adminibot.models.BankCardModel;
+import io.dojogeek.adminibot.models.OtherPaymentMethodModel;
+import io.dojogeek.adminibot.views.PaymentMethods;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PaymentMethodsPresenterTest {
+
+    
+
+}
