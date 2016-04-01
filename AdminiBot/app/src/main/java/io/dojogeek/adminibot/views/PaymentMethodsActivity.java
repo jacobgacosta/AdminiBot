ca0fafde56c2cc63d3b980932a1e22fba2d9c7f6
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-04-01 14:08:44 -0600

    Implement "showTypesPaymentMethods" operation

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/PaymentMethodsActivity.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/PaymentMethodsActivity.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import io.dojogeek.adminibot.enums.TypePaymentMethodEnum;
 
-public class PaymentMethodsActivity {
+public class PaymentMethodsActivity implements PaymentMethods {
 
+    @Override
+    public void showTypesPaymentMethods(List<TypePaymentMethodEnum> typePaymentMethodEnumList) {
 
-
+    }
 }
