1e0ac1648fba9f6d7c1ce3cf8f3c9f5151744150
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-04-05 12:31:56 -0500

    Create PaymentMethodAdapterTest class

---
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/adapters/PaymentMethodAdapterTest.java
@@ -0,0 +1,39 @@
+package io.dojogeek.adminibot.adapters;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.res.ResourcesCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.List;
+
+import io.dojogeek.adminibot.R;
+import io.dojogeek.adminibot.enums.TypePaymentMethodEnum;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(PowerMockRunner.class)
+public class PaymentMethodAdapterTest {
+
+    
+
+}
