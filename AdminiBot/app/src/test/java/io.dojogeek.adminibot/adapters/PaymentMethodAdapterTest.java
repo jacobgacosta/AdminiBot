8e2555d62a25fbfb08b85194754dd5fd745e1056
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-04-05 12:37:23 -0500

    Add test case to test PaymentMethodAdapter

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/adapters/PaymentMethodAdapterTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/adapters/PaymentMethodAdapterTest.java
@@ -34,6 +34,78 @@ import static org.mockito.Mockito.*;
 @RunWith(PowerMockRunner.class)
 public class PaymentMethodAdapterTest {
 
-    
+    @Mock
+    private Context mContext;
+
+    @Mock
+    private List<TypePaymentMethodEnum> mTypePaymentMethodEnumList;
+
+    @Mock
+    private LayoutInflater mLayoutInflater;
+
+    @Mock
+    private View mLayoutPaymentMethod;
+
+    @Mock
+    private View mConvertView;
+
+    @Mock
+    private ViewGroup mParent;
+
+    @Mock
+    private ImageView mImagePaymentMethod;
+
+    @Mock
+    private TextView mDescriptionPaymentMethod;
+
+    @Mock
+    private Resources mResources;
+
+    @Mock
+    private Drawable mDrawable;
+
+    @InjectMocks
+    private ArrayAdapter<TypePaymentMethodEnum> typePaymentMethodEnumArrayAdapter =
+            new PaymentMethodAdapter(mContext, mTypePaymentMethodEnumList);
+
+    @Test
+    public void testGetView_mappingData() {
+
+        int position = 0;
+        int rosourceId = 546785498;
+        String stringResource = "Efectivo";
+
+        PowerMockito.mockStatic(ResourcesCompat.class);
+        BDDMockito.given(ResourcesCompat.getDrawable(mResources, rosourceId, null)).willReturn(mDrawable);
+
+        when(mTypePaymentMethodEnumList.get(position)).thenReturn(TypePaymentMethodEnum.CASH);
+        when(mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).thenReturn(mLayoutInflater);
+        when(mLayoutInflater.inflate(R.layout.item_payment_method, mParent, false)).thenReturn(mLayoutPaymentMethod);
+        when(mLayoutPaymentMethod.findViewById(R.id.img_payment_method)).thenReturn(mImagePaymentMethod);
+        when(mLayoutPaymentMethod.findViewById(R.id.payment_method_description)).thenReturn(mDescriptionPaymentMethod);
+        when(mContext.getResources()).thenReturn(mResources);
+        when(mResources.getIdentifier("cash", "string", mContext.getPackageName())).thenReturn(rosourceId);
+        when(mResources.getString(rosourceId)).thenReturn(stringResource);
+        when(mResources.getIdentifier("cash", "drawable", mContext.getPackageName())).thenReturn(rosourceId);
+
+        View actualView =
+                typePaymentMethodEnumArrayAdapter.getView(position, mConvertView, mParent);
+
+        assertNotNull(actualView);
+        assertThat(actualView, is(mLayoutPaymentMethod));
+        verify(mTypePaymentMethodEnumList, times(2)).get(position);
+        verify(mContext).getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        verify(mLayoutInflater).inflate(R.layout.item_payment_method, mParent, false);
+        verify(mLayoutPaymentMethod).findViewById(R.id.img_payment_method);
+        verify(mLayoutPaymentMethod).findViewById(R.id.payment_method_description);
+
+        verify(mContext, times(2)).getResources();
+        verify(mResources).getIdentifier(mTypePaymentMethodEnumList.get(position).getName(), "string", mContext.getPackageName());
+        verify(mResources).getString(rosourceId);
+        verify(mResources).getIdentifier(mTypePaymentMethodEnumList.get(position).getName(), "drawable", mContext.getPackageName());
+
+        verify(mImagePaymentMethod).setImageDrawable(mDrawable);
+        verify(mDescriptionPaymentMethod).setText(stringResource);
+    }
 
 }
