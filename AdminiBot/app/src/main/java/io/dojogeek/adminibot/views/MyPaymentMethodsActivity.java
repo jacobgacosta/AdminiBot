153da99e1c263a36c1b99d76806b735cd908e76f
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-04-08 19:53:15 -0500

    Create MyPaymentMethodsActivity class

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/MyPaymentMethodsActivity.java
@@ -0,0 +1,15 @@
+package io.dojogeek.adminibot.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.dojogeek.adminibot.R;
+
+public class MyPaymentMethodsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_payment_methods);
+    }
+}
