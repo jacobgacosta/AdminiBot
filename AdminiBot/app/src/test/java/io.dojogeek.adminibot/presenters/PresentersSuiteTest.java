e385ef2d9e20f0709b652f1c5c4b6cdbb3f46028
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-04-08 19:47:01 -0500

    Create PresentersSuiteTest class

---
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/presenters/PresentersSuiteTest.java
@@ -0,0 +1,19 @@
+package io.dojogeek.adminibot.presenters;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import io.dojogeek.adminibot.validators.CompoundValidatorTest;
+import io.dojogeek.adminibot.validators.CompoundValidatorsFactoryTest;
+import io.dojogeek.adminibot.validators.LenghtValidatorTest;
+import io.dojogeek.adminibot.validators.LoginValidatorTest;
+import io.dojogeek.adminibot.validators.RegexValidatorTest;
+import io.dojogeek.adminibot.validators.RequiredValueValidatorTest;
+import io.dojogeek.adminibot.validators.UserValidatorTest;
+import io.dojogeek.adminibot.validators.ValidatorTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({PaymentMethodsPresenterTest.class, ExpensesInboxPresenterTest.class,
+        ExpenseCreationPresenterTest.class})
+public class PresentersSuiteTest {
+}
