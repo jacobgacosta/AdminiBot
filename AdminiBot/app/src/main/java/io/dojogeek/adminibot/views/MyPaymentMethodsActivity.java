82ac37ec93b30ae700fdac6746e2f2d7870f95f7
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-04-10 19:34:31 -0500

    Implements BaseActivity methods

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/MyPaymentMethodsActivity.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/MyPaymentMethodsActivity.java
@@ -3,13 +3,44 @@ package io.dojogeek.adminibot.views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import dagger.AppComponent;
 import io.dojogeek.adminibot.R;
 
-public class MyPaymentMethodsActivity extends AppCompatActivity {
+public class MyPaymentMethodsActivity extends BaseActivity implements MyPaymentMethods {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_payment_methods);
+    }
+
+    @Override
+    protected void setupComponent(AppComponent appComponent) {
+
+    }
+
+    @Override
+    protected void loadViews() {
+
+    }
+
+    @Override
+    protected void addListenersToViews() {
+
+    }
+
+    @Override
+    protected void loadDataView() {
+
+    }
+
+    @Override
+    protected int getLayoutActivity() {
+        return R.layout.activity_my_payment_methods;
+    }
+
+    @Override
+    protected void closeConnections() {
+
     }
 }
