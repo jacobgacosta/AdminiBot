2589c55c6e3fdc4712451d0c3dc8cb3cc1d91f5d
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-04-27 22:21:29 -0500

    Create ArgumentException class

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/exceptions/ArgumentException.java
@@ -0,0 +1,12 @@
+package io.dojogeek.adminibot.exceptions;
+
+/**
+ * Created by norveo on 4/10/16.
+ */
+public class ArgumentException extends Exception {
+
+    public ArgumentException(String message) {
+        super(message);
+    }
+
+}
