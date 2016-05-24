843191b3ca17c859be5af6590d70ac38f54114c4
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-05-24 12:30:35 -0500

    Add Trademark and BankCard to "creationTables" test

--- AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/AdminiBotSQLiteOpenHelperTest.java
+++ AdminiBot/app/src/androidTest/java/io/dojogeek/adminibot/sqlite/AdminiBotSQLiteOpenHelperTest.java
@@ -73,10 +73,10 @@ public class AdminiBotSQLiteOpenHelperTest {
                 ExpensesOthersPaymentMethodsContract.ExpenseOtherPaymentMethod.TABLE_NAME,
                 ExpensesTypesContract.ExpenseType.TABLE_NAME, IncomesContract.Incomes.TABLE_NAME,
                 PaymentMethodsContract.PaymentMethods.TABLE_NAME,
-                /*TypesPaymentMethodsContract.TypePaymentMethod.TABLE_NAME*/ UsersContract.User.TABLE_NAME,
+                TrademarkContract.Trademark.TABLE_NAME, UsersContract.User.TABLE_NAME,
                 IncomesOthersPaymentMethodsContract.IncomePaymentMethod.TABLE_NAME, BankCardsContract.BankCard.TABLE_NAME,
                 IncomesBankCardsContract.IncomesBankCards.TABLE_NAME, ExpensesBankCardsContract.ExpensesBankCard.TABLE_NAME,
-                /*CardTypeContract.CardType.TABLE_NAME*/ CardDetailContract.CardDetail.TABLE_NAME};
+                BanksContract.Bank.TABLE_NAME, CardDetailContract.CardDetail.TABLE_NAME};
 
         for (String table : tables) {
             Cursor cursor = selectTable(table);
