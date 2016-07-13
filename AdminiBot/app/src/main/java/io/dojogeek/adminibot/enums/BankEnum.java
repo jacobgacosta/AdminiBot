1c10f54b5c43ffa62286e7ca8ff4473a8f99f510
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-07-12 22:52:20 -0500

    Change data type for constructor parameters

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/enums/BankEnum.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/enums/BankEnum.java
@@ -1,38 +1,40 @@
 package io.dojogeek.adminibot.enums;
 
 
+import io.dojogeek.adminibot.R;
+
 public enum BankEnum {
 
-    BBVA_BANCOMER("Bancomer", "ic_bbva"),
-    BANAMEX("Banamex", "ic_banamex"),
-    BANORTE("Banorte", "ic_banorte"),
-    SANTANDER("Santander", "ic_santander"),
-    HSBC("HSBC", "ic_hsbc"),
-    INBURSA_WALMART("Wallmart", "ic_wallmart"),
-    SCOTIABANK("Scotia bank", "ic_scotia"),
-    AMERICAN_EXPRESS("American Express", "ic_amex"),
+    BBVA_BANCOMER(R.string.banks_bancomer, "ic_bbva"),
+    BANAMEX(R.string.banks_banamex, "ic_banamex"),
+    BANORTE(R.string.banks_banorte, "ic_banorte"),
+    SANTANDER(R.string.banks_santander, "ic_santander"),
+    HSBC(R.string.banks_hsbc, "ic_hsbc"),
+    INBURSA_WALMART(R.string.banks_wallmart, "ic_wallmart"),
+    SCOTIABANK(R.string.banks_scotiabank, "ic_scotia"),
+    AMERICAN_EXPRESS(R.string.banks_amex, "ic_amex"),
 //    INTERACCIONES("Interacciones", "ic_interacciones"),
-    BANCO_AZTECA("Banco Azteca", "ic_banco_azteca"),
-    BANREGIO("Banregio", "ic_banregio"),
+    BANCO_AZTECA(R.string.banks_bnk_azteca, "ic_banco_azteca"),
+    BANREGIO(R.string.banks_banregio, "ic_banregio"),
 //    MULTIVA("Multiva", "ic_multiva"),
 //    MIFIEL("Mifiel", "ic_mifiel"),
 //    COMPARTAMOS("Compartamos", "ic_compartamos"),
 //    AFIRME("Afirme", "ic_afirme"),
 //    VE_POR_MAS("Ve por más", "ic_ve_por_mas"),
 //    INVEX("Invex", "ic_invex"),
-    BANCOPPEL("Bancoppel", "ic_bancoppel"),
-    BANCO_AHORRO_FAMSA("Banco ahorro famsa", "ic_baf");
+    BANCOPPEL(R.string.banks_bancoopel, "ic_bancoppel"),
+    BANCO_AHORRO_FAMSA(R.string.banks_famsa, "ic_baf");
 //    BANSI("Bansi", "ic_bansi");
 
-    private String mName;
+    private int mName;
     private String mImageName;
 
-    private BankEnum(String name, String imageName) {
+    private BankEnum(int name, String imageName) {
         mName = name;
         mImageName = imageName;
     }
 
-    public String getName() {
+    public int getName() {
         return mName;
     }
 
