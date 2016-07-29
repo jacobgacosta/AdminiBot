5132d1432b080b54164eb204a4bd2915079c8c1a
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-07-29 12:19:39 -0500

    Add inject method for CashActivity

--- AdminiBot/app/src/main/java/dagger/AdminiBotComponent.java
+++ AdminiBot/app/src/main/java/dagger/AdminiBotComponent.java
@@ -2,6 +2,7 @@ package dagger;
 
 import io.dojogeek.adminibot.views.AddPaymentMethodActivity;
 import io.dojogeek.adminibot.views.CardCreationActivity;
+import io.dojogeek.adminibot.views.CashActivity;
 import io.dojogeek.adminibot.views.CreditCardActivity;
 import io.dojogeek.adminibot.views.PaymentMethodsActivity;
 import io.dojogeek.adminibot.views.RegisterExpenseActivity;
@@ -28,4 +29,6 @@ public interface AdminiBotComponent {
     void inject(CardCreationActivity activity);
 
     void inject(CreditCardActivity activity);
+
+    void inject(CashActivity activity);
 }
