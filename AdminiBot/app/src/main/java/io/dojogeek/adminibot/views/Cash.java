7a8c29d3b4ca249bbcdf23e80716eca25ffd01f2
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-08-05 12:45:35 -0500

    Define cash actions methods

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/Cash.java
@@ -0,0 +1,10 @@
+package io.dojogeek.adminibot.views;
+
+public interface Cash {
+
+    void notifySuccessfulInsertion();
+
+    void notifyErrorInsertion();
+
+    void returnToMyPaymentsMethods();
+}
