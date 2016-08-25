a22272e3e6e43373f1353168bd8b68046662eacc
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-08-24 21:59:45 -0500

    Change CashDao to OtherPaymentMethodDao

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/presenters/CashPresenterImpl.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/presenters/CashPresenterImpl.java
@@ -4,26 +4,28 @@ import android.content.Context;
 import android.database.SQLException;
 
 import io.dojogeek.adminibot.daos.CashDao;
+import io.dojogeek.adminibot.daos.OtherPaymentMethodDao;
 import io.dojogeek.adminibot.models.CashModel;
+import io.dojogeek.adminibot.models.OtherPaymentMethodModel;
 import io.dojogeek.adminibot.views.Cash;
 
 public class CashPresenterImpl implements CashPresenter {
 
     private Cash mCash;
-    private CashDao mCashDao;
+    private OtherPaymentMethodDao mOtherPaymentMethodDao;
 
 
-    public CashPresenterImpl(Cash cash, CashDao cashDao) {
+    public CashPresenterImpl(Cash cash, OtherPaymentMethodDao otherPaymentMethodDao) {
         this.mCash = cash;
-        this.mCashDao = cashDao;
+        this.mOtherPaymentMethodDao = otherPaymentMethodDao;
     }
 
     @Override
-    public void createCash(CashModel cashModel) {
+    public void createCash(OtherPaymentMethodModel otherPaymentMethodModel) {
 
         try {
 
-            mCashDao.createCash(cashModel);
+            mOtherPaymentMethodDao.createOtherPaymentMethod(otherPaymentMethodModel);
             mCash.notifySuccessfulInsertion();
             mCash.returnToMyPaymentsMethods();
 
