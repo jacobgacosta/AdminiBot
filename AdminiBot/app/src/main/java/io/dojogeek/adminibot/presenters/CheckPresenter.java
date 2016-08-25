46a6605fc3e66d6bfc09e8c23b6c035b9f85cbe9
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-08-24 22:00:22 -0500

    Create CheckPresenter interface

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/presenters/CheckPresenter.java
@@ -0,0 +1,9 @@
+package io.dojogeek.adminibot.presenters;
+
+import io.dojogeek.adminibot.models.OtherPaymentMethodModel;
+
+public interface CheckPresenter {
+
+    void createCheck(OtherPaymentMethodModel otherPaymentMethodModel);
+
+}
