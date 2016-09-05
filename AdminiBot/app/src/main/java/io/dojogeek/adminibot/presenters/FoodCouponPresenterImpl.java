e55086302138c8fbf82495a4cb21f132e88c51f4
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-09-05 17:13:50 -0500

    Implement createFoodCoupon method

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/presenters/FoodCouponPresenterImpl.java
@@ -0,0 +1,34 @@
+package io.dojogeek.adminibot.presenters;
+
+import android.database.SQLException;
+
+import io.dojogeek.adminibot.daos.OtherPaymentMethodDao;
+import io.dojogeek.adminibot.models.OtherPaymentMethodModel;
+import io.dojogeek.adminibot.views.FoodCoupons;
+
+public class FoodCouponPresenterImpl implements FoodCouponPresenter {
+
+    private FoodCoupons mFoodCoupons;
+    private OtherPaymentMethodDao mOtherPaymentMethodDao;
+
+    public FoodCouponPresenterImpl(FoodCoupons foodCoupons, OtherPaymentMethodDao otherPaymentMethodDao) {
+        mFoodCoupons = foodCoupons;
+        mOtherPaymentMethodDao = otherPaymentMethodDao;
+    }
+
+    @Override
+    public void createFoodCoupon(OtherPaymentMethodModel otherPaymentMethodModel) {
+
+        try {
+
+            mOtherPaymentMethodDao.createOtherPaymentMethod(otherPaymentMethodModel);
+            mFoodCoupons.notifySuccessfulInsertion();
+            mFoodCoupons.returnToMyPaymentsMethods();
+
+        } catch (SQLException exception) {
+
+            mFoodCoupons.notifyErrorInsertion();
+        }
+
+    }
+}
