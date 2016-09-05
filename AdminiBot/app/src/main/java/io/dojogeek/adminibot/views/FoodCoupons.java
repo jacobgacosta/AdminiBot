5659ebfa9fa6b58140acc5122bb0e9a16c3f49ac
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-09-05 17:14:15 -0500

    Add FoodCoupons actions

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/FoodCoupons.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/FoodCoupons.java
@@ -1,4 +1,11 @@
 package io.dojogeek.adminibot.views;
 
 public interface FoodCoupons {
+
+    void notifySuccessfulInsertion();
+
+    void notifyErrorInsertion();
+
+    void returnToMyPaymentsMethods();
+
 }
