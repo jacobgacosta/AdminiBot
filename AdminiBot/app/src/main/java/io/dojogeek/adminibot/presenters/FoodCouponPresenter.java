d4bbb2bd6a0a4f330e98c3503b17892116cecf57
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-09-05 17:13:35 -0500

    Create FoodCouponPresenter

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/presenters/FoodCouponPresenter.java
@@ -0,0 +1,9 @@
+package io.dojogeek.adminibot.presenters;
+
+import io.dojogeek.adminibot.models.OtherPaymentMethodModel;
+
+public interface FoodCouponPresenter {
+
+    void createFoodCoupon(OtherPaymentMethodModel otherPaymentMethodModel);
+
+}
