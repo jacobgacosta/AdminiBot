fdb621675ae885494f09e2efd138ffd93b6a8e68
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-09-21 14:33:15 -0500

    Add "LenghtValidator" constructor

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/validators/LenghtValidator.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/validators/LenghtValidator.java
@@ -6,15 +6,25 @@ import io.dojogeek.adminibot.exceptions.ValidatorNullValueException;
 
 public class LenghtValidator implements DataValidator<String> {
 
-    private int maxLenght = 0;
+    private int maxLength = 0;
+    private int minLength = 0;
     private int errorMsg;
 
-    public LenghtValidator(int maxLenght) {
-        this.maxLenght = maxLenght;
+    public LenghtValidator(int maxLength) {
+        this.maxLength = maxLength;
     }
 
-    public static LenghtValidator withMaxLenght(int maxLenght) {
-        return new LenghtValidator(maxLenght);
+    public LenghtValidator(int minLength, int maxLength) {
+        this.minLength = minLength;
+        this.maxLength = maxLength;
+    }
+
+    public static LenghtValidator withMaxLength(int maxLenght) {
+        return new LenghtValidator(0, maxLenght);
+    }
+
+    public static LenghtValidator withMinAndMaxLength(int minLength, int maxLength) {
+        return new LenghtValidator(minLength, maxLength);
     }
 
     public LenghtValidator withErroMessage(int errorMessage) {
@@ -27,7 +37,7 @@ public class LenghtValidator implements DataValidator<String> {
 
         if (value == null) {
             throw new ValidatorNullValueException("The value can't be null");
-        } else if (value.trim().length() <= maxLenght) {
+        } else if (value.trim().length() <= maxLength) {
             return  true;
         }
 
