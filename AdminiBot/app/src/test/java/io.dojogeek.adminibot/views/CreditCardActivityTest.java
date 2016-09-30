492076e9ce23a667fba12985c7ef2c7c20865e3d
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-09-30 12:57:27 -0500

    Add test case to validate "success submit"

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/views/CreditCardActivityTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/views/CreditCardActivityTest.java
@@ -1,23 +1,24 @@
 package io.dojogeek.adminibot.views;
 
-import android.support.v7.app.AppCompatDialogFragment;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import dagger.AdminiBotComponent;
 import dagger.AdminiBotModule;
@@ -25,6 +26,12 @@ import dagger.AppComponent;
 import io.dojogeek.adminibot.R;
 import io.dojogeek.adminibot.components.DatePickerFragment;
 import io.dojogeek.adminibot.components.SpinnerWithInternalImage;
+import io.dojogeek.adminibot.enums.CardTypeEnum;
+import io.dojogeek.adminibot.models.BankCardModel;
+import io.dojogeek.adminibot.models.CardDetailModel;
+import io.dojogeek.adminibot.models.CreditCardModel;
+import io.dojogeek.adminibot.presenters.CreditCardPresenter;
+import io.dojogeek.adminibot.validators.CreditCardValidator;
 
 import static org.junit.Assert.assertTrue;
 import static org.hamcrest.CoreMatchers.not;
@@ -36,9 +43,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.powermock.api.mockito.PowerMockito.doNothing;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 @RunWith(PowerMockRunner.class)
@@ -73,10 +77,16 @@ public class CreditCardActivityTest {
     private LinearLayout mBrandsCards;
 
     @Mock
-    private ImageView mCardBrand;
+    private RadioGroup mCardBrands;
+
+    @Mock
+    private RadioButton mOption;
 
     @Mock
     private Button mButton;
+
+    @Mock
+    private CreditCardPresenter mCreditCardPresenter;
 
     @InjectMocks
     @Spy
@@ -118,9 +128,9 @@ public class CreditCardActivityTest {
         doReturn(mSpinnerWithInternalImage).when(mCreditCardActivity).findViewById(R.id.banks);
         doReturn(mCreditLimit).when(mCreditCardActivity).findViewById(R.id.credit_limit);
         doReturn(mBrandsCards).when(mCreditCardActivity).findViewById(R.id.cards_brands);
-        doReturn(mCardBrand).when(mCreditCardActivity).findViewById(R.id.visa);
-        doReturn(mCardBrand).when(mCreditCardActivity).findViewById(R.id.mastercard);
-        doReturn(mCardBrand).when(mCreditCardActivity).findViewById(R.id.amex);
+        doReturn(mCardBrands).when(mCreditCardActivity).findViewById(R.id.cards_brands);
+
+
         doReturn(mCuttoffDate).when(mCreditCardActivity).findViewById(R.id.cuttoff_date);
         doReturn(mPayDayLimit).when(mCreditCardActivity).findViewById(R.id.pay_day_limit);
         doReturn(mCurrentBalance).when(mCreditCardActivity).findViewById(R.id.current_balance);
@@ -133,9 +143,6 @@ public class CreditCardActivityTest {
         verify(mCreditCardActivity).findViewById(R.id.banks);
         verify(mCreditCardActivity).findViewById(R.id.credit_limit);
         verify(mCreditCardActivity).findViewById(R.id.cards_brands);
-        verify(mCreditCardActivity).findViewById(R.id.visa);
-        verify(mCreditCardActivity).findViewById(R.id.mastercard);
-        verify(mCreditCardActivity).findViewById(R.id.amex);
         verify(mCreditCardActivity).findViewById(R.id.cuttoff_date);
         verify(mCreditCardActivity).findViewById(R.id.create_credit_card);
         verify(mCreditCardActivity).findViewById(R.id.pay_day_limit);
@@ -150,6 +157,7 @@ public class CreditCardActivityTest {
 
         verify(mCuttoffDate).setOnClickListener(mCreditCardActivity);
         verify(mPayDayLimit).setOnClickListener(mCreditCardActivity);
+        verify(mButton).setOnClickListener(mCreditCardActivity);
     }
 
     @Test
@@ -201,5 +209,118 @@ public class CreditCardActivityTest {
         mCreditCardActivity.loadDataView();
 
         verify(mCreditCardActivity).setTitle(R.string.title_activity_add_card);
+    }
+
+    @Test
+    public void testSubmitCreditCard_successSubmit() throws Exception {
+
+        int submitButton = R.id.create_credit_card;
+        when(mButton.getId()).thenReturn(submitButton);
+
+        Editable editableCardName = mock(Editable.class);
+        when(mCardName.getText()).thenReturn(editableCardName);
+        String cardName = "Test card";
+        when(editableCardName.toString()).thenReturn(cardName);
+
+        Editable editableCardNumber = mock(Editable.class);
+        when(mCardNumber.getText()).thenReturn(editableCardNumber);
+        String cardNumber = "879";
+        when(editableCardNumber.toString()).thenReturn(cardNumber);
+
+        int checkedRadioButtonId = R.id.visa;
+        when(mCardBrands.getCheckedRadioButtonId()).thenReturn(checkedRadioButtonId);
+//        doReturn(mOption).when(mCreditCardActivity).findViewById(checkedRadioButtonId);
+
+        Long bankId = 1L;
+        when(mSpinnerWithInternalImage.getSelectedItem()).thenReturn(bankId);
+
+        Editable editableCreditLimit = mock(Editable.class);
+        when(mCreditLimit.getText()).thenReturn(editableCreditLimit);
+        String creditLimit = "98744.90";
+        when(editableCreditLimit.toString()).thenReturn(creditLimit);
+
+        Editable editableCurrentBalance = mock(Editable.class);
+        when(mCurrentBalance.getText()).thenReturn(editableCurrentBalance);
+        String currentBalance = "9894.76";
+        when(editableCurrentBalance.toString()).thenReturn(currentBalance);
+
+        Editable editableCuttoffDate = mock(Editable.class);
+        when(mCuttoffDate.getText()).thenReturn(editableCuttoffDate);
+        String cuttoffDate = "2016-9-12";
+        when(editableCuttoffDate.toString()).thenReturn(cuttoffDate);
+
+        Editable editablePayDayLimit = mock(Editable.class);
+        when(mPayDayLimit.getText()).thenReturn(editablePayDayLimit);
+        String payDayLimit = "2016-10-18";
+        when(editablePayDayLimit.toString()).thenReturn(payDayLimit);
+
+        CreditCardValidator creditCardValidatorMock = mock(CreditCardValidator.class);
+        whenNew(CreditCardValidator.class).withNoArguments().thenReturn(creditCardValidatorMock);
+        when(creditCardValidatorMock.validate()).thenReturn(true);
+
+        BankCardModel bankCardModelMock = mock(BankCardModel.class);
+        whenNew(BankCardModel.class).withNoArguments().thenReturn(bankCardModelMock);
+
+        CardDetailModel cardDetailModelMock = mock(CardDetailModel.class);
+        whenNew(CardDetailModel.class).withNoArguments().thenReturn(cardDetailModelMock);
+
+        CreditCardModel creditCardModelMock = mock(CreditCardModel.class);
+        whenNew(CreditCardModel.class).withNoArguments().thenReturn(creditCardModelMock);
+
+        mCreditCardActivity.onClick(mButton);
+
+        verify(mButton).getId();
+        verify(mCardName, times(2)).getText();
+        verify(mCardNumber, times(2)).getText();
+        verify(mCardBrands, times(2)).getCheckedRadioButtonId();
+        verify(mSpinnerWithInternalImage, times(2)).getSelectedItem();
+        verify(mCreditLimit, times(3)).getText();
+        verify(mCurrentBalance, times(3)).getText();
+        verify(mCuttoffDate, times(2)).getText();
+        verify(mPayDayLimit, times(2)).getText();
+        verify(creditCardValidatorMock).setCardNumber(cardNumber);
+        verify(creditCardValidatorMock).setPaydayLimit(payDayLimit);
+        verify(creditCardValidatorMock).setCuttoffDate(cuttoffDate);
+        verify(creditCardValidatorMock).setCurrentBalance(currentBalance);
+        verify(creditCardValidatorMock).setCardName(cardName);
+        verify(creditCardValidatorMock).setCreditCardBrand("VISA");
+        verify(creditCardValidatorMock).setCreditLimit(creditLimit);
+        verify(bankCardModelMock).setName(cardName);
+        verify(bankCardModelMock).setBrand("VISA");
+        verify(bankCardModelMock).setNumber(cardNumber);
+        verify(bankCardModelMock).setBankId(bankId);
+        verify(bankCardModelMock).setAvailableCredit(calculateAvailableCredit(currentBalance, creditLimit));
+        verify(bankCardModelMock).setCardType(CardTypeEnum.CREDIT_CARD);
+        verify(cardDetailModelMock).setCuttoffDate(cuttoffDate);
+        verify(cardDetailModelMock).setPayDayLimit(payDayLimit);
+        verify(cardDetailModelMock).setCurrentBalance(new Double(currentBalance));
+        verify(cardDetailModelMock).setCreditLimit(new Double(creditLimit));
+        verify(creditCardModelMock).setBankCardModel(bankCardModelMock);
+        verify(creditCardModelMock).setCardDetailModel(cardDetailModelMock);
+        verify(mCreditCardPresenter).createCreditCard(creditCardModelMock);
+    }
+
+    @Test
+    public void testLoadDataView_buildBanksSpinner() {
+
+        Mockito.doNothing().when(mCreditCardActivity).setTitle(R.string.title_activity_add_card);
+
+        mCreditCardActivity.loadDataView();
+
+        verify(mCreditCardPresenter).loadBanks();
+    }
+
+    @Test
+    public void testFillBankItemsSpinner() {
+
+        Map<Long, Map<String, Integer>> items = new HashMap<>();
+
+        mCreditCardActivity.fillBankItemsSpinner(items);
+
+        verify(mSpinnerWithInternalImage).createSpinner(R.string.add_card_banks_hint, items);
+    }
+
+    private double calculateAvailableCredit(String currentBalance, String creditLimit) {
+        return new Double(creditLimit) - new Double(currentBalance);
     }
 }
