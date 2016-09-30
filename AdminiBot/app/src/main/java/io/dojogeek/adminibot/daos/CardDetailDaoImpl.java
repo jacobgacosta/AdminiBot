30c294f5a19bd1c40fcfd93edf0d0583c20672a5
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-09-30 12:58:13 -0500

    Remove "bank card id" field

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/CardDetailDaoImpl.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/CardDetailDaoImpl.java
@@ -90,7 +90,6 @@ public class CardDetailDaoImpl extends SQLiteGlobalDao implements CardDetailDao 
         contentValues.put(CardDetailContract.CardDetail.COLUMN_CURRENT_BALANCE, cardDetailModel.getCurrentBalance());
         contentValues.put(CardDetailContract.CardDetail.COLUMN_CUTOFF_DATE, cardDetailModel.getCuttoffDate());
         contentValues.put(CardDetailContract.CardDetail.COLUMN_PAY_DAY_LIMIT, cardDetailModel.getPayDayLimit());
-        contentValues.put(CardDetailContract.CardDetail.COLUMN_BANK_CARD_ID, cardDetailModel.getBankCardId());
 
         return contentValues;
     }
@@ -104,14 +103,12 @@ public class CardDetailDaoImpl extends SQLiteGlobalDao implements CardDetailDao 
         double currentBalance = cursor.getDouble(cursor.getColumnIndex(CardDetailContract.CardDetail.COLUMN_CURRENT_BALANCE));
         String cutoffDate = cursor.getString(cursor.getColumnIndex(CardDetailContract.CardDetail.COLUMN_CUTOFF_DATE));
         String payDayLimit = cursor.getString(cursor.getColumnIndex(CardDetailContract.CardDetail.COLUMN_PAY_DAY_LIMIT));
-        long bankCardId = cursor.getLong(cursor.getColumnIndex(CardDetailContract.CardDetail.COLUMN_BANK_CARD_ID));
 
         cardDetailModel.setId(id);
         cardDetailModel.setCreditLimit(creditLimit);
         cardDetailModel.setCurrentBalance(currentBalance);
         cardDetailModel.setCuttoffDate(cutoffDate);
         cardDetailModel.setPayDayLimit(payDayLimit);
-        cardDetailModel.setBankCardId(bankCardId);
 
         return cardDetailModel;
     }
