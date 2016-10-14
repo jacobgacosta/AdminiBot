4df7a36edfaec2626ba8aa4239b36218d107bd15
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-10-14 12:29:30 -0500

    Implement CreditCardActivity

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/CreditCard.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/CreditCard.java
@@ -1,7 +1,18 @@
 package io.dojogeek.adminibot.views;
 
-/**
- * Created by norveo on 5/12/16.
- */
+import java.util.List;
+import java.util.Map;
+
+import io.dojogeek.adminibot.models.BankModel;
+
 public interface CreditCard {
+
+    void notifySuccessfulInsertion();
+
+    void notifyErrorInsertion();
+
+    void fillBankItemsSpinner(Map<Long, Map<String, Integer>> items);
+
+    void returnToMyPaymentsMethods();
+
 }
--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/CreditCardActivity.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/CreditCardActivity.java
@@ -4,16 +4,22 @@ import android.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import javax.inject.Inject;
 
 import dagger.AdminiBotComponent;
 import dagger.AdminiBotModule;
@@ -22,9 +28,22 @@ import io.dojogeek.adminibot.R;
 import io.dojogeek.adminibot.components.DatePickerFragment;
 import io.dojogeek.adminibot.components.SpinnerWithInternalImage;
 import io.dojogeek.adminibot.enums.BankEnum;
+import io.dojogeek.adminibot.enums.CardTypeEnum;
+import io.dojogeek.adminibot.models.BankCardModel;
+import io.dojogeek.adminibot.models.BankModel;
+import io.dojogeek.adminibot.models.CardDetailModel;
+import io.dojogeek.adminibot.models.CreditCardModel;
+import io.dojogeek.adminibot.presenters.CreditCardPresenter;
+import io.dojogeek.adminibot.utils.LaunchIntents;
+import io.dojogeek.adminibot.validators.CreditCardValidator;
 
 public class CreditCardActivity extends BaseActivity implements CreditCard, View.OnClickListener {
 
+    public static final int SUCCESS_INSERTION_CREDIT_CARD = R.string.success_insertion_cc;
+    public static final int ERROR_INSERTION_CREDIT_CARD = R.string.error_insertion_cc;
+    private static final String VISA = "VISA";
+    private static final String MASTER_CARD = "MASTER CARD";
+    private static final String AMEX = "AMEX";
     private EditText mCardName;
     private EditText mCardNumber;
     private EditText mCurrentBalance;
@@ -32,12 +51,59 @@ public class CreditCardActivity extends BaseActivity implements CreditCard, View
     private EditText mCreditLimit;
     private EditText mCuttoffDate;
     private SpinnerWithInternalImage mSpinnerWithInternalImage;
-    private RadioButton mVisa;
-    private RadioButton mMasterCard;
-    private RadioButton mAmex;
-    private LinearLayout mCardsBrands;
+    private RadioButton mOption;
+    private RadioGroup mCardBrands;
     private static String DATEPICKER_TAG = "datePicker";
     private Button mCreateCreditCard;
+
+    @Inject
+    public CreditCardPresenter mCreditCardPresenter;
+
+    @Override
+    public void onClick(View view) {
+
+        int viewId = view.getId();
+
+        switch (viewId) {
+            case R.id.cuttoff_date:
+                DatePickerFragment cuttoffDateFragment = new DatePickerFragment();
+                cuttoffDateFragment.setIdWidgetContainerDate(R.id.cuttoff_date);
+                cuttoffDateFragment.show(getSupportFragmentManager(), DATEPICKER_TAG);
+                break;
+            case R.id.pay_day_limit:
+                DatePickerFragment payDayLimitFragment = new DatePickerFragment();
+                payDayLimitFragment.setIdWidgetContainerDate(R.id.pay_day_limit);
+                payDayLimitFragment.show(getSupportFragmentManager(), DATEPICKER_TAG);
+                break;
+            case R.id.create_credit_card:
+                processInformation();
+                break;
+            default:
+                Log.v(this.getClass().getName(), "No operations for this event");
+                break;
+        }
+    }
+
+    @Override
+    public void notifySuccessfulInsertion() {
+        showMessage(CreditCardActivity.SUCCESS_INSERTION_CREDIT_CARD);
+    }
+
+    @Override
+    public void notifyErrorInsertion() {
+        showMessage(CreditCardActivity.ERROR_INSERTION_CREDIT_CARD);
+    }
+
+    @Override
+    public void fillBankItemsSpinner(Map<Long, Map<String, Integer>> items) {
+
+        mSpinnerWithInternalImage.createSpinner(R.string.add_card_banks_hint, items);
+    }
+
+    @Override
+    public void returnToMyPaymentsMethods() {
+        LaunchIntents.launchIntentClearTop(this, MainActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,10 +122,7 @@ public class CreditCardActivity extends BaseActivity implements CreditCard, View
         mCardNumber = (EditText) findViewById(R.id.card_number);
         mSpinnerWithInternalImage = (SpinnerWithInternalImage) findViewById(R.id.banks);
         mCreditLimit = (EditText) findViewById(R.id.credit_limit);
-        mCardsBrands = (LinearLayout) findViewById(R.id.cards_brands);
-        mVisa = (RadioButton) findViewById(R.id.visa);
-        mMasterCard = (RadioButton) findViewById(R.id.mastercard);
-        mAmex = (RadioButton) findViewById(R.id.amex);
+        mCardBrands = (RadioGroup) findViewById(R.id.cards_brands);
         mCuttoffDate = (EditText) findViewById(R.id.cuttoff_date);
         mCurrentBalance = (EditText) findViewById(R.id.current_balance);
         mPayDayLimit = (EditText) findViewById(R.id.pay_day_limit);
@@ -70,6 +133,7 @@ public class CreditCardActivity extends BaseActivity implements CreditCard, View
     protected void addListenersToViews() {
         mCuttoffDate.setOnClickListener(this);
         mPayDayLimit.setOnClickListener(this);
+        mCreateCreditCard.setOnClickListener(this);
     }
 
     @Override
@@ -77,9 +141,7 @@ public class CreditCardActivity extends BaseActivity implements CreditCard, View
 
         setTitle(R.string.title_activity_add_card);
 
-        Map<String, Integer> map = createItemsMapFromBankEnum(BankEnum.values());
-
-        mSpinnerWithInternalImage.createSpinner(R.string.add_card_banks_hint, map);
+        mCreditCardPresenter.loadBanks();
 
     }
 
@@ -90,40 +152,154 @@ public class CreditCardActivity extends BaseActivity implements CreditCard, View
 
     @Override
     protected void closeConnections() {
-
+        mCreditCardPresenter.unnusedView();
     }
 
-    private Map<String, Integer> createItemsMapFromBankEnum(BankEnum[] banks) {
+    private void processInformation() {
 
-        Map<String, Integer> banksMap = new HashMap<>();
+        CreditCardValidator creditCardValidator = applyFieldValidators();
 
-        banksMap.put("ic_amex", R.string.banks_amex);
-        banksMap.put("ic_visa", R.string.banks_banamex);
-        banksMap.put("ic_mastercard", R.string.banks_bancoopel);
-//        for (BankEnum bank : banks) {
-//
-//        }
+        boolean isValidCreditCard = creditCardValidator.validate();
 
-        return banksMap;
-    }
-
-    @Override
-    public void onClick(View view) {
-
-        int viewId = view.getId();
-
-        switch (viewId) {
-            case R.id.cuttoff_date:
-                DatePickerFragment cuttoffDateFragment = new DatePickerFragment();
-                cuttoffDateFragment.setIdWidgetContainerDate(R.id.cuttoff_date);
-                cuttoffDateFragment.show(getSupportFragmentManager(), DATEPICKER_TAG);
-                break;
-            case R.id.pay_day_limit:
-                DatePickerFragment payDayLimitFragment = new DatePickerFragment();
-                payDayLimitFragment.setIdWidgetContainerDate(R.id.pay_day_limit);
-                payDayLimitFragment.show(getSupportFragmentManager(), DATEPICKER_TAG);
-                break;
+        if (isValidCreditCard) {
+            processValidCreditCard();
+        } else {
+            showErrors(creditCardValidator);
         }
 
     }
+
+    private CreditCardValidator applyFieldValidators() {
+
+        String cardName = mCardName.getText().toString();
+        String cardNumber = mCardNumber.getText().toString();
+        String cardBrand = getRadioButtonTextFromId(mCardBrands.getCheckedRadioButtonId());
+        Long bankId = (Long) mSpinnerWithInternalImage.getSelectedItem();
+
+        String bank = "";
+
+        if (bankId != null) {
+            bank = bankId.toString();
+        }
+
+        String creditLimit = mCreditLimit.getText().toString();
+        String currentBalance = mCurrentBalance.getText().toString();
+        String cuttoffDate = mCuttoffDate.getText().toString();
+        String payDayLimit = mPayDayLimit.getText().toString();
+
+        CreditCardValidator creditCardValidator = new CreditCardValidator();
+        creditCardValidator.setCardName(cardName);
+        creditCardValidator.setCardNumber(cardNumber);
+        creditCardValidator.setCreditCardBrand(cardBrand);
+        creditCardValidator.setBank(bank);
+        creditCardValidator.setCreditLimit(creditLimit);
+        creditCardValidator.setCurrentBalance(currentBalance);
+        creditCardValidator.setCuttoffDate(cuttoffDate);
+        creditCardValidator.setPaydayLimit(payDayLimit);
+
+        return creditCardValidator;
+
+    }
+
+    private String getRadioButtonTextFromId(int radioButtonId) {
+
+        String brandMark = null;
+
+        switch (radioButtonId) {
+            case R.id.visa:
+                brandMark = VISA;
+                break;
+            case R.id.mastercard:
+                brandMark = MASTER_CARD;
+                break;
+            case R.id.amex:
+                brandMark = AMEX;
+                break;
+            default:
+                Log.v(this.getClass().getName(), "No operations for this event");
+                break;
+        }
+        return brandMark;
+    }
+
+    private void processValidCreditCard() {
+
+        BankCardModel bankCardModel = buildBankCardModel();
+
+        CardDetailModel cardDetailModel = buildCardDetailModel();
+
+        CreditCardModel creditCardModel = new CreditCardModel();
+        creditCardModel.setBankCardModel(bankCardModel);
+        creditCardModel.setCardDetailModel(cardDetailModel);
+
+        submitCreditCard(creditCardModel);
+    }
+
+    private void submitCreditCard(CreditCardModel creditCardModel) {
+
+        mCreditCardPresenter.createCreditCard(creditCardModel);
+
+    }
+
+    private CardDetailModel buildCardDetailModel() {
+
+        CardDetailModel cardDetailModel = new CardDetailModel();
+        cardDetailModel.setCuttoffDate(mCuttoffDate.getText().toString());
+        cardDetailModel.setPayDayLimit(mPayDayLimit.getText().toString());
+        cardDetailModel.setCurrentBalance(new Double(mCurrentBalance.getText().toString()));
+        cardDetailModel.setCreditLimit(new Double(mCreditLimit.getText().toString()));
+
+        return cardDetailModel;
+    }
+
+    private BankCardModel buildBankCardModel() {
+
+        BankCardModel bankCardModel = new BankCardModel();
+        bankCardModel.setName(mCardName.getText().toString());
+        bankCardModel.setNumber(mCardNumber.getText().toString());
+        bankCardModel.setBrand(getRadioButtonTextFromId(mCardBrands.getCheckedRadioButtonId()));
+        bankCardModel.setBankId((Long) mSpinnerWithInternalImage.getSelectedItem());
+        bankCardModel.setAvailableCredit(calculateAvailableCredit(mCurrentBalance.getText().toString(),
+                mCreditLimit.getText().toString()));
+        bankCardModel.setCardType(CardTypeEnum.CREDIT_CARD);
+
+        return bankCardModel;
+    }
+
+    private double calculateAvailableCredit(String currentBalance, String creditLimit) {
+        return new Double(creditLimit) - new Double(currentBalance);
+    }
+
+    private void showErrors(CreditCardValidator creditCardValidator) {
+        if (!creditCardValidator.isValidCardName()) {
+            showError(mCardName, creditCardValidator.getCardNameError());
+        } else if (!creditCardValidator.isValidCardNumber()) {
+            showError(mCardNumber, creditCardValidator.getCardNumberError());
+        } else if (!creditCardValidator.isValidCardBrand()) {
+            mCardBrands.requestFocus();
+            Toast.makeText(this, R.string.error_wrong_brand_card, Toast.LENGTH_LONG).show();
+        } else if (!creditCardValidator.isValidCardBank()) {
+            mSpinnerWithInternalImage.requestFocus();
+            Toast.makeText(this, R.string.error_wrong_bank, Toast.LENGTH_LONG).show();
+        } else if (!creditCardValidator.isValidCreditLimit()) {
+            showError(mCreditLimit, creditCardValidator.getCreditLimitError());
+        } else if (!creditCardValidator.isValidCurrentBalance()) {
+            showError(mCurrentBalance, creditCardValidator.getCurrentBalanceError());
+        } else if (!creditCardValidator.isValidCuttoffDate()) {
+            showError(mCuttoffDate, creditCardValidator.getCuttoffDateError());
+        } else if (!creditCardValidator.isValidPayDayLimit()) {
+            showError(mPayDayLimit, creditCardValidator.getPayDayLimitError());
+        }
+
+    }
+
+    private void showError(EditText editText, int resourceErrorMessage) {
+        editText.setError(getString(resourceErrorMessage));
+        editText.requestFocus();
+    }
+
+    private void showMessage(int resourceMessage) {
+        Toast.makeText(this, resourceMessage, Toast.LENGTH_LONG).show();
+    }
+
 }
