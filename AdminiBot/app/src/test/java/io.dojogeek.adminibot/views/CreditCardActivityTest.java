fc5c6177d9f5cbc205cbcf44ce43daa84914569f
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-10-14 12:07:08 -0500

    Implement "credit card" tests

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/views/CreditCardActivityTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/views/CreditCardActivityTest.java
@@ -7,6 +7,8 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +16,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -31,22 +34,31 @@ import io.dojogeek.adminibot.models.BankCardModel;
 import io.dojogeek.adminibot.models.CardDetailModel;
 import io.dojogeek.adminibot.models.CreditCardModel;
 import io.dojogeek.adminibot.presenters.CreditCardPresenter;
+import io.dojogeek.adminibot.utils.LaunchIntents;
 import io.dojogeek.adminibot.validators.CreditCardValidator;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.notNull;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.spy;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
+import static org.powermock.api.support.membermodification.MemberMatcher.method;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(CreditCardActivity.class)
+@PrepareForTest({Toast.class, CreditCardActivity.class, LaunchIntents.class})
 public class CreditCardActivityTest {
 
     private static final int NO_RESOURCE = 0;
@@ -73,8 +85,8 @@ public class CreditCardActivityTest {
     @Mock
     private SpinnerWithInternalImage mSpinnerWithInternalImage;
 
-    @Mock
-    private LinearLayout mBrandsCards;
+//    @Mock
+//    private LinearLayout mBrandsCards;
 
     @Mock
     private RadioGroup mCardBrands;
@@ -93,11 +105,12 @@ public class CreditCardActivityTest {
     private CreditCardActivity mCreditCardActivity = new CreditCardActivity();
 
     @Test
-    public void  testGetLayout_returnCorrectLayout() {
+    public void testGetLayout_returnCorrectLayout() {
 
         int actualLayout = mCreditCardActivity.getLayoutActivity();
 
         assertThat(actualLayout, is(not(NO_RESOURCE)));
+        assertEquals(R.layout.activity_credit_card, actualLayout);
 
     }
 
@@ -127,7 +140,7 @@ public class CreditCardActivityTest {
         doReturn(mCardNumber).when(mCreditCardActivity).findViewById(R.id.card_number);
         doReturn(mSpinnerWithInternalImage).when(mCreditCardActivity).findViewById(R.id.banks);
         doReturn(mCreditLimit).when(mCreditCardActivity).findViewById(R.id.credit_limit);
-        doReturn(mBrandsCards).when(mCreditCardActivity).findViewById(R.id.cards_brands);
+//        doReturn(mBrandsCards).when(mCreditCardActivity).findViewById(R.id.cards_brands);
         doReturn(mCardBrands).when(mCreditCardActivity).findViewById(R.id.cards_brands);
 
 
@@ -165,7 +178,7 @@ public class CreditCardActivityTest {
 
         View mockView = mock(View.class);
 
-        int viewId =  R.id.cuttoff_date;
+        int viewId = R.id.cuttoff_date;
 
         DatePickerFragment mockDatePickerFragment = mock(DatePickerFragment.class);
 
@@ -186,7 +199,7 @@ public class CreditCardActivityTest {
 
         View mockView = mock(View.class);
 
-        int viewId =  R.id.pay_day_limit;
+        int viewId = R.id.pay_day_limit;
 
         DatePickerFragment mockDatePickerFragment = mock(DatePickerFragment.class);
 
@@ -318,6 +331,333 @@ public class CreditCardActivityTest {
         mCreditCardActivity.fillBankItemsSpinner(items);
 
         verify(mSpinnerWithInternalImage).createSpinner(R.string.add_card_banks_hint, items);
+    }
+
+    @Test
+    public void testSubmitCash_showMessageErrorCardNameField() throws Exception {
+
+        when(mButton.getId()).thenReturn(R.id.create_credit_card);
+
+        CreditCardValidator creditCardValidatorMock = mock(CreditCardValidator.class);
+
+        PowerMockito.doReturn(creditCardValidatorMock).
+                when(mCreditCardActivity, method(CreditCardActivity.class, "applyFieldValidators")).withNoArguments();
+
+        when(creditCardValidatorMock.validate()).thenReturn(false);
+
+        when(creditCardValidatorMock.isValidCardName()).thenReturn(false);
+
+        when(creditCardValidatorMock.getCardNameError()).thenReturn(R.string.error_empty_value);
+
+        String errorMessage = "Field error";
+        doReturn(errorMessage).when(mCreditCardActivity).getString(R.string.error_empty_value);
+
+        mCreditCardActivity.onClick(mButton);
+
+        verify(creditCardValidatorMock).validate();
+        verify(creditCardValidatorMock).isValidCardName();
+        verify(mCardName).setError(errorMessage);
+        verify(mCardName).requestFocus();
+
+    }
+
+    @Test
+    public void testSubmitCash_showMessageErrorCardNumberField() throws Exception {
+
+        when(mButton.getId()).thenReturn(R.id.create_credit_card);
+
+        CreditCardValidator creditCardValidatorMock = mock(CreditCardValidator.class);
+
+        PowerMockito.doReturn(creditCardValidatorMock).
+                when(mCreditCardActivity, method(CreditCardActivity.class, "applyFieldValidators")).withNoArguments();
+
+        when(creditCardValidatorMock.validate()).thenReturn(false);
+        when(creditCardValidatorMock.isValidCardName()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardNumber()).thenReturn(false);
+
+        when(creditCardValidatorMock.getCardNumberError()).thenReturn(R.string.error_empty_value);
+
+        String errorMessage = "Field error";
+        doReturn(errorMessage).when(mCreditCardActivity).getString(R.string.error_empty_value);
+
+        mCreditCardActivity.onClick(mButton);
+
+        verify(creditCardValidatorMock).validate();
+        verify(creditCardValidatorMock).isValidCardNumber();
+        verify(mCardNumber).setError(errorMessage);
+        verify(mCardNumber).requestFocus();
+    }
+
+    @Test
+    public void testSubmitCash_showMessageErrorCardBrandField() throws Exception {
+
+        when(mButton.getId()).thenReturn(R.id.create_credit_card);
+
+        CreditCardValidator creditCardValidatorMock = mock(CreditCardValidator.class);
+
+        PowerMockito.doReturn(creditCardValidatorMock).
+                when(mCreditCardActivity, method(CreditCardActivity.class, "applyFieldValidators")).withNoArguments();
+
+        when(creditCardValidatorMock.validate()).thenReturn(false);
+
+        when(creditCardValidatorMock.isValidCardName()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardNumber()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardBrand()).thenReturn(false);
+
+        when(creditCardValidatorMock.getCreditCardBrandError()).thenReturn(R.string.error_empty_value);
+
+        mockStatic(Toast.class);
+
+        Toast mockToast = mock(Toast.class);
+
+        when(Toast.makeText(mCreditCardActivity,
+                R.string.error_wrong_brand_card, Toast.LENGTH_LONG)).
+                thenReturn(mockToast);
+
+        mCreditCardActivity.onClick(mButton);
+
+        verify(creditCardValidatorMock).validate();
+        verify(creditCardValidatorMock).isValidCardBrand();
+        verify(mCardBrands).requestFocus();
+        verifyStatic(times(1));
+        Toast.makeText(mCreditCardActivity, R.string.error_wrong_brand_card,
+                Toast.LENGTH_LONG);
+        verify(mockToast).show();
+
+    }
+
+    @Test
+    public void testSubmitCash_showMessageErrorBankField() throws Exception {
+
+        when(mButton.getId()).thenReturn(R.id.create_credit_card);
+
+        CreditCardValidator creditCardValidatorMock = mock(CreditCardValidator.class);
+
+        PowerMockito.doReturn(creditCardValidatorMock).
+                when(mCreditCardActivity, method(CreditCardActivity.class, "applyFieldValidators")).withNoArguments();
+
+        when(creditCardValidatorMock.validate()).thenReturn(false);
+
+        when(creditCardValidatorMock.isValidCardName()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardNumber()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardBrand()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardBank()).thenReturn(false);
+
+        when(creditCardValidatorMock.getBankError()).thenReturn(R.string.error_empty_value);
+
+        String errorMessage = "Field error";
+        doReturn(errorMessage).when(mCreditCardActivity).getString(R.string.error_empty_value);
+
+        mockStatic(Toast.class);
+
+        Toast mockToast = mock(Toast.class);
+
+        when(Toast.makeText(mCreditCardActivity,
+                R.string.error_wrong_bank, Toast.LENGTH_LONG)).
+                thenReturn(mockToast);
+
+        mCreditCardActivity.onClick(mButton);
+
+        verify(creditCardValidatorMock).validate();
+        verify(creditCardValidatorMock).isValidCardBank();
+        verify(mSpinnerWithInternalImage).requestFocus();
+        verifyStatic(times(1));
+        Toast.makeText(mCreditCardActivity, R.string.error_wrong_bank,
+                Toast.LENGTH_LONG);
+        verify(mockToast).show();
+    }
+
+    @Test
+    public void testSubmitCash_showMessageErrorCreditLimitField() throws Exception {
+
+        when(mButton.getId()).thenReturn(R.id.create_credit_card);
+
+        CreditCardValidator creditCardValidatorMock = mock(CreditCardValidator.class);
+
+        PowerMockito.doReturn(creditCardValidatorMock).
+                when(mCreditCardActivity, method(CreditCardActivity.class, "applyFieldValidators")).withNoArguments();
+
+        when(creditCardValidatorMock.validate()).thenReturn(false);
+
+        when(creditCardValidatorMock.isValidCardName()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardNumber()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardBrand()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardBank()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCreditLimit()).thenReturn(false);
+
+        when(creditCardValidatorMock.getCreditLimitError()).thenReturn(R.string.error_empty_value);
+
+        String errorMessage = "Field error";
+        doReturn(errorMessage).when(mCreditCardActivity).getString(R.string.error_empty_value);
+
+        mCreditCardActivity.onClick(mButton);
+
+        verify(creditCardValidatorMock).validate();
+        verify(creditCardValidatorMock).isValidCreditLimit();
+        verify(mCreditLimit).setError(errorMessage);
+        verify(mCreditLimit).requestFocus();
+
+    }
+
+    @Test
+    public void testSubmitCash_showMessageErrorCurrentBalanceField() throws Exception {
+
+        when(mButton.getId()).thenReturn(R.id.create_credit_card);
+
+        CreditCardValidator creditCardValidatorMock = mock(CreditCardValidator.class);
+
+        PowerMockito.doReturn(creditCardValidatorMock).
+                when(mCreditCardActivity, method(CreditCardActivity.class, "applyFieldValidators")).withNoArguments();
+
+        when(creditCardValidatorMock.validate()).thenReturn(false);
+
+        when(creditCardValidatorMock.isValidCardName()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardNumber()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardBrand()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardBank()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCreditLimit()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCurrentBalance()).thenReturn(false);
+
+        when(creditCardValidatorMock.getCurrentBalanceError()).thenReturn(R.string.error_empty_value);
+
+        String errorMessage = "Field error";
+        doReturn(errorMessage).when(mCreditCardActivity).getString(R.string.error_empty_value);
+
+        mCreditCardActivity.onClick(mButton);
+
+        verify(creditCardValidatorMock).validate();
+        verify(creditCardValidatorMock).isValidCurrentBalance();
+        verify(mCurrentBalance).setError(errorMessage);
+        verify(mCurrentBalance).requestFocus();
+
+    }
+
+    @Test
+    public void testSubmitCash_showMessageErrorCuttoffDateField() throws Exception {
+
+        when(mButton.getId()).thenReturn(R.id.create_credit_card);
+
+        CreditCardValidator creditCardValidatorMock = mock(CreditCardValidator.class);
+
+        PowerMockito.doReturn(creditCardValidatorMock).
+                when(mCreditCardActivity, method(CreditCardActivity.class, "applyFieldValidators")).withNoArguments();
+
+        when(creditCardValidatorMock.validate()).thenReturn(false);
+
+        when(creditCardValidatorMock.isValidCardName()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardNumber()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardBrand()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardBank()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCreditLimit()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCurrentBalance()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCuttoffDate()).thenReturn(false);
+
+        when(creditCardValidatorMock.getCuttoffDateError()).thenReturn(R.string.error_empty_value);
+
+        String errorMessage = "Field error";
+        doReturn(errorMessage).when(mCreditCardActivity).getString(R.string.error_empty_value);
+
+        mCreditCardActivity.onClick(mButton);
+
+        verify(creditCardValidatorMock).validate();
+        verify(creditCardValidatorMock).isValidCuttoffDate();
+        verify(mCuttoffDate).setError(errorMessage);
+        verify(mCuttoffDate).requestFocus();
+
+    }
+
+    @Test
+    public void testSubmitCash_showMessageErrorPayDayLimitField() throws Exception {
+
+        when(mButton.getId()).thenReturn(R.id.create_credit_card);
+
+        CreditCardValidator creditCardValidatorMock = mock(CreditCardValidator.class);
+
+        PowerMockito.doReturn(creditCardValidatorMock).
+                when(mCreditCardActivity, method(CreditCardActivity.class, "applyFieldValidators")).withNoArguments();
+
+        when(creditCardValidatorMock.validate()).thenReturn(false);
+
+        when(creditCardValidatorMock.isValidCardName()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardNumber()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardBrand()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCardBank()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCreditLimit()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCurrentBalance()).thenReturn(true);
+        when(creditCardValidatorMock.isValidCuttoffDate()).thenReturn(true);
+        when(creditCardValidatorMock.isValidPayDayLimit()).thenReturn(false);
+
+        when(creditCardValidatorMock.getPayDayLimitError()).thenReturn(R.string.error_empty_value);
+
+        String errorMessage = "Field error";
+        doReturn(errorMessage).when(mCreditCardActivity).getString(R.string.error_empty_value);
+
+        mCreditCardActivity.onClick(mButton);
+
+        verify(creditCardValidatorMock).validate();
+        verify(creditCardValidatorMock).isValidPayDayLimit();
+        verify(mPayDayLimit).setError(errorMessage);
+        verify(mPayDayLimit).requestFocus();
+
+    }
+
+    @Test
+    public void testCloseConnections() {
+
+        mCreditCardActivity.closeConnections();
+
+        verify(mCreditCardPresenter).unnusedView();
+    }
+
+    @Test
+    public void testSuccessfulInsertion_invokeToask() {
+
+        mockStatic(Toast.class);
+
+        Toast mockToast = mock(Toast.class);
+
+        when(Toast.makeText(mCreditCardActivity,
+                CreditCardActivity.SUCCESS_INSERTION_CREDIT_CARD, Toast.LENGTH_LONG)).thenReturn(mockToast);
+
+        mCreditCardActivity.notifySuccessfulInsertion();
+
+        verifyStatic(times(1));
+        Toast.makeText(mCreditCardActivity, CreditCardActivity.SUCCESS_INSERTION_CREDIT_CARD, Toast.LENGTH_LONG);
+        verify(mockToast).show();
+    }
+
+    @Test
+    public void testErrorInsertion_invokeToask() {
+
+        mockStatic(Toast.class);
+
+        Toast mockToast = mock(Toast.class);
+
+        when(Toast.makeText(mCreditCardActivity,
+                CreditCardActivity.ERROR_INSERTION_CREDIT_CARD, Toast.LENGTH_LONG)).thenReturn(mockToast);
+
+        mCreditCardActivity.notifyErrorInsertion();
+
+        verifyStatic(times(1));
+        Toast.makeText(mCreditCardActivity, CreditCardActivity.ERROR_INSERTION_CREDIT_CARD, Toast.LENGTH_LONG);
+        verify(mockToast).show();
+    }
+
+    @Test
+    public void testReturnToMyPaymentsMethods_invokeLaunchIntent() {
+
+        mockStatic(LaunchIntents.class);
+
+        PowerMockito.doNothing().when(LaunchIntents.class);
+
+        LaunchIntents.launchIntentClearTop(mCreditCardActivity, Class.class);
+
+        mCreditCardActivity.returnToMyPaymentsMethods();
+
+        verifyStatic(times(1));
+
+        LaunchIntents.launchIntentClearTop(mCreditCardActivity, MainActivity.class);
+
     }
 
     private double calculateAvailableCredit(String currentBalance, String creditLimit) {
