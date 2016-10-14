cd00e5e7cf669eb8a3b588cbff3445f8f9296c69
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-10-14 12:21:18 -0500

    Add bankCardId field

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/CardDetailDaoImpl.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/daos/CardDetailDaoImpl.java
@@ -20,8 +20,6 @@ public class CardDetailDaoImpl extends SQLiteGlobalDao implements CardDetailDao 
     @Override
     public long createCardDetail(CardDetailModel cardDetailModel) {
 
-        openConnection();
-
         ContentValues contentValues = createContentValues(cardDetailModel);
 
         long result = mDatabase.insert(CardDetailContract.CardDetail.TABLE_NAME, CardDetailContract.CardDetail.COLUMN_NAME_NULLABLE,
@@ -90,6 +88,7 @@ public class CardDetailDaoImpl extends SQLiteGlobalDao implements CardDetailDao 
         contentValues.put(CardDetailContract.CardDetail.COLUMN_CURRENT_BALANCE, cardDetailModel.getCurrentBalance());
         contentValues.put(CardDetailContract.CardDetail.COLUMN_CUTOFF_DATE, cardDetailModel.getCuttoffDate());
         contentValues.put(CardDetailContract.CardDetail.COLUMN_PAY_DAY_LIMIT, cardDetailModel.getPayDayLimit());
+        contentValues.put(CardDetailContract.CardDetail.COLUMN_BANK_CARD_ID, cardDetailModel.getBankCardId());
 
         return contentValues;
     }
@@ -103,12 +102,14 @@ public class CardDetailDaoImpl extends SQLiteGlobalDao implements CardDetailDao 
         double currentBalance = cursor.getDouble(cursor.getColumnIndex(CardDetailContract.CardDetail.COLUMN_CURRENT_BALANCE));
         String cutoffDate = cursor.getString(cursor.getColumnIndex(CardDetailContract.CardDetail.COLUMN_CUTOFF_DATE));
         String payDayLimit = cursor.getString(cursor.getColumnIndex(CardDetailContract.CardDetail.COLUMN_PAY_DAY_LIMIT));
+        long bankCardId = cursor.getLong(cursor.getColumnIndex(CardDetailContract.CardDetail.COLUMN_BANK_CARD_ID));
 
         cardDetailModel.setId(id);
         cardDetailModel.setCreditLimit(creditLimit);
         cardDetailModel.setCurrentBalance(currentBalance);
         cardDetailModel.setCuttoffDate(cutoffDate);
         cardDetailModel.setPayDayLimit(payDayLimit);
+        cardDetailModel.setBankCardId(bankCardId);
 
         return cardDetailModel;
     }
