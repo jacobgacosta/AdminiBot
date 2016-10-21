9827d8538156873626f9d01f7ba3b79b6f2b07be
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-10-21 12:36:57 -0500

    Create RecyclerItemException

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/exceptions/RecyclerItemException.java
@@ -0,0 +1,9 @@
+package io.dojogeek.adminibot.exceptions;
+
+public class RecyclerItemException extends Exception {
+
+    public RecyclerItemException(String message) {
+        super(message);
+    }
+
+}
