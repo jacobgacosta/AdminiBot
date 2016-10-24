c94f9554c329406037afc3d414706d62f604ce86
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-10-24 10:11:52 -0500

    Create DtoCreditCardAdapter

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/adapters/dtos/DtoCreditCardAdapter.java
@@ -0,0 +1,41 @@
+package io.dojogeek.adminibot.adapters.dtos;
+
+public class DtoCreditCardAdapter {
+
+    private String mCreditCardName;
+    private String mCreditCardNumber;
+    private String mCreditCardBankImageName;
+    private long mCardId;
+
+    public String getCreditCardName() {
+        return mCreditCardName;
+    }
+
+    public void setCreditCardName(String creditCardName) {
+        mCreditCardName = creditCardName;
+    }
+
+    public String getCreditCardNumber() {
+        return mCreditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        mCreditCardNumber = creditCardNumber;
+    }
+
+    public String getCreditCardBankImageName() {
+        return mCreditCardBankImageName;
+    }
+
+    public void setCreditCardBankImageName(String creditCardBankImageName) {
+        mCreditCardBankImageName = creditCardBankImageName;
+    }
+
+    public long getCardId() {
+        return mCardId;
+    }
+
+    public void setCardId(long cardId) {
+        mCardId = cardId;
+    }
+}
