dec12ac6cf48ba08b9cdd4847b136232129d884f
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-10-24 10:12:04 -0500

    Create RecyclerItemOnClickListener

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/listeners/RecyclerItemOnClickListener.java
@@ -0,0 +1,9 @@
+package io.dojogeek.adminibot.listeners;
+
+import android.view.View;
+
+public interface RecyclerItemOnClickListener {
+
+    void onClick(View itemView, long selectedRecordId);
+
+}
