313cdc3a8e5167cc22fcf42ef20c06d5bd56a7e3
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-11-01 10:50:53 -0600

    Define showCreditCardDetail

---
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/CreditCardDetail.java
@@ -0,0 +1,10 @@
+package io.dojogeek.adminibot.views;
+
+import io.dojogeek.adminibot.dtos.DtoCreditCardDetail;
+import io.dojogeek.adminibot.models.CreditCardModel;
+
+public interface CreditCardDetail {
+
+    void showCreditCardDetail(DtoCreditCardDetail creditCardDetail);
+
+}
