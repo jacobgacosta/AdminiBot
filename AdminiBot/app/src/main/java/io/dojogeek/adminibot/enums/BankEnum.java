9c819a82743ec51dc0d641ba763416f9f737dbf2
Jacob G.Acosta <jgacosta@dojogeek.io>
2016-12-29 23:26:41 -0600

    Use ic_amex like default name for card icon

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/enums/BankEnum.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/enums/BankEnum.java
@@ -5,25 +5,25 @@ import io.dojogeek.adminibot.R;
 
 public enum BankEnum {
 
-    BBVA_BANCOMER(R.string.banks_bancomer, "ic_bbva"),
-    BANAMEX(R.string.banks_banamex, "ic_banamex"),
-    BANORTE(R.string.banks_banorte, "ic_banorte"),
-    SANTANDER(R.string.banks_santander, "ic_santander"),
-    HSBC(R.string.banks_hsbc, "ic_hsbc"),
-    INBURSA_WALMART(R.string.banks_wallmart, "ic_wallmart"),
-    SCOTIABANK(R.string.banks_scotiabank, "ic_scotia"),
+    BBVA_BANCOMER(R.string.banks_bancomer, "ic_amex"),
+    BANAMEX(R.string.banks_banamex, "ic_amex"),
+    BANORTE(R.string.banks_banorte, "ic_amex"),
+    SANTANDER(R.string.banks_santander, "ic_amex"),
+    HSBC(R.string.banks_hsbc, "ic_amex"),
+    INBURSA_WALMART(R.string.banks_wallmart, "ic_amex"),
+    SCOTIABANK(R.string.banks_scotiabank, "ic_amex"),
     AMERICAN_EXPRESS(R.string.banks_amex, "ic_amex"),
 //    INTERACCIONES("Interacciones", "ic_interacciones"),
-    BANCO_AZTECA(R.string.banks_bnk_azteca, "ic_banco_azteca"),
-    BANREGIO(R.string.banks_banregio, "ic_banregio"),
+    BANCO_AZTECA(R.string.banks_bnk_azteca, "ic_amex"),
+    BANREGIO(R.string.banks_banregio, "ic_amex"),
 //    MULTIVA("Multiva", "ic_multiva"),
 //    MIFIEL("Mifiel", "ic_mifiel"),
 //    COMPARTAMOS("Compartamos", "ic_compartamos"),
 //    AFIRME("Afirme", "ic_afirme"),
 //    VE_POR_MAS("Ve por más", "ic_ve_por_mas"),
 //    INVEX("Invex", "ic_invex"),
-    BANCOPPEL(R.string.banks_bancoopel, "ic_bancoppel"),
-    BANCO_AHORRO_FAMSA(R.string.banks_famsa, "ic_baf");
+    BANCOPPEL(R.string.banks_bancoopel, "ic_amex"),
+    BANCO_AHORRO_FAMSA(R.string.banks_famsa, "ic_amex");
 //    BANSI("Bansi", "ic_bansi");
 
     private int mName;
