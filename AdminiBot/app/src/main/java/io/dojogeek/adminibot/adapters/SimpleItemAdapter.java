506311cf99cc0177a178925d4c8874cacdfa080f
Jacob G.Acosta <jgacosta@dojogeek.io>
2017-01-03 23:03:38 -0600

    Add imageView to ViewHolder

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/adapters/SimpleItemAdapter.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/adapters/SimpleItemAdapter.java
@@ -1,10 +1,15 @@
 package io.dojogeek.adminibot.adapters;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -15,12 +20,13 @@ import io.dojogeek.adminibot.listeners.RecyclerItemOnClickListener;
 
 public class SimpleItemAdapter extends RecyclerView.Adapter<SimpleItemAdapter.ViewHolder> {
 
+    public static final String DRAWABLE = "drawable";
     private List<DtoSimpleAdapter> mSimpleAdapterList;
     private static Context sContext;
 
     public SimpleItemAdapter(Context context, List<DtoSimpleAdapter> simpleAdapterList) {
         mSimpleAdapterList = simpleAdapterList;
-        sContext = validateListenerContext(context);
+        sContext = context;
     }
 
     @Override
@@ -39,7 +45,9 @@ public class SimpleItemAdapter extends RecyclerView.Adapter<SimpleItemAdapter.Vi
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.itemId = mSimpleAdapterList.get(position).getId();
         holder.title.setText(mSimpleAdapterList.get(position).getTitle());
-        holder.subtitle.setText(mSimpleAdapterList.get(position).getSubtitle()) ;
+        holder.subtitle.setText(mSimpleAdapterList.get(position).getSubtitle());
+        holder.imageView.setImageDrawable(getDrawableFromName(mSimpleAdapterList.
+                        get(position).getIconName()));
 
     }
 
@@ -58,11 +66,13 @@ public class SimpleItemAdapter extends RecyclerView.Adapter<SimpleItemAdapter.Vi
         public long itemId;
         public TextView title;
         public TextView subtitle;
+        public ImageView imageView;
 
         public ViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.payment_method_title);
             subtitle = (TextView) itemView.findViewById(R.id.payment_method_subtitle);
+            imageView = (ImageView) itemView.findViewById(R.id.payment_method_icon);
 
             setListenerToView(itemView);
         }
@@ -86,4 +96,15 @@ public class SimpleItemAdapter extends RecyclerView.Adapter<SimpleItemAdapter.Vi
 
         return context;
     }
+
+    private Drawable getDrawableFromName(String nameResource) {
+
+        Resources resources = sContext.getResources();
+        int drawableId = resources.getIdentifier(nameResource, DRAWABLE, sContext.getPackageName());
+
+        Drawable drawable = ResourcesCompat.getDrawable(resources, drawableId, null);
+
+        return drawable;
+    }
+
 }
