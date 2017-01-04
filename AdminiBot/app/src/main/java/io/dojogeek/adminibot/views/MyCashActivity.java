e27c700566a4644d0eeaa31fe1a306200da667e7
Jacob G.Acosta <jgacosta@dojogeek.io>
2017-01-03 23:09:14 -0600

    Implement listMyCash method

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/MyCashActivity.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/MyCashActivity.java
@@ -1,16 +1,27 @@
 package io.dojogeek.adminibot.views;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+import javax.inject.Inject;
 
 import dagger.AdminiBotComponent;
 import dagger.AdminiBotModule;
 import dagger.AppComponent;
 import io.dojogeek.adminibot.R;
+import io.dojogeek.adminibot.adapters.SimpleItemAdapter;
+import io.dojogeek.adminibot.dtos.DtoSimpleAdapter;
+import io.dojogeek.adminibot.presenters.MyCashPresenter;
 
 public class MyCashActivity extends BaseActivity implements MyCash {
 
     private RecyclerView mRecyclerView;
+
+    @Inject
+    public MyCashPresenter mMyCashPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,15 +48,37 @@ public class MyCashActivity extends BaseActivity implements MyCash {
     @Override
     protected void loadDataView() {
 
+        setTitle(R.string.title_my_cash_activity);
+
+        loadMyCash();
     }
 
     @Override
     protected int getLayoutActivity() {
-        return R.layout.activity_my_cash;
+        return R.layout.activity_my_credit_cards;
     }
 
     @Override
     protected void closeConnections() {
+        mMyCashPresenter.unnusedView();
+    }
+
+    private void loadMyCash() {
+        mMyCashPresenter.obtainCash();
+    }
+
+    @Override
+    public void listMyCash(List<DtoSimpleAdapter> cashList) {
+
+        mRecyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        SimpleItemAdapter simpleItemAdapter = new SimpleItemAdapter(this, cashList);
+
+        mRecyclerView.setAdapter(simpleItemAdapter);
 
     }
 }
