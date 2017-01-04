b875afa94a3c1f5b7fba41001af2b355cceefe1a
Jacob G.Acosta <jgacosta@dojogeek.io>
2017-01-03 23:00:52 -0600

    Implement test cases for MyCashActivity

--- AdminiBot/app/src/test/java/io.dojogeek.adminibot/views/MyCashActivityTest.java
+++ AdminiBot/app/src/test/java/io.dojogeek.adminibot/views/MyCashActivityTest.java
@@ -1,6 +1,8 @@
 package io.dojogeek.adminibot.views;
 
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.SimpleAdapter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,13 +12,21 @@ import org.mockito.Spy;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import dagger.AdminiBotComponent;
 import dagger.AdminiBotModule;
 import dagger.AppComponent;
 import io.dojogeek.adminibot.R;
+import io.dojogeek.adminibot.adapters.SimpleItemAdapter;
+import io.dojogeek.adminibot.dtos.DtoSimpleAdapter;
+import io.dojogeek.adminibot.presenters.CashPresenter;
+import io.dojogeek.adminibot.presenters.MyCashPresenter;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -30,6 +40,9 @@ public class MyCashActivityTest {
     @Mock
     private RecyclerView mRecyclerView;
 
+    @Mock
+    public MyCashPresenter mMyCashPresenter;
+
     @InjectMocks
     @Spy
     private MyCashActivity mCashActivity = new MyCashActivity();
@@ -39,7 +52,7 @@ public class MyCashActivityTest {
 
         int currentLayout = mCashActivity.getLayoutActivity();
 
-        assertThat(currentLayout, is(R.layout.activity_my_cash));
+        assertThat(currentLayout, is(R.layout.activity_my_credit_cards));
     }
 
     @Test
@@ -69,4 +82,51 @@ public class MyCashActivityTest {
         verify(mCashActivity).findViewById(R.id.my_recycler_view);
     }
 
+    @Test
+    public void testLoadDataView_setTitle() {
+
+        doNothing().when(mCashActivity).setTitle(R.string.title_my_cash_activity);
+
+        mCashActivity.loadDataView();
+
+        verify(mCashActivity).setTitle(R.string.title_my_cash_activity);
+    }
+
+    @Test
+    public void testLoadDataView_showCreditCardsList() throws Exception {
+
+        doNothing().when(mCashActivity).setTitle(R.string.title_my_cash_activity);
+
+        mCashActivity.loadDataView();
+
+        verify(mMyCashPresenter).obtainCash();
+
+    }
+
+    @Test
+    public void testUnnusedView_closeConnections() {
+
+        mCashActivity.closeConnections();
+
+        verify(mMyCashPresenter).unnusedView();
+    }
+
+    @Test
+    public void test_listMyCash() throws Exception {
+
+        LinearLayoutManager linearLayoutManagerMock = mock(LinearLayoutManager.class);
+        whenNew(LinearLayoutManager.class).withArguments(mCashActivity).thenReturn(linearLayoutManagerMock);
+
+        List<DtoSimpleAdapter> simpleAdapterList = new ArrayList<>();
+
+        SimpleItemAdapter simpleItemAdapterMock = mock(SimpleItemAdapter.class);
+        whenNew(SimpleItemAdapter.class).withArguments(mCashActivity, simpleAdapterList).
+                thenReturn(simpleItemAdapterMock);
+
+        mCashActivity.listMyCash(simpleAdapterList);
+
+        verify(mRecyclerView).setHasFixedSize(true);
+        verify(mRecyclerView).setLayoutManager(linearLayoutManagerMock);
+        verify(mRecyclerView).setAdapter(simpleItemAdapterMock);
+    }
 }
