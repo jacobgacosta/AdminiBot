f54ced71524ef86a636f8dd0772bacf6d8e49735
Jacob G.Acosta <jgacosta@dojogeek.io>
2017-01-03 23:09:03 -0600

    Declare listMyCash method

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/MyCash.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/MyCash.java
@@ -1,5 +1,11 @@
 package io.dojogeek.adminibot.views;
 
 
+import java.util.List;
+
+import io.dojogeek.adminibot.dtos.DtoSimpleAdapter;
+
 public interface MyCash {
+
+    void listMyCash(List<DtoSimpleAdapter> cashList);
 }
