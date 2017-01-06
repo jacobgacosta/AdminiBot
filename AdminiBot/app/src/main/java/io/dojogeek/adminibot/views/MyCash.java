7ffd04f18870f9c43cf9e4c5b047eaca170fe9e0
Jacob G.Acosta <jgacosta@dojogeek.io>
2017-01-05 21:03:14 -0600

    Declare showTotalCash method

--- AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/MyCash.java
+++ AdminiBot/app/src/main/java/io/dojogeek/adminibot/views/MyCash.java
@@ -1,6 +1,7 @@
 package io.dojogeek.adminibot.views;
 
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import io.dojogeek.adminibot.dtos.DtoSimpleAdapter;
@@ -8,4 +9,6 @@ import io.dojogeek.adminibot.dtos.DtoSimpleAdapter;
 public interface MyCash {
 
     void listMyCash(List<DtoSimpleAdapter> cashList);
+
+    void showTotalCash(BigDecimal total);
 }
