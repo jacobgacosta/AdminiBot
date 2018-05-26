105fa7fceb50626ff34dd88fa792649ddc09863a
Jacob G.Acosta <dosek17@gmail.com>
2018-05-25 22:39:27 -0500

    Implement Parceable
    Also equals and hashcode methods are overwritten.

--- app/src/main/java/io/dojogeek/adminibot/dtos/DebitCardDto.java
+++ app/src/main/java/io/dojogeek/adminibot/dtos/DebitCardDto.java
@@ -1,12 +1,35 @@
 package io.dojogeek.adminibot.dtos;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class DebitCardDto implements Serializable {
+public class DebitCardDto implements Parcelable {
+
+    public static final Creator<DebitCardDto> CREATOR = new Creator<DebitCardDto>() {
+        @Override
+        public DebitCardDto createFromParcel(Parcel in) {
+            return new DebitCardDto(in);
+        }
+
+        @Override
+        public DebitCardDto[] newArray(int size) {
+            return new DebitCardDto[size];
+        }
+    };
 
     private String mName;
     private String mNumber;
     private String mAmount;
+
+    public DebitCardDto() {
+
+    }
+
+    public DebitCardDto(Parcel in) {
+        mName = in.readString();
+        mNumber = in.readString();
+        mAmount = in.readString();
+    }
 
     public String getName() {
         return mName;
@@ -30,6 +53,50 @@ public class DebitCardDto implements Serializable {
 
     public void setAmount(String total) {
         this.mAmount = total;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mName);
+        dest.writeString(mNumber);
+        dest.writeString(mAmount);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof DebitCardDto)) {
+            return false;
+        }
+
+        DebitCardDto that = (DebitCardDto) o;
+
+        if (!mName.equals(that.mName)) {
+            return false;
+        }
+
+        if (!mNumber.equals(that.mNumber)) {
+            return false;
+        }
+
+        return mAmount.equals(that.mAmount);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = mName.hashCode();
+        result = 31 * result + mNumber.hashCode();
+        result = 31 * result + mAmount.hashCode();
+
+        return result;
     }
 
 }
