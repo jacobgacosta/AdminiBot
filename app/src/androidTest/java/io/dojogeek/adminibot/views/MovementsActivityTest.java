88fb2c2eade12a0ebe15d96a0c4ea3066c996636
Jacob G.Acosta <dosek17@gmail.com>
2018-05-27 23:18:40 -0500

    Remove unused imports

--- app/src/androidTest/java/io/dojogeek/adminibot/views/MovementsActivityTest.java
+++ app/src/androidTest/java/io/dojogeek/adminibot/views/MovementsActivityTest.java
@@ -25,7 +25,6 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.BundleMatchers.hasEntry;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtras;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -34,7 +33,6 @@ import static org.hamcrest.CoreMatchers.anything;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasItemInArray;
 
 @RunWith(AndroidJUnit4.class)
 @MediumTest
